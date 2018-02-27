9c2aa84e9e5efbea55a228363b1e3376e62ada49
Dave Smith <david.j.smith@itv.com>
2018-02-27 16:55:32 +0000

    Put gaps into the walls.

--- snake/src/main/scala/snake/SnakeInit.scala
+++ snake/src/main/scala/snake/SnakeInit.scala
@@ -6,23 +6,16 @@ import com.purplekingdomgames.indigo.gameengine.{Startup, ToReportable}
 
 object SnakeInit {
 
-  def initialise(gridSize: GridSize): AssetCollection => Startup[ErrorReport, SnakeStartupData] = _ => {
-    val wall: Graphic[SnakeEvent] =
-      Graphic(0, 0, 16, 16, 1, SnakeAssets.snakeTexture)
-
+  def initialise(gridSize: GridSize): AssetCollection => Startup[ErrorReport, SnakeStartupData] = _ =>
     SnakeStartupData(
       gridSize = gridSize,
       staticAssets = StaticAssets(
-        wall = wall,
+        wall = Graphic(0, 0, 16, 16, 1, SnakeAssets.snakeTexture),
         apple = Graphic(0, 0, 16, 16, 1, SnakeAssets.snakeTexture).withCrop(16, 0, 16, 16),
-//        snakeHead = Graphic(0, 0, 16, 16, 1, SnakeAssets.snakeTexture).withCrop(0, 16, 16, 16),
-//        snakeBody = Graphic(0, 0, 16, 16, 1, SnakeAssets.snakeTexture).withCrop(16, 16, 16, 16),
-//        snakeTail = Graphic(0, 0, 16, 16, 1, SnakeAssets.snakeTexture).withCrop(32, 16, 16, 16),
         snakeAlive = Graphic(0, 0, 16, 16, 1, SnakeAssets.snakeTexture).withCrop(48, 48, 16, 16),
         snakeDead = Graphic(0, 0, 16, 16, 1, SnakeAssets.snakeTexture).withCrop(32, 48, 16, 16)
       )
     )
-  }
 
 }
 
@@ -31,6 +24,7 @@ case class SnakeStartupData(gridSize: GridSize, staticAssets: StaticAssets)
 case class StaticAssets(wall: Graphic[SnakeEvent], apple: Graphic[SnakeEvent], snakeAlive: Graphic[SnakeEvent], snakeDead: Graphic[SnakeEvent])
 
 case class ErrorReport(errors: List[String])
+
 object ErrorReport {
 
   implicit val toErrorReport: ToReportable[ErrorReport] =
