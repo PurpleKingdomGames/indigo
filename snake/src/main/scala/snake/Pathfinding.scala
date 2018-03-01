ebf066db90573421e999be2dc969c28a45e4b440
Dave Smith <david.j.smith@itv.com>
2018-03-01 23:58:38 +0000

    WIP: Scoring the grid, see failing test.

--- snake/src/main/scala/snake/Pathfinding.scala
+++ snake/src/main/scala/snake/Pathfinding.scala
@@ -1,8 +1,14 @@
 package snake
 
+import scala.annotation.tailrec
+
 /**
-  * Briefly considered trying to do something clever (A* perhaps?),
-  * but I have small grids, so I'm doing something crude instead.
+  * This is very crude and inefficient, but should be ok for the Snake use case,
+  * in that:
+  *
+  * 1) Snake has small grids, so the amount of the grid to search requires no optimisation.
+  * 2) Snake does not do diagonals, so scoring does not expand diagonally either.
+  * 3) Snake does not have different types of terrain, so there is no weighting
   */
 object Pathfinding {
 
@@ -10,13 +16,16 @@ object Pathfinding {
 
 }
 
-case class SearchGrid(validationWidth: Int, validationHeight: Int, grid: List[GridSquare]) {
+case class SearchGrid(validationWidth: Int, validationHeight: Int, start: Coords, end: Coords, grid: List[GridSquare]) {
 
   def isValid: Boolean =
     SearchGrid.isValid(this)
 
   def sampleAt(coords: Coords): List[GridSquare] =
     SearchGrid.sampleAt(this, coords, validationWidth)
+
+  def score: SearchGrid =
+    SearchGrid.score(this)
 
 }
 
@@ -32,14 +41,10 @@ object SearchGrid {
 
   def sampleAt(searchGrid: SearchGrid, coords: Coords, gridWidth: Int): List[GridSquare] = {
     List(
-      coords + Coords.relativeUpLeft,
       coords + Coords.relativeUp,
-      coords + Coords.relativeUpRight,
       coords + Coords.relativeLeft,
       coords + Coords.relativeRight,
-      coords + Coords.relativeDownLeft,
-      coords + Coords.relativeDown,
-      coords + Coords.relativeDownRight
+      coords + Coords.relativeDown
     ).filter(c => coordsWithinGrid(searchGrid, c)).map(c => searchGrid.grid(c.toGridPosition(gridWidth)))
   }
 
@@ -61,8 +66,43 @@ object SearchGrid {
       }
     }
 
-    SearchGrid(gridWidth, gridHeight, grid)
+    SearchGrid(gridWidth, gridHeight, start, end, grid)
   }
+
+  def scoreGridSquares(searchGrid: SearchGrid): List[GridSquare] = {
+    @tailrec
+    def rec(unscored: List[GridSquare], scoreValue: Int, lastCoords: List[Coords], scored: List[GridSquare]): List[GridSquare] = {
+      (unscored, lastCoords) match {
+        case (Nil, _) =>
+          scored
+
+        case (_, Nil) =>
+          scored
+
+        case (us, lc) =>
+          val next: List[GridSquare] =
+            lc.flatMap { c =>
+              sampleAt(searchGrid, c, searchGrid.validationWidth)
+                .filter(g => us.contains(g))
+                .map(_.withScore(scoreValue))
+            }
+
+          rec(
+            us.filterNot(p => next.exists(_.coords == p.coords)),
+            scoreValue + 1,
+            next.map(_.coords),
+            scored ++ next
+          )
+      }
+    }
+    
+    val (done, todo) = searchGrid.grid.partition(_.isEnd)
+
+    rec(todo, 1, List(searchGrid.end), done).sortBy(_.index)
+  }
+
+  def score(searchGrid: SearchGrid): SearchGrid =
+    searchGrid.copy(grid = scoreGridSquares(searchGrid))
 
 }
 
@@ -119,28 +159,33 @@ sealed trait GridSquare {
   val isStart: Boolean
   val isEnd: Boolean
   val score: Option[Int]
+  def withScore(score: Int): GridSquare
 }
 case class EmptySquare(index: Int, coords: Coords, score: Option[Int]) extends GridSquare {
   val name: String = "empty"
   val isStart: Boolean = false
   val isEnd: Boolean = false
+  def withScore(score: Int): EmptySquare = this.copy(score = Option(score))
 }
 case class ImpassableSquare(index: Int, coords: Coords) extends GridSquare {
   val name: String = "impassable"
   val isStart: Boolean = false
   val isEnd: Boolean = false
   val score: Option[Int] = Some(GridSquare.max)
+  def withScore(score: Int): ImpassableSquare = this
 }
 case class StartSquare(index: Int, coords: Coords) extends GridSquare {
   val name: String = "start"
   val isStart: Boolean = true
   val isEnd: Boolean = false
   val score: Option[Int] = Some(0)
+  def withScore(score: Int): StartSquare = this
 }
 case class EndSquare(index: Int, coords: Coords) extends GridSquare {
   val name: String = "end"
   val isStart: Boolean = false
   val isEnd: Boolean = true
   val score: Option[Int] = Some(GridSquare.max)
+  def withScore(score: Int): EndSquare = this
 }
 
--- snake/src/test/scala/snake/PathfindingSpec.scala
+++ snake/src/test/scala/snake/PathfindingSpec.scala
@@ -19,7 +19,27 @@ class PathfindingSpec extends FunSpec with Matchers {
   describe("Scoring the grid") {
 
     it("should be able to score a grid") {
-      pending
+      val start: Coords = Coords(2, 1)
+      val end: Coords = Coords(0, 2)
+      val impassable: Coords = Coords(1, 0)
+
+      val searchGrid = SearchGrid.generate(start, end, List(impassable), 3, 3)
+
+      val expected: List[GridSquare] =
+        List(
+          EmptySquare(0, Coords(0, 0), Some(2)),
+          ImpassableSquare(1, Coords(1, 0)),
+          EmptySquare(2, Coords(2, 0), None),
+          EmptySquare(3, Coords(0, 1), Some(1)),
+          EmptySquare(4, Coords(1, 1), Some(2)),
+          StartSquare(5, Coords(2, 1)),
+          EndSquare(6, Coords(0, 2)),
+          EmptySquare(7, Coords(1, 2), Some(1)),
+          EmptySquare(8, Coords(2, 2), Some(2))
+        )
+
+      searchGrid.score shouldEqual SearchGrid(3, 3, start, end, expected)
+
     }
 
   }
@@ -35,15 +55,11 @@ class PathfindingSpec extends FunSpec with Matchers {
 
       val expected: List[GridSquare] =
         List(
-          EmptySquare(1, Coords(1, 0), None),
           EmptySquare(2, Coords(2, 0), None),
-          EmptySquare(3, Coords(3, 0), None),
           StartSquare(5, Coords(1, 1)),
           //Sample point
           EmptySquare(7, Coords(3, 1), None),
-          EmptySquare(9, Coords(1, 2), None),
-          ImpassableSquare(10, Coords(2, 2)),
-          EndSquare(11, Coords(3, 2))
+          ImpassableSquare(10, Coords(2, 2))
         )
 
       searchGrid.sampleAt(Coords(2, 1)) shouldEqual expected
@@ -58,11 +74,9 @@ class PathfindingSpec extends FunSpec with Matchers {
 
       val expected: List[GridSquare] =
         List(
-          EmptySquare(2, Coords(2, 0), None),
           EmptySquare(3, Coords(3, 0), None),
           EmptySquare(6, Coords(2, 1), None),
           //Sample point
-          ImpassableSquare(10, Coords(2, 2)),
           EndSquare(11, Coords(3, 2))
         )
 
@@ -80,8 +94,7 @@ class PathfindingSpec extends FunSpec with Matchers {
       List(
         //Sample point
         EmptySquare(1, Coords(1, 0), None),
-        EmptySquare(4, Coords(0, 1), None),
-        StartSquare(5, Coords(1, 1))
+        EmptySquare(4, Coords(0, 1), None)
       )
 
       searchGrid.sampleAt(Coords(0, 0)) shouldEqual expected
@@ -144,9 +157,11 @@ class PathfindingSpec extends FunSpec with Matchers {
   describe("Validating a grid") {
 
     it("should be able to spot a good grid") {
+      val start: Coords = Coords(1, 1)
+      val end: Coords = Coords(2, 2)
 
       val grid: List[GridSquare] = List(
-        StartSquare(0, coords),
+        StartSquare(0, start),
         EmptySquare(0, coords, None),
         EmptySquare(0, coords, None),
 
@@ -156,13 +171,15 @@ class PathfindingSpec extends FunSpec with Matchers {
 
         EmptySquare(0, coords, None),
         EmptySquare(0, coords, None),
-        EndSquare(0, coords)
+        EndSquare(0, end)
       )
 
-      SearchGrid.isValid(SearchGrid(3, 3, grid)) shouldBe true
+      SearchGrid.isValid(SearchGrid(3, 3, start, end, grid)) shouldBe true
     }
 
     it("should be able to spot a grid missing a start") {
+      val start: Coords = Coords(1, 1)
+      val end: Coords = Coords(2, 2)
 
       val grid: List[GridSquare] = List(
         EmptySquare(0, coords, None),
@@ -175,16 +192,18 @@ class PathfindingSpec extends FunSpec with Matchers {
 
         EmptySquare(0, coords, None),
         EmptySquare(0, coords, None),
-        EndSquare(0, coords)
+        EndSquare(0, end)
       )
 
-      SearchGrid.isValid(SearchGrid(3, 3, grid)) shouldBe false
+      SearchGrid.isValid(SearchGrid(3, 3, start, end, grid)) shouldBe false
     }
 
     it("should be able to spot a grid missing an end") {
+      val start: Coords = Coords(1, 1)
+      val end: Coords = Coords(2, 2)
 
       val grid: List[GridSquare] = List(
-        StartSquare(0, coords),
+        StartSquare(0, start),
         EmptySquare(0, coords, None),
         EmptySquare(0, coords, None),
 
@@ -197,14 +216,16 @@ class PathfindingSpec extends FunSpec with Matchers {
         EmptySquare(0, coords, None)
       )
 
-      SearchGrid.isValid(SearchGrid(3, 3, grid)) shouldBe false
+      SearchGrid.isValid(SearchGrid(3, 3, start, end, grid)) shouldBe false
 
     }
 
     it("should be able to spot a grid of the wrong size") {
+      val start: Coords = Coords(1, 1)
+      val end: Coords = Coords(2, 2)
 
       val grid: List[GridSquare] = List(
-        StartSquare(0, coords),
+        StartSquare(0, start),
         EmptySquare(0, coords, None),
         EmptySquare(0, coords, None),
 
@@ -214,10 +235,10 @@ class PathfindingSpec extends FunSpec with Matchers {
 
         EmptySquare(0, coords, None),
         //EmptySquare(0, coords, None), //missing on purpose!
-        EndSquare(0, coords)
+        EndSquare(0, end)
       )
 
-      SearchGrid.isValid(SearchGrid(3, 3, grid)) shouldBe false
+      SearchGrid.isValid(SearchGrid(3, 3, start, end, grid)) shouldBe false
 
     }
 
