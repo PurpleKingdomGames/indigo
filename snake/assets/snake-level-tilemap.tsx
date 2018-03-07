fddd34d8847a4a919d890cb0dd916501eb47941e
Dave Smith <davesmith.00000@gmail.com>
2018-03-07 21:45:49 +0000

    Messing about with pixels.

---
+++ snake/assets/snake-level-tilemap.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="snake-level-tilemap" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="snake3.png" width="512" height="512"/>
+</tileset>
