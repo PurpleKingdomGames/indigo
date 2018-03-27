5808d379a026017d4d73fe9afcd2a75c43b511b4
Dave Smith <david.j.smith@itv.com>
2018-03-27 09:40:03 +0100

    Started reworking scene graph updates.

--- indigo/src/main/scala/com/purplekingdomgames/indigo/gameengine/scenegraph/SceneGraphUpdate.scala
+++ indigo/src/main/scala/com/purplekingdomgames/indigo/gameengine/scenegraph/SceneGraphUpdate.scala
@@ -23,3 +23,47 @@ object SceneGraphUpdate {
     SceneGraphUpdate(SceneGraphRootNode(gameLayer, lightingLayer, uiLayer), viewEvents)
 
 }
+
+case class SceneUpdate(layers: SceneLayers, viewEvents: List[ViewEvent], audio: SceneAudio) {
+  def +(other: SceneUpdate): SceneUpdate =
+    SceneUpdate.append(this, other)
+
+  def toSceneGraphUpdate: SceneGraphUpdate =
+    SceneGraphUpdate(
+      SceneGraphRootNode(
+        SceneGraphGameLayer(layers.game),
+        SceneGraphLightingLayer(layers.lighting),
+        SceneGraphUiLayer(layers.ui)
+      ),
+      viewEvents
+    )
+}
+object SceneUpdate {
+  def empty: SceneUpdate =
+    SceneUpdate(SceneLayers.empty, Nil, SceneAudio.Silent)
+
+  def append(a: SceneUpdate, b: SceneUpdate): SceneUpdate =
+    SceneUpdate(
+      a.layers + b.layers,
+      a.viewEvents ++ b.viewEvents,
+      SceneAudio.Silent
+    )
+}
+
+case class SceneLayers(game: List[SceneGraphNode], lighting: List[SceneGraphNode], ui: List[SceneGraphNode]) {
+  def +(other: SceneLayers): SceneLayers =
+    SceneLayers.append(this, other)
+}
+object SceneLayers {
+  def empty: SceneLayers =
+    SceneLayers(Nil, Nil, Nil)
+
+  def append(a: SceneLayers, b: SceneLayers): SceneLayers =
+    SceneLayers(a.game ++ b.game, a.lighting ++ b.lighting, a.ui ++ b.ui)
+}
+
+case class SceneAudio()
+object SceneAudio {
+  val Silent: SceneAudio =
+    SceneAudio()
+}
