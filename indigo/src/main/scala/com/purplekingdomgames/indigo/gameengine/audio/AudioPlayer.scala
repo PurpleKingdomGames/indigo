0ea8ccf30ae9a0698ffeea54d125a41a755424bf
Dave Smith <david.j.smith@itv.com>
2018-03-30 21:02:17 +0100

    Revert "Debugging sound effect playback, failed on pool loop."
    This reverts commit 4a8695b6721e621e1473943b4dd49aef0b010abd.

--- indigo/src/main/scala/com/purplekingdomgames/indigo/gameengine/audio/AudioPlayer.scala
+++ indigo/src/main/scala/com/purplekingdomgames/indigo/gameengine/audio/AudioPlayer.scala
@@ -7,8 +7,6 @@ import org.scalajs.dom.raw.{AudioBuffer, AudioContext}
 
 object AudioPlayer {
 
-  val MaxSoundEffects: Int = 5
-
   private var player: Option[IAudioPlayer] = None
 
   def apply(loadedAudioAssets: List[LoadedAudioAsset]): IAudioPlayer = {
@@ -32,11 +30,18 @@ trait IAudioPlayer {
 
 final class AudioPlayerImpl(loadedAudioAssets: List[LoadedAudioAsset], context: AudioContext) extends IAudioPlayer {
 
-  private val pool: SoundEffectPool = new SoundEffectPool(AudioPlayer.MaxSoundEffects, context)
-
+  //TODO: Should use a pool of buffer / gain node combos?
   def playSound(assetRef: String, volume: Double): Unit = {
     loadedAudioAssets.find(_.name == assetRef).foreach { sound =>
-      pool.addAndPlay(sound, volume)
+      val source = context.createBufferSource()
+      source.buffer = sound.data
+
+      val gainNode = context.createGain()
+      source.connect(gainNode)
+      gainNode.connect(context.destination)
+      gainNode.gain.value = volume
+
+      source.start(0)
     }
   }
 
@@ -50,66 +55,53 @@ class SoundEffectPool(size: Int, context: AudioContext) {
   private var position: Int = 0
   private val pool: scalajs.js.Array[AudioNode] = new scalajs.js.Array[AudioNode](size)
 
-  private def populate(): Unit = {
-    (0 until size).foreach { i =>
-      println("> " + i)
+  def populate(): Unit = {
+    (0 until size).foreach { _ =>
       val source = context.createBufferSource()
       val gainNode = context.createGain()
       source.connect(gainNode)
 
-      pool.update(i, new AudioNode(source, gainNode))
+      pool.push(new AudioNode(source, gainNode))
     }
   }
 
   def addAndPlay(audioAsset: LoadedAudioAsset, volume: Double): Unit = {
-
-    println(position)
     val node: AudioNode = pool(position)
 
-    pool.update(
-      position,
-      node
-        .replaceBuffer(audioAsset.data)
-        .connectToDestination(context.destination)
-        .setVolume(volume)
-        .startPlayback()
-    )
+    pool(position) = node
+      .replaceBuffer(audioAsset.data)
+      .connectToDestination(context.destination)
+      .setVolume(volume)
+      .startPlayback()
 
     position = if(position + 1 < size) position + 1 else 0
   }
-
-  populate()
 
 }
 
 class AudioNode(audioBufferSourceNode: AudioBufferSourceNode, gainNode: GainNode) {
 
   def replaceBuffer(audioBuffer: AudioBuffer): AudioNode = {
-    println("replaceBuffer")
     audioBufferSourceNode.buffer = audioBuffer
     this
   }
 
   def connectToDestination(audioDestinationNode: AudioDestinationNode): AudioNode = {
-    println("connectToDestination")
     gainNode.connect(audioDestinationNode)
     this
   }
 
   def disconnectFromDestination(audioDestinationNode: AudioDestinationNode): AudioNode = {
-    println("disconnectFromDestination")
     gainNode.disconnect(audioDestinationNode)
     this
   }
 
   def setVolume(volume: Double): AudioNode = {
-    println("setVolume")
     gainNode.gain.value = volume
     this
   }
 
   def startPlayback(): AudioNode = {
-    println("startPlayback")
     audioBufferSourceNode.start(0)
     this
   }
