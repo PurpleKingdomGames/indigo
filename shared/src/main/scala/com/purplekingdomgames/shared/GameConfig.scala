120bbf6cf9902a5aaa0b0007fd15537248943dd9
Dave Smith <david.j.smith@itv.com>
2018-04-01 17:38:45 +0100

    Added GameViewport presets

--- shared/src/main/scala/com/purplekingdomgames/shared/GameConfig.scala
+++ shared/src/main/scala/com/purplekingdomgames/shared/GameConfig.scala
@@ -60,6 +60,22 @@ case class GameViewport(width: Int, height: Int) {
   val verticalMiddle: Int = height / 2
   val center: (Int, Int) = (horizontalMiddle, verticalMiddle)
 }
+object GameViewport {
+  val atWUXGA: GameViewport =
+    GameViewport(1920, 1200)
+  val atWUXGABy2: GameViewport =
+    GameViewport(960, 600)
+
+  val at1080p: GameViewport =
+    GameViewport(1920, 1080)
+  val at1080pBy2: GameViewport =
+    GameViewport(960, 540)
+
+  val at720p: GameViewport =
+    GameViewport(1280, 720)
+  val at720pBy2: GameViewport =
+    GameViewport(640, 360)
+}
 
 case class AdvancedGameConfig(recordMetrics: Boolean, logMetricsReportIntervalMs: Int, disableSkipModelUpdates: Boolean, disableSkipViewUpdates: Boolean)
 object AdvancedGameConfig {
