17ed6b39994c91f27ca085d5f965358cdc7aa089
Dave Smith <davesmith.00000@gmail.com>
2018-04-05 22:40:04 +0100

    Working on assets for snake.

---
+++ snake/assets/snake-tiles-30x30.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="snake-tiles-30x30" tilewidth="30" tileheight="30" tilecount="64" columns="8">
+ <image source="snake4.png" width="256" height="256"/>
+</tileset>
