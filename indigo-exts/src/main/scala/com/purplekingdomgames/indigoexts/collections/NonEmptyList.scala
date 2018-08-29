0a6208f0d2f4955a4b9c151ef52a49f6a72f661e
Dave Smith <david.j.smith@itv.com>
2018-08-29 07:26:19 +0100

    WIP: NonEmptyList, added more ops, need to make tests pass

--- indigo-exts/src/main/scala/com/purplekingdomgames/indigoexts/collections/NonEmptyList.scala
+++ indigo-exts/src/main/scala/com/purplekingdomgames/indigoexts/collections/NonEmptyList.scala
@@ -1,28 +1,105 @@
 package com.purplekingdomgames.indigoexts.collections
 
 case class NonEmptyList[A](head: A, tail: List[A]) {
+
+  def ===(other: NonEmptyList[A]): Boolean =
+    NonEmptyList.equality(this, other)
+
+  def length: Int =
+    NonEmptyList.length(this)
+
   def toList: List[A] =
     head :: tail
 
   def foldLeft[Z](acc: Z)(f: (Z, A) => Z): Z =
-    tail.foldLeft(f(acc, head))(f)
+    NonEmptyList.foldLeft(this)(acc)(f)
 
   def reduceLeft(f: (A, A) => A): A =
-    foldLeft(head)(f)
+    NonEmptyList.reduceLeft(this)(f)
 
   def :+(next: A): NonEmptyList[A] =
-    this.copy(tail = tail :+ next)
+    NonEmptyList.append(this)(next)
 
   def ::(first: A): NonEmptyList[A] =
-    this.copy(first, head :: tail)
+    NonEmptyList.cons(this)(first)
 
-  def zipWithIndex: NonEmptyList[(A, Int)] =
-    this.copy(
-      head = (head, 0),
-      tail.zipWithIndex.map(p => (p._1, p._2 + 1))
-    )
+  def ++(other: NonEmptyList[A]): NonEmptyList[A] =
+    NonEmptyList.combine(this)(other)
 
   def map[B](f: A => B): NonEmptyList[B] =
-    NonEmptyList(f(head), tail.map(f))
+    NonEmptyList.map(this)(f)
+
+  def flatMap[B](f: A => NonEmptyList[B]): NonEmptyList[B] =
+    NonEmptyList.flatMap(this)(f)
+
+  def zipWithIndex: NonEmptyList[(A, Int)] =
+    NonEmptyList.zipWithIndex(this)
+
+  def zip[B](other: NonEmptyList[B]): NonEmptyList[(A, B)] =
+    NonEmptyList.zip(this, other)
+
+  def forall(p: A => Boolean): Boolean =
+    NonEmptyList.forall(this)(p)
+
+  def find(p: A => Boolean): Option[A] =
+    NonEmptyList.find(this)(p)
+
+  def exists(p: A => Boolean): Boolean =
+    NonEmptyList.exists(this)(p)
+
+}
+
+object NonEmptyList {
+
+  def apply[A](a: A): NonEmptyList[A] =
+    NonEmptyList(a, List.empty[A])
+
+  def point[A](a: A): NonEmptyList[A] =
+    apply(a)
+
+  def equality[A](a: NonEmptyList[A], b: NonEmptyList[A]): Boolean =
+    a.length == b.length && a.zip(b).forall(as => as._1 == as._2)
+
+  def length[A](fa: NonEmptyList[A]): Int =
+    fa.tail.length + 1
+
+  def map[A, B](fa: NonEmptyList[A])(f: A => B): NonEmptyList[B] =
+    NonEmptyList(f(fa.head), fa.tail.map(f))
+
+  def combine[A](fa: NonEmptyList[A])(fb: NonEmptyList[A]): NonEmptyList[A] =
+    NonEmptyList(fa.head, fa.tail ++ fb.toList)
+
+  def flatten[A](fa: NonEmptyList[NonEmptyList[A]]): NonEmptyList[A] =
+    fa.tail.foldLeft(fa.head)(_ ++ _)
+
+  def flatMap[A, B](fa: NonEmptyList[A])(f: A => NonEmptyList[B]): NonEmptyList[B] =
+    flatten(map(fa)(f))
+
+  def foldLeft[A, Z](fa: NonEmptyList[A])(acc: Z)(f: (Z, A) => Z): Z =
+    fa.tail.foldLeft(f(acc, fa.head))(f)
+
+  def reduceLeft[A](fa: NonEmptyList[A])(f: (A, A) => A): A =
+    foldLeft(fa)(fa.head)(f)
+
+  def append[A](fa: NonEmptyList[A])(next: A): NonEmptyList[A] =
+    NonEmptyList(fa.head, fa.tail :+ next)
+
+  def cons[A](fa: NonEmptyList[A])(first: A): NonEmptyList[A] =
+    NonEmptyList(first, fa.head :: fa.tail)
+
+  def zipWithIndex[A](fa: NonEmptyList[A]): NonEmptyList[(A, Int)] =
+    NonEmptyList((fa.head, 0), fa.tail.zipWithIndex.map(p => (p._1, p._2 + 1)))
+
+  def zip[A, B](fa: NonEmptyList[A], fb: NonEmptyList[B]): NonEmptyList[(A, B)] =
+    NonEmptyList((fa.head, fb.head), fa.tail.zip(fb.tail))
+
+  def forall[A](fa: NonEmptyList[A])(p: A => Boolean): Boolean =
+    fa.toList.forall(p)
+
+  def find[A](fa: NonEmptyList[A])(p: A => Boolean): Option[A] =
+    fa.toList.find(p)
+
+  def exists[A](fa: NonEmptyList[A])(p: A => Boolean): Boolean =
+    fa.toList.exists(p)
 
 }
---
+++ indigo-exts/src/test/scala/com/purplekingdomgames/indigoexts/collections/NonEmptyListSpec.scala
@@ -0,0 +1,70 @@
+package com.purplekingdomgames.indigoexts.collections
+import org.scalatest.{FunSpec, Matchers}
+
+class NonEmptyListSpec extends FunSpec with Matchers {
+
+  describe("NonEmptyList ops") {
+
+    it("should have: equality") {
+      pending
+    }
+
+    it("should have: point") {
+      pending
+    }
+
+    it("should have: map") {
+      pending
+    }
+
+    it("should have: combine") {
+      pending
+    }
+
+    it("should have: flatten") {
+      pending
+    }
+
+    it("should have: flatMap") {
+      pending
+    }
+
+    it("should have: foldLeft") {
+      pending
+    }
+
+    it("should have: reduceLeft") {
+      pending
+    }
+
+    it("should have: append") {
+      pending
+    }
+
+    it("should have: cons") {
+      pending
+    }
+
+    it("should have: zipWithIndex") {
+      pending
+    }
+
+    it("should have: zip") {
+      pending
+    }
+
+    it("should have: forall") {
+      pending
+    }
+
+    it("should have: find") {
+      pending
+    }
+
+    it("should have: exists") {
+      pending
+    }
+
+  }
+
+}
