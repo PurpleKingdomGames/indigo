67fbc104599590b609c96afe9c7314e5b991677d
Dave Smith <david.j.smith@itv.com>
2018-10-21 19:46:08 +0100

    Added empty GameLoopSpec

---
+++ indigo/src/test/scala/indigo/gameengine/GameLoopSpec.scala
@@ -0,0 +1,12 @@
+package indigo.gameengine
+import org.scalatest.{FunSpec, Matchers}
+
+class GameLoopSpec extends FunSpec with Matchers {
+
+  describe("Processing model updates") {
+
+    pending
+
+  }
+
+}
