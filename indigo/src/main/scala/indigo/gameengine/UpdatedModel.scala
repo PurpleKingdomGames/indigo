bd99484a6e7c1d905d049ea1bbccab60739adcf9
Dave Smith <david.j.smith@itv.com>
2018-10-31 17:30:48 +0000

    Added methods for adding inframe and global events to UpdateModel

--- indigo/src/main/scala/indigo/gameengine/UpdatedModel.scala
+++ indigo/src/main/scala/indigo/gameengine/UpdatedModel.scala
@@ -1,7 +1,17 @@
 package indigo.gameengine
 import indigo.gameengine.events.{GlobalEvent, InFrameEvent}
 
-case class UpdatedModel[Model](model: Model, globalEvents: List[GlobalEvent], inFrameEvents: List[InFrameEvent])
+case class UpdatedModel[Model](model: Model, globalEvents: List[GlobalEvent], inFrameEvents: List[InFrameEvent]) {
+  def addGlobalEvents(events: GlobalEvent*): UpdatedModel[Model] =
+    addGlobalEvents(events.toList)
+  def addGlobalEvents(events: List[GlobalEvent]): UpdatedModel[Model] =
+    this.copy(globalEvents = globalEvents ++ events)
+
+  def addInFrameEvents(events: InFrameEvent*): UpdatedModel[Model] =
+    addInFrameEvents(events.toList)
+  def addInFrameEvents(events: List[InFrameEvent]): UpdatedModel[Model] =
+    this.copy(inFrameEvents = inFrameEvents ++ events)
+}
 object UpdatedModel {
   implicit def liftModel[Model](m: Model): UpdatedModel[Model] =
     UpdatedModel(m)
@@ -10,7 +20,17 @@ object UpdatedModel {
     UpdatedModel(model, Nil, Nil)
 }
 
-case class UpdatedViewModel[Model](model: Model, globalEvents: List[GlobalEvent], inFrameEvents: List[InFrameEvent])
+case class UpdatedViewModel[Model](model: Model, globalEvents: List[GlobalEvent], inFrameEvents: List[InFrameEvent]) {
+  def addGlobalEvents(events: GlobalEvent*): UpdatedViewModel[Model] =
+    addGlobalEvents(events.toList)
+  def addGlobalEvents(events: List[GlobalEvent]): UpdatedViewModel[Model] =
+    this.copy(globalEvents = globalEvents ++ events)
+
+  def addInFrameEvents(events: InFrameEvent*): UpdatedViewModel[Model] =
+    addInFrameEvents(events.toList)
+  def addInFrameEvents(events: List[InFrameEvent]): UpdatedViewModel[Model] =
+    this.copy(inFrameEvents = inFrameEvents ++ events)
+}
 object UpdatedViewModel {
   implicit def liftViewModel[Model](m: Model): UpdatedViewModel[Model] =
     UpdatedViewModel(m)
