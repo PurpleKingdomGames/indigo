264028f030ea19ef2088e53726e35cdee7003e18
Dave Smith <david.j.smith@itv.com>
2019-04-03 23:10:00 +0100

    First CycleTest finally passing

--- indigo/src/test/scala/indigo/gameengine/scenegraph/animation/CycleTests.scala
+++ indigo/src/test/scala/indigo/gameengine/scenegraph/animation/CycleTests.scala
@@ -12,14 +12,14 @@ object CycleTests extends TestSuite {
   val frame1: Frame =
     Frame(Rectangle(Point(0, 0), Point(10, 10)), 10)
 
-  // val frame2: Frame =
-  //   Frame(Rectangle(0, 0, 20, 10), 10)
+  val frame2: Frame =
+    Frame(Rectangle(0, 0, 20, 10), 10)
 
-  // val frame3: Frame =
-  //   Frame(Rectangle(0, 0, 30, 10), 10)
+  val frame3: Frame =
+    Frame(Rectangle(0, 0, 30, 10), 10)
 
-  // val cycle: Cycle =
-  //   Cycle.create("test", NonEmptyList(frame1))
+  val cycle: Cycle =
+    Cycle.create("test", NonEmptyList(frame1))
 
   val tests: Tests =
     Tests {
@@ -27,8 +27,7 @@ object CycleTests extends TestSuite {
       "Cycle tests" - {
 
         "adding a frame" - {
-          //cycle.addFrame(frame2) === Cycle.create("test", NonEmptyList(frame1, frame2)) ==> true
-          1 ==> 2
+          cycle.addFrame(frame2) === Cycle.create("test", NonEmptyList(frame1, frame2)) ==> true
         }
 
       }
