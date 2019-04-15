da16f9568b4853b564e3ab597ba73fb5e9739432
Dave Smith <david.j.smith@itv.com>
2019-04-15 21:00:46 +0100

    Moved GameWithSubSystems to separate class

--- indigo-exts/src/main/scala/indigoexts/entry/IndigoGameBasic.scala
+++ indigo-exts/src/main/scala/indigoexts/entry/IndigoGameBasic.scala
@@ -69,28 +69,3 @@ trait IndigoGameBasic[StartupData, Model, ViewModel] {
     indigoGame.start()
 
 }
-
-final class GameWithSubSystems[Model](val model: Model, val subSystemsRegister: SubSystemsRegister)
-object GameWithSubSystems {
-  import indigo.abstractions.syntax._
-
-  def apply[Model](model: Model, subSystemsRegister: SubSystemsRegister): GameWithSubSystems[Model] =
-    new GameWithSubSystems[Model](model, subSystemsRegister)
-
-  def update[Model](
-      modelUpdate: (GameTime, Model, Dice) => GlobalEvent => Outcome[Model]
-  )(gameTime: GameTime, model: GameWithSubSystems[Model], dice: Dice): GlobalEvent => Outcome[GameWithSubSystems[Model]] =
-    e =>
-      (modelUpdate(gameTime, model.model, dice)(e), model.subSystemsRegister.update(gameTime, dice)(e))
-        .map2((m, s) => GameWithSubSystems(m, s))
-
-  def updateViewModel[Model, ViewModel](
-      viewModelUpdate: (GameTime, Model, ViewModel, FrameInputEvents, Dice) => Outcome[ViewModel]
-  )(gameTime: GameTime, model: GameWithSubSystems[Model], viewModel: ViewModel, frameInputEvents: FrameInputEvents, dice: Dice): Outcome[ViewModel] =
-    viewModelUpdate(gameTime, model.model, viewModel, frameInputEvents, dice)
-
-  def present[Model, ViewModel](
-      viewPresent: (GameTime, Model, ViewModel, FrameInputEvents) => SceneUpdateFragment
-  )(gameTime: GameTime, model: GameWithSubSystems[Model], viewModel: ViewModel, frameInputEvents: FrameInputEvents): SceneUpdateFragment =
-    viewPresent(gameTime, model.model, viewModel, frameInputEvents) |+| model.subSystemsRegister.render(gameTime)
-}
