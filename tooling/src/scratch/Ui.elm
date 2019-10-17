303bf1091e3c1928d5ee079d82f9cd85d626656d
Dave Smith <david.j.smith@itv.com>
2019-10-17 22:04:17 +0100

    Elm Ui Examples

---
+++ tooling/src/scratch/Ui.elm
@@ -0,0 +1,31 @@
+module Main exposing (main, myElement, myRowOfStuff)
+
+import Element exposing (Element, alignRight, centerY, el, fill, padding, rgb255, row, spacing, text, width)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
+
+
+main =
+    Element.layout []
+        myRowOfStuff
+
+
+myRowOfStuff : Element msg
+myRowOfStuff =
+    row [ width fill, centerY, spacing 30 ]
+        [ myElement
+        , myElement
+        , el [ alignRight ] myElement
+        ]
+
+
+myElement : Element msg
+myElement =
+    el
+        [ Background.color (rgb255 240 0 245)
+        , Font.color (rgb255 255 255 255)
+        , Border.rounded 3
+        , padding 30
+        ]
+        (text "stylish!")
---
+++ tooling/src/scratch/UiMain.elm
@@ -0,0 +1,65 @@
+module Main exposing (..)
+
+import Browser
+import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
+import Element.Input as Input
+import Html exposing (Html)
+
+
+main =
+    Browser.sandbox { init = initialModel, update = update, view = view }
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+type alias Model =
+    { amount : Int }
+
+
+initialModel : Model
+initialModel =
+    Model 0
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            { model | amount = model.amount + 1 }
+
+        Decrement ->
+            { model | amount = model.amount - 1 }
+
+
+view : Model -> Html.Html Msg
+view model =
+    Element.layout [] (counteElements model)
+
+
+blue =
+    Element.rgb255 238 238 238
+
+
+myButton : String -> Msg -> Element Msg
+myButton label msg =
+    Input.button
+        [ Background.color blue
+        ]
+        { onPress = Just msg
+        , label = text label
+        }
+
+
+counteElements : Model -> Element Msg
+counteElements model =
+    row [ spacing 30, padding 10 ]
+        [ myButton "Increment" Increment
+        , text <| String.fromInt model.amount
+        , myButton "Decrement" Decrement
+        ]
