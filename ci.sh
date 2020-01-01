beb7e91a14ff9c6bb6054313e493a2f8ed0111cc
Dave Smith <david.j.smith@itv.com>
2020-01-01 23:43:23 +0000

    Simple CI test

---
+++ ci.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Build the core...
+# Indigo
+cd indigo
+bash ci.sh
+cd ..
+
+# Check the core looks ok...
+# Demos
+# Examples
+
+# Supporting tooling...
+# JS API
+# Tooling
+
+# Documentation...
+# Docs site
+
