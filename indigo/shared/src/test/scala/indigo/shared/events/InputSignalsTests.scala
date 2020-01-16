fa916a660e0ca37421fa4601422baf2d5fed9d17
Dave Smith <david.j.smith@itv.com>
2020-01-16 10:32:50 +0000

    WIP: Added red placeholder tests for InputSignals

--- indigo/shared/src/test/scala/indigo/shared/events/InputSignalsTests.scala
+++ indigo/shared/src/test/scala/indigo/shared/events/InputSignalsTests.scala
@@ -22,6 +22,37 @@ object InputSignalsTests extends TestSuite {
         signals.mouse.wasMouseClickedWithin(bounds) ==> false
       }
 
+      "Mouse signals" - {
+
+        "position" - { 1 ==> 2 }
+        "leftMouseIsDown" - { 1 ==> 2 }
+        "mousePressed" - { 1 ==> 2 }
+        "mouseReleased" - { 1 ==> 2 }
+        "mouseClicked" - { 1 ==> 2 }
+        "mouseClickAt" - { 1 ==> 2 }
+        "mouseUpAt" - { 1 ==> 2 }
+        "mouseDownAt" - { 1 ==> 2 }
+        "wasMouseClickedAt" - { 1 ==> 2 }
+        "wasMouseUpAt" - { 1 ==> 2 }
+        "wasMouseDownAt" - { 1 ==> 2 }
+        "wasMousePositionAt" - { 1 ==> 2 }
+        "wasMouseClickedWithin" - { 1 ==> 2 }
+        "wasMouseUpWithin" - { 1 ==> 2 }
+        "wasMouseDownWithin" - { 1 ==> 2 }
+        "wasMousePositionWithin" - { 1 ==> 2 }
+
+      }
+
+      "Keyboard Signals" - {
+
+        "keys up" - { 1 ==> 2 }
+        "keys down" - { 1 ==> 2 }
+        "keys are down" - { 1 ==> 2 }
+        "keys are up" - { 1 ==> 2 }
+        "last key held down" - { 1 ==> 2 }
+
+      }
+
     }
 
 }
