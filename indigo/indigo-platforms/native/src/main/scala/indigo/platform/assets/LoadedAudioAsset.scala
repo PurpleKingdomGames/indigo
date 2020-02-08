6ef92da2433524ffe333b37dfbf7bd6877dd6bf5
Dave Smith <david.j.smith@itv.com>
2020-02-08 20:24:30 +0000

    Removed native placeholder

--- indigo/indigo-platforms/native/src/main/scala/indigo/platform/assets/LoadedAudioAsset.scala
+++
@@ -1,3 +0,0 @@
-package indigo.platform.assets
-
-final class LoadedAudioAsset(val name: AssetName, val data: AssetCollection.AudioDataFormat)
--- indigo/indigo-platforms/native/src/main/scala/indigo/platform/assets/LoadedTextAsset.scala
+++
@@ -1,3 +0,0 @@
-package indigo.platform.assets
-
-final class LoadedTextAsset(val name: AssetName, val data: AssetCollection.TextDataFormat)
