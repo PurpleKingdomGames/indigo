e7e016bdc2f249e2d205a8c3f00f5c7d410a6aab
David North <bigchimp2005+github@gmail.com>
2020-02-17 07:47:15 +0000

    Adds input mapper to Pirate JS demo

---
+++ demos/pirate-js/src/js/pirate.inputmapper.js
@@ -0,0 +1,30 @@
+class InputMapper {
+    apply = function(inputState) {
+      return checkGamepadInput(inputState).merge(checkKeyboardInput(inputState));
+    }
+
+    checkGamepadInput = function(inputState) {
+        if (inputState.gamepad.analog.left.x < -0.5 || inputState.gamepad.dpad.left)
+            return PirateState.MoveLeft;
+        else if (inputState.gamepad.analog.left.x > 0.5 || inputState.gamepad.dpad.right)
+            return PirateState.MoveRight;
+        else
+            return PirateState.Idle;
+    }
+
+    checkKeyboardInput = function(inputState) {
+        switch (inputState.keyboard.lastKeyHeldDown) {
+            case null:
+                return PirateState.Idle
+
+            case Some(Keys.LEFT_ARROW):
+                return PirateState.MoveLeft
+
+            case Some(Keys.RIGHT_ARROW):
+                return PirateState.MoveRight;
+
+            default:
+                return PirateState.Idle
+        }
+    }
+}
