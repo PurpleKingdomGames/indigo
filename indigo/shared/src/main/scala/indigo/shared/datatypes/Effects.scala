80a3380a154343fb1a09394ae349382ef3363685
Dave Smith <david.j.smith@itv.com>
2020-02-20 23:52:48 +0000

    WIP: New Effect values need to be in DisplayObject

--- indigo/shared/src/main/scala/indigo/shared/datatypes/Effects.scala
+++ indigo/shared/src/main/scala/indigo/shared/datatypes/Effects.scala
@@ -1,29 +1,133 @@
 package indigo.shared.datatypes
 
+final class Effects(
+    val tint: RGBA,
+    val colorOverlay: RGBA,
+    val gradiantOverlay: LinearGradiantOverlay,
+    val outerBorder: ColorAmount,
+    val innerBorder: ColorAmount,
+    val outerGlow: ColorAmount,
+    val innerGlow: ColorAmount,
+    val blur: Double,
+    val alpha: Double,
+    val flip: Flip
+) {
+  def withTint(newValue: RGBA): Effects =
+    Effects(newValue, colorOverlay, gradiantOverlay, outerBorder, innerBorder, outerGlow, innerGlow, blur, alpha, flip)
 
-final class Effects(val alpha: Double, val tint: RGBA, val flip: Flip) {
-  def withAlpha(newAlpha: Double): Effects =
-    Effects(newAlpha, tint, flip)
+  def withColorOverlay(newValue: RGBA): Effects =
+    Effects(tint, newValue, gradiantOverlay, outerBorder, innerBorder, outerGlow, innerGlow, blur, alpha, flip)
 
-  def withTint(newTint: RGBA): Effects =
-    Effects(alpha, newTint, flip)
+  def withGradiantOverlay(newValue: LinearGradiantOverlay): Effects =
+    Effects(tint, colorOverlay, newValue, outerBorder, innerBorder, outerGlow, innerGlow, blur, alpha, flip)
 
-  def withFlip(newFlip: Flip): Effects =
-    Effects(alpha, tint, newFlip)
+  def withOuterBorder(newValue: ColorAmount): Effects =
+    Effects(tint, colorOverlay, gradiantOverlay, newValue, innerBorder, outerGlow, innerGlow, blur, alpha, flip)
+
+  def withInnerBorder(newValue: ColorAmount): Effects =
+    Effects(tint, colorOverlay, gradiantOverlay, outerBorder, newValue, outerGlow, innerGlow, blur, alpha, flip)
+
+  def withOuterGlow(newValue: ColorAmount): Effects =
+    Effects(tint, colorOverlay, gradiantOverlay, outerBorder, innerBorder, newValue, innerGlow, blur, alpha, flip)
+
+  def withInnerGlow(newValue: ColorAmount): Effects =
+    Effects(tint, colorOverlay, gradiantOverlay, outerBorder, innerBorder, outerGlow, newValue, blur, alpha, flip)
+
+  def withBlur(newValue: Double): Effects =
+    Effects(tint, colorOverlay, gradiantOverlay, outerBorder, innerBorder, outerGlow, innerGlow, newValue, alpha, flip)
+
+  def withAlpha(newValue: Double): Effects =
+    Effects(tint, colorOverlay, gradiantOverlay, outerBorder, innerBorder, outerGlow, innerGlow, blur, newValue, flip)
+
+  def withFlip(newValue: Flip): Effects =
+    Effects(tint, colorOverlay, gradiantOverlay, outerBorder, innerBorder, outerGlow, innerGlow, blur, alpha, newValue)
 
   def hash: String =
-    alpha.toString() + tint.hash + flip.hash
+    tint.hash +
+      colorOverlay.hash +
+      gradiantOverlay.hash +
+      outerBorder.hash +
+      innerBorder.hash +
+      outerGlow.hash +
+      innerGlow.hash +
+      blur.toString() +
+      alpha.toString() +
+      flip.hash
 }
 object Effects {
   val default: Effects = Effects(
-    alpha = 1.0,
     tint = RGBA.None,
+    colorOverlay = RGBA.Zero,
+    gradiantOverlay = LinearGradiantOverlay.default,
+    outerBorder = ColorAmount.default,
+    innerBorder = ColorAmount.default,
+    outerGlow = ColorAmount.default,
+    innerGlow = ColorAmount.default,
+    blur = 0.0,
+    alpha = 1.0,
     flip = Flip(
       horizontal = false,
       vertical = false
     )
   )
 
-  def apply(alpha: Double, tint: RGBA, flip: Flip): Effects =
-    new Effects(alpha, tint, flip)
+  def apply(
+      tint: RGBA,
+      colorOverlay: RGBA,
+      gradiantOverlay: LinearGradiantOverlay,
+      outerBorder: ColorAmount,
+      innerBorder: ColorAmount,
+      outerGlow: ColorAmount,
+      innerGlow: ColorAmount,
+      blur: Double,
+      alpha: Double,
+      flip: Flip
+  ): Effects =
+    new Effects(
+      tint,
+      colorOverlay,
+      gradiantOverlay,
+      outerBorder,
+      innerBorder,
+      outerGlow,
+      innerGlow,
+      blur,
+      alpha,
+      flip
+    )
+}
+
+final class LinearGradiantOverlay(
+    val fromPoint: Point,
+    val fromColor: RGBA,
+    val toPoint: Point,
+    val toColor: RGBA
+) {
+
+  def hash: String =
+    fromPoint.x.toString +
+      fromPoint.y.toString +
+      fromColor.hash +
+      toPoint.x.toString +
+      toPoint.y.toString +
+      toColor.hash
+}
+object LinearGradiantOverlay {
+  def apply(fromPoint: Point, fromColor: RGBA, toPoint: Point, toColor: RGBA): LinearGradiantOverlay =
+    new LinearGradiantOverlay(fromPoint, fromColor, toPoint, toColor)
+
+  val default: LinearGradiantOverlay =
+    LinearGradiantOverlay(Point.zero, RGBA.Zero, Point.zero, RGBA.Zero)
+}
+
+final class ColorAmount(val color: RGBA, val amount: Double) {
+  def hash: String =
+    color.hash + amount.toString()
+}
+object ColorAmount {
+  def apply(color: RGBA, amount: Double): ColorAmount =
+    new ColorAmount(color, amount)
+
+  val default: ColorAmount =
+    ColorAmount(RGBA.Zero, 0.0)
 }
---
+++ indigo/shared/src/main/scala/indigo/shared/datatypes/RGB.scala
@@ -0,0 +1,70 @@
+package indigo.shared.datatypes
+
+import indigo.shared.AsString
+import indigo.shared.EqualTo
+
+final class RGB(val r: Double, val g: Double, val b: Double) {
+  def +(other: RGB): RGB =
+    RGB.combine(this, other)
+
+  def withRed(newRed: Double): RGB =
+    RGB(newRed, g, b)
+
+  def withGreen(newGreen: Double): RGB =
+    RGB(r, newGreen, b)
+
+  def withBlue(newBlue: Double): RGB =
+    RGB(r, g, newBlue)
+
+  def hash: String =
+    r.toString() + g.toString() + b.toString()
+
+  override def toString: String =
+    implicitly[AsString[RGB]].show(this)
+}
+object RGB {
+
+  def apply(red: Double, green: Double, blue: Double): RGB =
+    new RGB(red, green, blue)
+
+  implicit val show: AsString[RGB] = {
+    val ev = implicitly[AsString[Double]]
+
+    AsString.create { v =>
+      s"RGB(${ev.show(v.r)}, ${ev.show(v.g)}, ${ev.show(v.b)})"
+    }
+  }
+
+  implicit val eq: EqualTo[RGB] = {
+    val ev = implicitly[EqualTo[Double]]
+
+    EqualTo.create { (a, b) =>
+      ev.equal(a.r, b.r) && ev.equal(a.g, b.g) && ev.equal(a.b, b.b)
+    }
+  }
+
+  val Red: RGB     = RGB(1, 0, 0)
+  val Green: RGB   = RGB(0, 1, 0)
+  val Blue: RGB    = RGB(0, 0, 1)
+  val Yellow: RGB  = RGB(1, 1, 0)
+  val Magenta: RGB = RGB(1, 0, 1)
+  val Cyan: RGB    = RGB(0, 1, 1)
+  val White: RGB   = RGB(1, 1, 1)
+  val Black: RGB   = RGB(0, 0, 0)
+
+  val Normal: RGB = White
+  val None: RGB   = White
+  val Zero: RGB   = RGB(0, 0, 0)
+
+  def combine(a: RGB, b: RGB): RGB =
+    (a, b) match {
+      case (RGB.None, x) =>
+        x
+      case (x, RGB.None) =>
+        x
+      case (x, y) =>
+        RGB(x.r + y.r, x.g + y.g, x.b + y.b)
+    }
+
+}
+
