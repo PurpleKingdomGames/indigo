0f89dae6a4709160b1574d42bdf2558018803195
Dave Smith <david.j.smith@itv.com>
2020-02-25 21:43:41 +0000

    GameViewport: center (Int, Int) -> Point

--- indigo/shared/src/main/scala/indigo/shared/config/GameViewport.scala
+++ indigo/shared/src/main/scala/indigo/shared/config/GameViewport.scala
@@ -1,11 +1,12 @@
 package indigo.shared.config
 
 import indigo.shared.datatypes.Rectangle
+import indigo.shared.datatypes.Point
 
 final case class GameViewport(width: Int, height: Int) {
   val horizontalMiddle: Int = width / 2
   val verticalMiddle: Int   = height / 2
-  val center: (Int, Int)    = (horizontalMiddle, verticalMiddle)
+  val center: Point         = Point(horizontalMiddle, verticalMiddle)
 
   def giveDimensions(magnification: Int): Rectangle =
     Rectangle(0, 0, width / magnification, height / magnification)
