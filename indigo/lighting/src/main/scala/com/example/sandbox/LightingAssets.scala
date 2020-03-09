5728386cf9bf2cc95fafdd5f9687ea4cc7919c71
Dave Smith <david.j.smith@itv.com>
2020-03-09 21:55:42 +0000

    Tag Lighting assets

--- indigo/lighting/src/main/scala/com/example/sandbox/LightingAssets.scala
+++ indigo/lighting/src/main/scala/com/example/sandbox/LightingAssets.scala
@@ -18,12 +18,14 @@ object LightingAssets {
       Some(junctionBoxSpecular)
     )
 
+  val tag = AssetTag("atlas1")
+
   def assets: Set[AssetType] =
     Set(
-      AssetType.Image(junctionBoxAlbedo, AssetPath("assets/" + junctionBoxAlbedo.value + ".png")),
-      AssetType.Image(junctionBoxEmission, AssetPath("assets/" + junctionBoxEmission.value + ".png")),
-      AssetType.Image(junctionBoxNormal, AssetPath("assets/" + junctionBoxNormal.value + ".png")),
-      AssetType.Image(junctionBoxSpecular, AssetPath("assets/" + junctionBoxSpecular.value + ".png"))
+      AssetType.Image(junctionBoxAlbedo, AssetPath("assets/" + junctionBoxAlbedo.value + ".png")).withTag(tag),
+      AssetType.Image(junctionBoxEmission, AssetPath("assets/" + junctionBoxEmission.value + ".png")).withTag(tag),
+      AssetType.Image(junctionBoxNormal, AssetPath("assets/" + junctionBoxNormal.value + ".png")).withTag(tag),
+      AssetType.Image(junctionBoxSpecular, AssetPath("assets/" + junctionBoxSpecular.value + ".png")).withTag(tag)
     )
 
 }
