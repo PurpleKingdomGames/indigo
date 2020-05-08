f69d173abfa21a1ee1eafcf009b85cbf50666ba3
Dave Smith <david.j.smith@itv.com>
2020-05-08 21:45:06 +0100

    Snake converted to a simple Mill build

--- demos/snake/snake/src/snake/scenes/GameOverScene.scala
+++ demos/snake/snake/src/snake/scenes/GameOverScene.scala
@@ -52,7 +52,7 @@ object GameOverScene extends Scene[SnakeGameModel, SnakeViewModel] {
     SceneUpdateFragment.empty
       .addUiLayerNodes(
         Text("Game Over!", horizontalCenter, verticalMiddle - 20, 1, GameAssets.fontKey).alignCenter,
-        Text(s"You scored: $pointsScored pts!", horizontalCenter, verticalMiddle - 5, 1, GameAssets.fontKey).alignCenter,
+        Text(s"You scored: ${pointsScored.toString()} pts!", horizontalCenter, verticalMiddle - 5, 1, GameAssets.fontKey).alignCenter,
         Text("(hit space to restart)", horizontalCenter, 220, 1, GameAssets.fontKey).alignCenter
       )
   }
