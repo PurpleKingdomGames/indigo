1f6516ef762dda100ab82eb35a7fb792271793cb
Dave Smith <david.j.smith@itv.com>
2020-05-29 01:06:04 +0100

    Updated AdvancedGameConfigDelegate to support rendering tech

--- jsapi/indigojs/src/indigojs/delegates/config/AdvancedGameConfigDelegate.scala
+++ jsapi/indigojs/src/indigojs/delegates/config/AdvancedGameConfigDelegate.scala
@@ -3,16 +3,20 @@ package indigojs.delegates.config
 import scala.scalajs.js.annotation._
 
 import indigo.shared.config.AdvancedGameConfig
+import indigo.shared.config.RenderingTechnology
 
 @SuppressWarnings(Array("org.wartremover.warts.Any"))
 @JSExportTopLevel("AdvancedGameConfig")
 final class AdvancedGameConfigDelegate(
+    _renderingTechnology: String,
     _antiAliasing: Boolean,
     _batchSize: Int,
     _disableSkipModelUpdates: Boolean,
     _disableSkipViewUpdates: Boolean
 ) {
 
+  @JSExport
+  val renderingTechnology = _renderingTechnology
   @JSExport
   val antiAliasing = _antiAliasing
   @JSExport
@@ -24,6 +28,12 @@ final class AdvancedGameConfigDelegate(
 
   def toInternal: AdvancedGameConfig =
     AdvancedGameConfig(
+      renderingTechnology.toLowerCase match {
+        case "webgl1"             => RenderingTechnology.WebGL1
+        case "webgl2"             => RenderingTechnology.WebGL2
+        case "webgl2withfallback" => RenderingTechnology.WebGL2WithFallback
+        case _                    => RenderingTechnology.WebGL2WithFallback
+      },
       antiAliasing,
       batchSize,
       disableSkipModelUpdates,
@@ -34,6 +44,7 @@ final class AdvancedGameConfigDelegate(
 object AdvancedGameConfigDelegate {
   val default: AdvancedGameConfigDelegate =
     new AdvancedGameConfigDelegate(
+      _renderingTechnology = "WebGL2WithFallback",
       _antiAliasing = false,
       _batchSize = 256,
       _disableSkipModelUpdates = false,
