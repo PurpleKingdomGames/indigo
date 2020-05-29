d993b349c2099fd6ed1f57940c49ea5fc407f265
Dave Smith <david.j.smith@itv.com>
2020-05-29 14:52:51 +0100

    Safari compatible WebGL detection

--- indigo/indigo-platforms/js/src/main/scala/indigo/platform/renderer/RendererInitialiser.scala
+++ indigo/indigo-platforms/js/src/main/scala/indigo/platform/renderer/RendererInitialiser.scala
@@ -34,6 +34,9 @@ final class RendererInitialiser(renderingTechnology: RenderingTechnology) {
     r
   }
 
+  def createCanvas(width: Int, height: Int, parent: Element): html.Canvas =
+    createNamedCanvas(width, height, "indigo", Some(parent))
+
   @SuppressWarnings(
     Array(
       "org.wartremover.warts.NonUnitStatements",
@@ -43,13 +46,20 @@ final class RendererInitialiser(renderingTechnology: RenderingTechnology) {
       "org.wartremover.warts.Var"
     )
   )
-  def createCanvas(width: Int, height: Int, parent: Element): html.Canvas = {
-    var canvas: html.Canvas = dom.document.getElementById("indigo").asInstanceOf[html.Canvas]
+  def createNamedCanvas(width: Int, height: Int, name: String, appendToParent: Option[Element]): html.Canvas = {
+    var canvas: html.Canvas = dom.document.getElementById(name).asInstanceOf[html.Canvas]
 
     if (canvas == null) {
       canvas = dom.document.createElement("canvas").asInstanceOf[html.Canvas]
-      parent.appendChild(canvas)
-      canvas.id = "indigo"
+
+      appendToParent match {
+        case Some(parent) =>
+          parent.appendChild(canvas)
+        case None =>
+          ()
+      }
+
+      canvas.id = name
       canvas.width = width
       canvas.height = height
     }
@@ -81,63 +91,94 @@ final class RendererInitialiser(renderingTechnology: RenderingTechnology) {
     )
   )
   private def getContext(canvas: html.Canvas, antiAliasing: Boolean): (WebGLRenderingContext, RenderingTechnology) = {
-
     val args =
       Dynamic.literal("premultipliedAlpha" -> false, "alpha" -> false, "antialias" -> antiAliasing)
 
+    val tech = chooseRenderingTechnology(renderingTechnology, args)
+
     def useWebGL1(): (WebGLRenderingContext, RenderingTechnology) = {
       val gl = (canvas.getContext("webgl", args) || canvas.getContext("experimental-webgl", args)).asInstanceOf[raw.WebGLRenderingContext]
-
-      if (gl == null)
-        throw new Exception("This browser does not support WebGL 1.0")
-      else {
-        IndigoLogger.info("Using WebGL 1.0")
-        (gl, RenderingTechnology.WebGL1)
-      }
+      (gl, RenderingTechnology.WebGL1)
     }
 
-    renderingTechnology match {
-      case RenderingTechnology.WebGL1 =>
-        useWebGL1()
-
-      case RenderingTechnology.WebGL2 =>
-        val gl2 = (canvas.getContext("webgl2", args)).asInstanceOf[raw.WebGLRenderingContext]
-
-        if (gl2 == null)
-          throw new Exception("This browser does not support WebGL 2.0")
-        else if (!isWebGL2ReallySupported(gl2))
-          throw new Exception("This browser claims to support WebGL 2.0, but is not fully implemented.")
-        else {
-          IndigoLogger.info("Using WebGL 2.0")
-          (gl2, RenderingTechnology.WebGL2)
-        }
-
-      case RenderingTechnology.WebGL2WithFallback =>
-        var gl2 = (canvas.getContext("webgl2", args)).asInstanceOf[raw.WebGLRenderingContext]
-
-        if (gl2 == null) {
-          IndigoLogger.info("This browser does not support WebGL 2.0, trying WebGL 1.0")
-          useWebGL1()
-        } else if (!isWebGL2ReallySupported(gl2)) {
-          IndigoLogger.info("This browser claims to support WebGL 2.0, but is not fully implemented. Trying WebGL 1.0")
-          gl2 = null
-          useWebGL1()
-        } else {
-          IndigoLogger.info("Using WebGL 2.0")
-          (gl2, RenderingTechnology.WebGL2)
-        }
+    def useWebGL2(): (WebGLRenderingContext, RenderingTechnology) = {
+      val gl2 = (canvas.getContext("webgl2", args)).asInstanceOf[raw.WebGLRenderingContext]
+      (gl2, RenderingTechnology.WebGL2)
     }
 
+    tech match {
+      case Left(_)  => useWebGL1()
+      case Right(_) => useWebGL2()
+    }
   }
 
   @SuppressWarnings(
     Array(
       "org.wartremover.warts.AsInstanceOf",
       "org.wartremover.warts.IsInstanceOf",
-      "org.wartremover.warts.Throw"
+      "org.wartremover.warts.Equals",
+      "org.wartremover.warts.Null",
+      "org.wartremover.warts.Throw",
+      "org.wartremover.warts.Var"
     )
   )
-  def isWebGL2ReallySupported(gl2: raw.WebGLRenderingContext): Boolean = {
+  private def chooseRenderingTechnology(usersPreference: RenderingTechnology, args: Dynamic): Either[RenderingTechnology.WebGL1.type, RenderingTechnology.WebGL2.type] = {
+    val tempCanvas = createNamedCanvas(1, 1, "indigowebgl2test", None)
+
+    def tryWebGL1(prefix: String): Either[RenderingTechnology.WebGL1.type, RenderingTechnology.WebGL2.type] = {
+      val gl = (tempCanvas.getContext("webgl", args) || tempCanvas.getContext("experimental-webgl", args)).asInstanceOf[raw.WebGLRenderingContext]
+
+      if (gl == null)
+        throw new Exception(prefix + "This browser does not support WebGL 1.0.")
+      else {
+        IndigoLogger.info("Using WebGL 1.0.")
+        Left(RenderingTechnology.WebGL1)
+      }
+    }
+
+    usersPreference match {
+      case RenderingTechnology.WebGL1 =>
+        tryWebGL1("WebGL 1.0 required by indigo game. ")
+
+      case RenderingTechnology.WebGL2 =>
+        val gl2 = (tempCanvas.getContext("webgl2", args)).asInstanceOf[raw.WebGLRenderingContext]
+
+        if (gl2 == null)
+          throw new Exception("WebGL 2.0 required by indigo game. This browser does not support WebGL 2.0.")
+        else if (!isWebGL2ReallySupported(gl2))
+          throw new Exception("WebGL 2.0 required by indigo game. This browser claims to support WebGL 2.0, but is not fully implemented.")
+        else {
+          IndigoLogger.info("Using WebGL 2.0")
+          Right(RenderingTechnology.WebGL2)
+        }
+
+      case RenderingTechnology.WebGL2WithFallback =>
+        var gl2 = (tempCanvas.getContext("webgl2", args)).asInstanceOf[raw.WebGLRenderingContext]
+
+        if (gl2 == null) {
+          IndigoLogger.info("This browser does not support WebGL 2.0, trying WebGL 1.0.")
+          tryWebGL1("")
+        } else if (!isWebGL2ReallySupported(gl2)) {
+          IndigoLogger.info("This browser claims to support WebGL 2.0, but it is not fully implemented. Trying WebGL 1.0.")
+          gl2 = null
+          tryWebGL1("")
+        } else {
+          IndigoLogger.info("Using WebGL 2.0.")
+          Right(RenderingTechnology.WebGL2)
+        }
+    }
+  }
+
+  @SuppressWarnings(
+    Array(
+      "org.wartremover.warts.AsInstanceOf",
+      "org.wartremover.warts.IsInstanceOf",
+      "org.wartremover.warts.Throw",
+      "org.wartremover.warts.Null",
+      "org.wartremover.warts.Equals"
+    )
+  )
+  private def isWebGL2ReallySupported(gl2: raw.WebGLRenderingContext): Boolean = {
 
     def testWebGL2Compatibility(param: Int, min: Int): Boolean =
       try if (gl2.getParameter(param).isInstanceOf[Int]) {
@@ -159,7 +200,7 @@ final class RendererInitialiser(renderingTechnology: RenderingTechnology) {
       (indigo.facades.WebGL2RenderingContext.MAX_COMBINED_UNIFORM_BLOCKS, 24)
     )
 
-    tests.forall(t => testWebGL2Compatibility(t._1, t._2))
+    gl2 != null && tests.forall(t => testWebGL2Compatibility(t._1, t._2))
   }
 
 }
