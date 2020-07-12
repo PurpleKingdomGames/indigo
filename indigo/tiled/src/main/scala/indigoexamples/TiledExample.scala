7ac45e5ad2d9c8f9803fdd0b54bad5333d8eaaf0
Dave Smith <david.j.smith@itv.com>
2020-07-13 00:27:00 +0100

    Tile example working

--- indigo/tiled/src/main/scala/indigoexamples/TiledExample.scala
+++ indigo/tiled/src/main/scala/indigoexamples/TiledExample.scala
@@ -1,45 +1,54 @@
 package indigoexamples
 
 import indigo._
+import indigo.json.Json
 
 import scala.scalajs.js.annotation._
 
 @JSExportTopLevel("IndigoGame")
-object TiledExample extends IndigoSandbox[Unit, Unit] {
+object TiledExample extends IndigoSandbox[Group, Unit] {
 
-  val config: GameConfig = defaultGameConfig
+  val config: GameConfig =
+    defaultGameConfig
+      .withViewport(19 * 32, 11 * 32)
 
-  val assetName: AssetName = AssetName("graphics")
+  val terrianData: AssetName  = AssetName("terrain-data")
+  val terrianImage: AssetName = AssetName("terrain-image")
 
-  val assets: Set[AssetType] = Set(AssetType.Image(assetName, AssetPath("assets/graphics.png")))
+  val assets: Set[AssetType] =
+    Set(
+      AssetType.Text(terrianData, AssetPath("assets/terrain.json")),
+      AssetType.Image(terrianImage, AssetPath("assets/terrain.png"))
+    )
 
   val fonts: Set[FontInfo] = Set()
 
   val animations: Set[Animation] = Set()
 
-  def setup(assetCollection: AssetCollection, dice: Dice): Startup[StartupErrors, Unit] =
-    Startup.Success(())
+  def setup(assetCollection: AssetCollection, dice: Dice): Startup[StartupErrors, Group] = {
 
-  def initialModel(startupData: Unit): Unit =
+    val maybeTiledMap = for {
+      j <- assetCollection.findTextDataByName(terrianData)
+      t <- Json.tiledMapFromJson(j)
+      g <- t.toGroup(terrianImage)
+    } yield g
+
+    maybeTiledMap match {
+      case None =>
+        Startup.Failure.withErrors("Could not generate TiledMap from data.")
+
+      case Some(tiledMap) =>
+        Startup.Success(tiledMap)
+    }
+
+  }
+
+  def initialModel(startupData: Group): Unit =
     ()
 
-  def updateModel(context: FrameContext[Unit], model: Unit): GlobalEvent => Outcome[Unit] =
+  def updateModel(context: FrameContext[Group], model: Unit): GlobalEvent => Outcome[Unit] =
     _ => Outcome(model)
 
-  def present(context: FrameContext[Unit], model: Unit): SceneUpdateFragment =
-    SceneUpdateFragment.empty.addGameLayerNodes(
-      Group(
-        Group(
-          Graphic(0, 0, 256, 256, 1, Material.Textured(assetName)).moveTo(64, 10).moveBy(-50, -50),
-          Graphic(0, 0, 32, 32, 1, Material.Textured(assetName)).withCrop(32, 0, 32, 32).moveBy(-50, -50),
-          Graphic(0, 0, 128, 128, 1, Material.Textured(assetName))
-            .moveTo(0, 128)
-            .withCrop(128, 0, 128, 128)
-            .withTint(0, 1, 1)
-            .withAlpha(0.5)
-            .moveBy(-50, -50)
-        ).moveTo(100, 100),
-        // Graphic(0, 0, 256, 256, 1, Material.Textured(assetName)).moveTo(64, 10).moveBy(-50, -50)
-      )
-    )
+  def present(context: FrameContext[Group], model: Unit): SceneUpdateFragment =
+    SceneUpdateFragment(context.startUpData)
 }
