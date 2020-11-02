5e31378ecfd8f027d1d3f6e1fddfd4264dfb78b6
Dave Smith <davesmith.00000@gmail.com>
2020-11-02 22:18:02 +0000

    Fixed scaladoc typos

--- indigo/shared/src/main/scala/indigo/shared/FrameContext.scala
+++ indigo/shared/src/main/scala/indigo/shared/FrameContext.scala
@@ -15,9 +15,9 @@ import indigo.shared.time.Seconds
   * In includes values that are unique to this frame, and also globally available services.
   *
   * @param gameTime A sampled instance of time that you should use everywhere that you need a time value.
-  * @param dice A psuedorandom number generator, made predicatable/reproducable by being seeded on the current running time.
+  * @param dice A pseudo-random number generator, made predictable / reproducible  by being seeded on the current running time.
   * @param inputState A snapshot of the state of the various input methods, also allows input mapping of combinations of inputs.
-  * @param boundaryLocator A service that can be interogated for the calculated dimensions of screen elements.
+  * @param boundaryLocator A service that can be interrogated for the calculated dimensions of screen elements.
   * @param startUpData A read only reference to any and all data created during start up / set up.
   */
 final class FrameContext[StartUpData](
