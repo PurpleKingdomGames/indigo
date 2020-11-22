494ba464b2ba615f3b51ace42fa5b2c005571786
Dave Smith <davesmith.00000@gmail.com>
2020-11-22 11:08:09 +0000

    AssetType scaladocs

--- indigo/shared/src/main/scala/indigo/shared/assets/AssetType.scala
+++ indigo/shared/src/main/scala/indigo/shared/assets/AssetType.scala
@@ -45,21 +45,57 @@ object AssetType {
     rec(assets, Nil)
   }
 
-  final class Tagged(private val assets: List[Image]) extends AssetType {
-    def toList: List[Image] = assets
+  /**
+    * Tagged instance, the preferred constructor is the apply method: Tagged("my tag")(image1..imageN)
+    *
+    * Images can optionally be "tagged", this tells indigo that images with the same tag should be grouped together for more efficient use. E.g. you might tag all the images for a given level with the same tag.
+    *
+    * @param tag The AssetTag to apply to all the images
+    * @param images The image assets to be tagged
+    */
+  final case class Tagged(tag: AssetTag, images: List[Image]) extends AssetType {
+    def toList: List[Image] = images.map(_.withTag(tag))
   }
   object Tagged {
-    def apply(tag: String)(images: Image*): Tagged =
-      new Tagged(images.toList.map(_.withTag(AssetTag(tag))))
 
+    /**
+      * Images can optionally be "tagged", this tells indigo that images with the same tag should be grouped together for more efficient use. E.g. you might tag all the images for a given level with the same tag.
+      *
+      * @param tag A string tag name, which is converted to an AssetTag
+      * @param images The image assets to be tagged
+      * @return An instance of Tagged which can be nested along side your usual asset declarations
+      */
+    def apply(tag: String)(images: Image*): Tagged =
+      Tagged(AssetTag(tag), images.toList)
+
+    /**
+      * Extractor for Tagged
+      *
+      * @param tagged the tagged instance to extract
+      * @return a list of tagged image assets
+      */
     def unapply(tagged: Tagged): Option[List[Image]] =
       Some(tagged.toList)
   }
 
+  /**
+    * Represents a text asset, Indigo does not care about the type of data held in the
+    * text file, it is up to the programmer to parse it.
+    *
+    * @param name the asset name used to look up the loaded asset
+    * @param path the path to the asset
+    */
   final case class Text(name: AssetName, path: AssetPath) extends AssetTypePrimitive {
     def toList: List[AssetType] = List(this)
   }
 
+  /**
+    * Represents any browser compatible image asset
+    *
+    * @param name the asset name used to look up the loaded asset
+    * @param path the path to the asset
+    * @param tag Images can optionally be "tagged", this tells indigo that images with the same tag should be grouped together for more efficient use. E.g. you might tag all the images for a given level with the same tag.
+    */
   final case class Image(name: AssetName, path: AssetPath, tag: Option[AssetTag]) extends AssetTypePrimitive {
     def withTag(tag: AssetTag): Image =
       this.copy(tag = Option(tag))
@@ -74,6 +110,12 @@ object AssetType {
     def apply(name: AssetName, path: AssetPath): Image = Image(name, path, None)
   }
 
+  /**
+    * Represents any browser compatible audio asset
+    *
+    * @param name the asset name used to look up the loaded asset
+    * @param path the path to the asset
+    */
   final case class Audio(name: AssetName, path: AssetPath) extends AssetTypePrimitive {
     def toList: List[AssetType] = List(this)
   }
