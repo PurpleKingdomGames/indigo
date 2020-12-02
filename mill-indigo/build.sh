391103b1a7fc22f83866f1fbd9127ab644ab8111
Dave Smith <davesmith.00000@gmail.com>
2020-12-02 08:12:27 +0000

    WIP: Mill + Dotty

--- mill-indigo/build.sh
+++ mill-indigo/build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
-mill clean mill-indigo
-mill mill-indigo.publishLocal
+mill clean mill-indigo[2.13]
+mill clean mill-indigo[3.0.0-M2]
 
+mill mill-indigo[2.13].compile
+mill mill-indigo[3.0.0-M2].compile
+
+mill mill-indigo[2.13].publishLocal
+mill mill-indigo[3.0.0-M2].publishLocal
