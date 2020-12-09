5ed82950ae4ff8f518287bf003f06a54a660317c
Dave Smith <davesmith.00000@gmail.com>
2020-12-09 09:44:18 +0000

    Pirates demo up to date

--- demos/pirate/src/main/scala/pirate/scenes/level/subsystems/CloudsAutomata.scala
+++ demos/pirate/src/main/scala/pirate/scenes/level/subsystems/CloudsAutomata.scala
@@ -14,25 +14,27 @@ object CloudsAutomata {
   The Signal controls the clouds movement over time, the "over time" part is critical.
   `Signal.Lerp` mean "Linear Interpolation" i.e. smoothly transition from position A
   to position B over a given time frame.
-  The important thing here is that the Signal doesn't know what the time actually is 
+  The important thing here is that the Signal doesn't know what the time actually is
   and relies on being told. Which means you can also go back in time by lying to the
   signal!
   Lerp then is a function `t: Seconds => position: Point`, and to make use of it we
   map the signal to our renderable cloud graphic.
-  */
-  val signal: (AutomatonSeedValues, SceneGraphNode) => Signal[AutomatonUpdate] =
-    (seed, node) =>
-      node match {
-        case cloud: Graphic =>
-          Signal
-            .Lerp(seed.spawnedAt, Point(-150, seed.spawnedAt.y), seed.lifeSpan)
-            .map { position =>
-              AutomatonUpdate(cloud.moveTo(position))
-            }
+   */
+  val signal: SignalReader[(AutomatonSeedValues, SceneGraphNode), AutomatonUpdate] =
+    SignalReader {
+      case (seed, node) =>
+        node match {
+          case cloud: Graphic =>
+            Signal
+              .Lerp(seed.spawnedAt, Point(-150, seed.spawnedAt.y), seed.lifeSpan)
+              .map { position =>
+                AutomatonUpdate(cloud.moveTo(position))
+              }
 
-        case _ =>
-          Signal.fixed(AutomatonUpdate.empty)
-      }
+          case _ =>
+            Signal.fixed(AutomatonUpdate.empty)
+        }
+    }
 
   // One spawn, the automaton instance will choose "OneOf"
   // these graphics to be drawn as for the rest of its life.
