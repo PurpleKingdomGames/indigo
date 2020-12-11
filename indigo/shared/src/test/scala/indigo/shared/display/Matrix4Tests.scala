bc37144cf4ee2ec01938abca387993412441f33e
Dave Smith <davesmith.00000@gmail.com>
2020-12-11 19:06:34 +0000

    Revert "Merge pull request #87 from PurpleKingdomGames/matrices"
    This reverts commit e891a08b11c19ca1ccd622617edcf726a6a864a1.

--- indigo/shared/src/test/scala/indigo/shared/display/Matrix4Tests.scala
+++ indigo/shared/src/test/scala/indigo/shared/display/Matrix4Tests.scala
@@ -1,165 +1,85 @@
 package indigo.shared.display
 
+import indigo.shared.EqualTo._
 import indigo.shared.datatypes.Matrix4
 
 class Matrix4Tests extends munit.FunSuite {
 
-  test("identity") {
+  test("Identity") {
 
-    val expected =
-      Matrix4(
-        (1, 0, 0, 0),
-        (0, 1, 0, 0),
-        (0, 0, 1, 0),
-        (0, 0, 0, 1)
-      )
+    val expected: List[Double] = List(
+      1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1
+    )
 
-    assertEquals(Matrix4.identity, expected)
+    assertEquals(Matrix4.identity.mat === expected, true)
 
   }
 
-  test("translate x") {
+  test("Translation.should be able to translate in the X direction") {
 
-    val expected =
-      Matrix4(
-        (1, 0, 0, 0),
-        (0, 1, 0, 0),
-        (0, 0, 1, 0),
-        (2, 0, 0, 1)
-      )
+    val expected: List[Double] = List(
+      1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 2, 0, 0, 1
+    )
 
-    assertEquals(Matrix4.identity.translate(2.0, 0, 0), expected)
+    assertEquals(Matrix4.identity.translate(2.0, 0, 0).mat === expected, true)
 
   }
 
-  test("translate y") {
+  test("Translation.should be able to translate in the Y direction") {
 
-    val expected =
-      Matrix4(
-        (1, 0, 0, 0),
-        (0, 1, 0, 0),
-        (0, 0, 1, 0),
-        (0, 2, 0, 1)
-      )
+    val expected: List[Double] = List(
+      1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 2, 0, 1
+    )
 
-    assertEquals(Matrix4.identity.translate(0, 2.0, 0), expected)
+    assertEquals(Matrix4.identity.translate(0, 2.0, 0).mat === expected, true)
 
   }
 
-  test("translate z") {
+  test("Translation.should be able to translate in the Z direction") {
 
-    val expected =
-      Matrix4(
-        (1, 0, 0, 0),
-        (0, 1, 0, 0),
-        (0, 0, 1, 0),
-        (0, 0, 2, 1)
-      )
+    val expected: List[Double] = List(
+      1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 2, 1
+    )
 
-    assertEquals(Matrix4.identity.translate(0, 0, 2.0), expected)
+    assertEquals(Matrix4.identity.translate(0, 0, 2.0).mat === expected, true)
 
   }
 
-  test("rotation") {
+  test("Rotation.") {
 
     val s = Math.sin(Math.PI)
     val c = Math.cos(Math.PI)
 
-    val expected =
-      Matrix4(
-        (c, s, 0, 0),
-        (-s, c, 0, 0),
-        (0, 0, 1, 0),
-        (0, 0, 0, 1)
-      )
+    val expected: List[Double] = List(
+      c,
+      s,
+      0,
+      0,
+      -s,
+      c,
+      0,
+      0,
+      0,
+      0,
+      1,
+      0,
+      0,
+      0,
+      0,
+      1
+    )
 
-    assertEquals(Matrix4.identity.rotate(Math.PI), expected)
+    assertEquals(Matrix4.identity.rotate(Math.PI).mat === expected, true)
   }
 
-  test("scale") {
+  test("Scale.should be able to translate in the X direction") {
 
-    val expected =
-      Matrix4(
-        (2, 0, 0, 0),
-        (0, 3, 0, 0),
-        (0, 0, 4, 0),
-        (0, 0, 0, 1)
-      )
+    val expected: List[Double] = List(
+      2, 0, 0, 0, 0, 3, 0, 0, 0, 0, 4, 0, 0, 0, 0, 1
+    )
 
-    assertEquals(Matrix4.identity.scale(2.0, 3.0, 4.0), expected)
+    assertEquals(Matrix4.identity.scale(2.0, 3.0, 4.0).mat === expected, true)
 
-  }
-
-  test("transpose") {
-
-    val mat =
-      Matrix4(
-        (1, 2, 3, 4),
-        (5, 6, 7, 8),
-        (9, 10, 11, 12),
-        (13, 14, 15, 16)
-      )
-
-    val expected =
-      Matrix4(
-        (1, 5, 9, 13),
-        (2, 6, 10, 14),
-        (3, 7, 11, 15),
-        (4, 8, 12, 16)
-      )
-
-    assertEquals(mat.transpose, expected)
-  }
-
-  test("multiply") {
-    val mat1: Matrix4 =
-      Matrix4(
-        (1, 2, 3, 4),
-        (2, 1, 2, 3),
-        (3, 2, 1, 2),
-        (4, 3, 2, 1)
-      )
-
-    val mat2: Matrix4 =
-      Matrix4(
-        (10, 20, 30, 40),
-        (20, 10, 20, 30),
-        (30, 20, 10, 20),
-        (40, 30, 20, 10)
-      )
-
-    val expected: Matrix4 =
-      Matrix4(
-        (300, 220, 180, 200),
-        (220, 180, 160, 180),
-        (180, 160, 180, 220),
-        (200, 180, 220, 300)
-      )
-
-    val actual: Matrix4 =
-      mat1 * mat2
-
-    assertEquals(actual, expected)
-  }
-
-  test("flip") {
-    val mat =
-      Matrix4(
-        (1, 2, 3, 4),
-        (5, 6, 7, 8),
-        (9, 10, 11, 12),
-        (13, 14, 15, 16)
-      )
-
-    val expected =
-      Matrix4(
-        (-1, -2, -3, 4),
-        (-5, -6, -7, 8),
-        (-9, -10, -11, 12),
-        (-13, -14, -15, 16)
-      )
-
-    assertEquals(mat.flip(true, true), expected)
   }
 
 }
