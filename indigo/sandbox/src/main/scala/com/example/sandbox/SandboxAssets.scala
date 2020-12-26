56176aab5bfb2db4f9a408a54bb8ef2baeb79d67
Dave Smith <davesmith.00000@gmail.com>
2020-12-26 11:59:35 +0000

    Added additional asset to sandbox

--- indigo/sandbox/src/main/scala/com/example/sandbox/SandboxAssets.scala
+++ indigo/sandbox/src/main/scala/com/example/sandbox/SandboxAssets.scala
@@ -7,17 +7,20 @@ object SandboxAssets {
   val smallFontName: AssetName = AssetName("smallFontName")
   val dudeName: AssetName      = AssetName("base_charactor")
   val light: AssetName         = AssetName("light")
+  val dots: AssetName         = AssetName("dots")
 
   val smallFontNameMaterial: Material.Textured = Material.Textured(smallFontName)
   val dudeNameMaterial: Material.Textured      = Material.Textured(dudeName)
   val lightMaterial: Material.Textured         = Material.Textured(light)
+  val dotsMaterial: Material.Textured         = Material.Textured(dots)
 
   def assets: Set[AssetType] =
     Set(
       AssetType.Image(smallFontName, AssetPath("assets/boxy_font.png")),
       AssetType.Image(light, AssetPath("assets/light_texture.png")),
       AssetType.Text(AssetName(dudeName.value + "-json"), AssetPath("assets/" + dudeName.value + ".json")),
-      AssetType.Image(dudeName, AssetPath("assets/" + dudeName.value + ".png"))
+      AssetType.Image(dudeName, AssetPath("assets/" + dudeName.value + ".png")),
+      AssetType.Image(dots, AssetPath("assets/" + dots.value + ".png"))
     )
 
 }
