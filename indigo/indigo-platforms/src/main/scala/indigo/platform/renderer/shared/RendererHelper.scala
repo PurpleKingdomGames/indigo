7c2d9a1164907d6bc1a005c4e7f0b562fbc52f31
Dave Smith <davesmith.00000@gmail.com>
2021-01-03 00:49:22 +0000

    WebGL 1.0 working again

--- indigo/indigo-platforms/src/main/scala/indigo/platform/renderer/shared/RendererHelper.scala
+++ indigo/indigo-platforms/src/main/scala/indigo/platform/renderer/shared/RendererHelper.scala
@@ -3,6 +3,7 @@ package indigo.platform.renderer.shared
 import indigo.shared.display.DisplayObject
 import indigo.shared.display.SpriteSheetFrame
 import indigo.shared.datatypes.Vector2
+import indigo.shared.datatypes.Vector3
 import indigo.shared.display.DisplayEffects
 import indigo.shared.datatypes.Matrix4
 
@@ -17,7 +18,7 @@ object RendererHelper {
       // x = 0,
       // y = 0,
       // z = 1,
-      transform = Matrix4.scale(1.0, -1.0, 1.0),
+      transform = makeMatrix(w.toDouble, h.toDouble),
       width = w,
       height = h,
       // rotation = 0,
@@ -39,6 +40,11 @@ object RendererHelper {
       // flipHorizontal = 1.0f,
       // flipVertical = -1.0f
     )
+
+  private def makeMatrix(w: Double, h: Double): Matrix4 =
+    Matrix4
+      .translation(Vector3(0.5d, 0.5d, 0.0d)) // reposition on screen
+      .scale(Vector3(w, h, 1.0))
 
   def mat4ToJsArray(mat4d: Matrix4): scalajs.js.Array[Double] =
     mat4d.toList.toJSArray
