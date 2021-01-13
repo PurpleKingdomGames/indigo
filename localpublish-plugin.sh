a18322a033c202ef32c099fe56b6e165b872ffd6
Dave Smith <davesmith.00000@gmail.com>
2021-01-13 23:14:13 +0000

    Local plugin build, makes and includes worker js files

--- localpublish-plugin.sh
+++ localpublish-plugin.sh
@@ -2,6 +2,23 @@
 
 set -e
 
+# Clean up local files
+echo ">>> Removing ivy artefacts"
+rm -fr ~/.ivy2/local/io.indigoengine/indigo-plugin_2.12
+rm -fr ~/.ivy2/local/io.indigoengine/indigo-plugin_2.13
+rm -fr ~/.ivy2/local/io.indigoengine/mill-indigo_2.13
+rm -fr ~/.ivy2/local/io.indigoengine/sbt-indigo
+
+# Indigo
+echo ">>> Indigo Workers"
+cd indigo
+sbt exportWorkers
+cp indigo-render-worker/target/scala-3.0.0-M3/indigo-render-worker-opt.js ../indigo-plugin/indigo-plugin/resources/workers/
+cp indigo-render-worker/target/scala-3.0.0-M3/indigo-render-worker-opt.js.map ../indigo-plugin/indigo-plugin/resources/workers/
+cp indigo-render-worker/target/scala-3.0.0-M3/indigo-render-worker-opt.js ../indigo-plugin/indigo-plugin/resources/workers/
+cp indigo-render-worker/target/scala-3.0.0-M3/indigo-render-worker-opt.js.map ../indigo-plugin/indigo-plugin/resources/workers/
+cd ..
+
 # Indigo Plugin
 echo ">>> Indigo Plugin"
 cd indigo-plugin
