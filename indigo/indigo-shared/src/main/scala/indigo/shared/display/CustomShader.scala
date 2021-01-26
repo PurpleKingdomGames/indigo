8383fce335eaea763934bfcb0bd4b8040e04ad87
Dave Smith <davesmith.00000@gmail.com>
2021-01-26 00:22:28 +0000

    You can define shaders locally to your game

---
+++ indigo/indigo-shared/src/main/scala/indigo/shared/display/CustomShader.scala
@@ -0,0 +1,11 @@
+package indigo.shared.display
+
+// import indigo.shared.assets.AssetName
+
+sealed trait CustomShader {
+  val id: ShaderId
+}
+object CustomShader {
+  final case class Source(id: ShaderId, vertex: String, fragment: String) extends CustomShader
+  // final case class Text(id: ShaderId, vertex: AssetName, fragment: AssetName) extends CustomShader
+}
