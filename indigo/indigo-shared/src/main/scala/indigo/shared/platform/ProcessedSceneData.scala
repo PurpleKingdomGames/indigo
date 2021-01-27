4190b87efa8985b63e1b170b784e64d53a99dbe2
Dave Smith <davesmith.00000@gmail.com>
2021-01-27 09:20:53 +0000

    WIP: Started renderer code for layers - not rendering anything!

--- indigo/indigo-shared/src/main/scala/indigo/shared/platform/ProcessedSceneData.scala
+++ indigo/indigo-shared/src/main/scala/indigo/shared/platform/ProcessedSceneData.scala
@@ -1,20 +1,22 @@
 package indigo.shared.platform
 
 import indigo.shared.datatypes.mutable.CheapMatrix4
-import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 import indigo.shared.display.DisplayEntity
 import indigo.shared.display.DisplayObject
+import indigo.shared.display.DisplayLayer
 import indigo.shared.scenegraph.Light
 import indigo.shared.datatypes.RGBA
 
 final class ProcessedSceneData(
+    val layers: List[DisplayLayer],
     val gameProjection: CheapMatrix4,
     val lightingProjection: CheapMatrix4,
     val uiProjection: CheapMatrix4,
-    val gameLayerDisplayObjects: mutable.ListBuffer[DisplayEntity],
-    val lightingLayerDisplayObjects: mutable.ListBuffer[DisplayEntity],
-    val distortionLayerDisplayObjects: mutable.ListBuffer[DisplayEntity],
-    val uiLayerDisplayObjects: mutable.ListBuffer[DisplayEntity],
+    val gameLayerDisplayObjects: ListBuffer[DisplayEntity],
+    val lightingLayerDisplayObjects: ListBuffer[DisplayEntity],
+    val distortionLayerDisplayObjects: ListBuffer[DisplayEntity],
+    val uiLayerDisplayObjects: ListBuffer[DisplayEntity],
     val cloneBlankDisplayObjects: Map[String, DisplayObject],
     val lights: List[Light],
     val clearColor: RGBA,
