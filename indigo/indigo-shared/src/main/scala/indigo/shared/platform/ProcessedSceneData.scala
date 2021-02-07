b2ab32dac168de2ed113c01902e70ac4c13c4741
Dave Smith <davesmith.00000@gmail.com>
2021-02-07 23:19:02 +0000

    SceneProcessor does not look for specific layers

--- indigo/indigo-shared/src/main/scala/indigo/shared/platform/ProcessedSceneData.scala
+++ indigo/indigo-shared/src/main/scala/indigo/shared/platform/ProcessedSceneData.scala
@@ -1,8 +1,8 @@
 package indigo.shared.platform
 
-import indigo.shared.datatypes.mutable.CheapMatrix4
-import scala.collection.mutable.ListBuffer
-import indigo.shared.display.DisplayEntity
+// import indigo.shared.datatypes.mutable.CheapMatrix4
+// import scala.collection.mutable.ListBuffer
+// import indigo.shared.display.DisplayEntity
 import indigo.shared.display.DisplayObject
 import indigo.shared.display.DisplayLayer
 import indigo.shared.scenegraph.Light
@@ -10,22 +10,22 @@ import indigo.shared.datatypes.RGBA
 
 final class ProcessedSceneData(
     val layers: List[DisplayLayer],
-    val gameProjection: CheapMatrix4,
-    val lightingProjection: CheapMatrix4,
-    val uiProjection: CheapMatrix4,
-    val gameLayerDisplayObjects: ListBuffer[DisplayEntity],
-    val lightingLayerDisplayObjects: ListBuffer[DisplayEntity],
-    val distortionLayerDisplayObjects: ListBuffer[DisplayEntity],
-    val uiLayerDisplayObjects: ListBuffer[DisplayEntity],
+    // val gameProjection: CheapMatrix4,
+    // val lightingProjection: CheapMatrix4,
+    // val uiProjection: CheapMatrix4,
+    // val gameLayerDisplayObjects: ListBuffer[DisplayEntity],
+    // val lightingLayerDisplayObjects: ListBuffer[DisplayEntity],
+    // val distortionLayerDisplayObjects: ListBuffer[DisplayEntity],
+    // val uiLayerDisplayObjects: ListBuffer[DisplayEntity],
     val cloneBlankDisplayObjects: Map[String, DisplayObject],
     val lights: List[Light],
     val clearColor: RGBA,
-    val gameLayerColorOverlay: RGBA,
-    val uiLayerColorOverlay: RGBA,
-    val gameLayerTint: RGBA,
-    val lightingLayerTint: RGBA,
-    val uiLayerTint: RGBA,
-    val gameLayerSaturation: Double,
-    val lightingLayerSaturation: Double,
-    val uiLayerSaturation: Double
+    // val gameLayerColorOverlay: RGBA,
+    // val uiLayerColorOverlay: RGBA,
+    // val gameLayerTint: RGBA,
+    // val lightingLayerTint: RGBA,
+    // val uiLayerTint: RGBA,
+    // val gameLayerSaturation: Double,
+    // val lightingLayerSaturation: Double,
+    // val uiLayerSaturation: Double
 )
