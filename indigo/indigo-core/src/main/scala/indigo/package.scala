4f5a58ba1e0f93ae9f8c475cb7fec843563acb18
Dave Smith <davesmith.00000@gmail.com>
2021-02-14 12:14:21 +0000

    Add alias for EventHandler

--- indigo/indigo-core/src/main/scala/indigo/package.scala
+++ indigo/indigo-core/src/main/scala/indigo/package.scala
@@ -97,6 +97,7 @@ package object indigo {
   type SubSystemEvent = shared.events.SubSystemEvent
   type ViewEvent      = shared.events.ViewEvent
   type InputEvent     = shared.events.InputEvent
+  type EventHandler   = shared.scenegraph.EventHandler
 
   type EventFilters = shared.events.EventFilters
   val EventFilters: shared.events.EventFilters.type = shared.events.EventFilters
