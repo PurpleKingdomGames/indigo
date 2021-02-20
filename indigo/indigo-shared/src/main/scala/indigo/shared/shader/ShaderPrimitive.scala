868ed02f7d4868266f60ed74973bcca37d983e0d
Dave Smith <davesmith.00000@gmail.com>
2021-02-20 13:59:38 +0000

    Added length and correct array data to ShaderPrimitive

--- indigo/indigo-shared/src/main/scala/indigo/shared/shader/ShaderPrimitive.scala
+++ indigo/indigo-shared/src/main/scala/indigo/shared/shader/ShaderPrimitive.scala
@@ -2,18 +2,21 @@ package indigo.shared.shader
 
 sealed trait ShaderPrimitive {
   def hash: String
+  def length: Int
   def toArray: Array[Float]
 }
 object ShaderPrimitive {
 
   final case class float(value: Double) extends ShaderPrimitive {
     def hash: String          = s"float${value.toString()}"
-    def toArray: Array[Float] = Array(value.toFloat, 0.0f, 0.0f, 0.0f)
+    def length: Int           = 1
+    def toArray: Array[Float] = Array(value.toFloat)
   }
 
   final case class vec2(x: Double, y: Double) extends ShaderPrimitive {
     def hash: String          = s"vec2$x$y"
-    def toArray: Array[Float] = Array(x.toFloat, y.toFloat, 0.0f, 0.0f)
+    def length: Int           = 2
+    def toArray: Array[Float] = Array(x.toFloat, y.toFloat)
   }
   object vec2 {
     def apply(fill: Double): vec2 =
@@ -21,6 +24,7 @@ object ShaderPrimitive {
   }
   final case class vec3(x: Double, y: Double, z: Double) extends ShaderPrimitive {
     def hash: String          = s"vec3$x$y$z"
+    def length: Int           = 4
     def toArray: Array[Float] = Array(x.toFloat, y.toFloat, z.toFloat, 0.0f)
   }
   object vec3 {
@@ -29,6 +33,7 @@ object ShaderPrimitive {
   }
   final case class vec4(x: Double, y: Double, z: Double, w: Double) extends ShaderPrimitive {
     def hash: String          = s"vec4$x$y$z$w"
+    def length: Int           = 4
     def toArray: Array[Float] = Array(x.toFloat, y.toFloat, z.toFloat, w.toFloat)
   }
   object vec4 {
