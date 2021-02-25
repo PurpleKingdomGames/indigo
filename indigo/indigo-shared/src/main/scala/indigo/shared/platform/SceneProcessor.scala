51b1eaaa6758d418270e9d5ce95961c9df71bb0d
Dave Smith <davesmith.00000@gmail.com>
2021-02-25 23:45:49 +0000

    Shapes are cloneable, fixed context bugs in renderer

--- indigo/indigo-shared/src/main/scala/indigo/shared/platform/SceneProcessor.scala
+++ indigo/indigo-shared/src/main/scala/indigo/shared/platform/SceneProcessor.scala
@@ -12,6 +12,7 @@ import indigo.shared.display.DisplayLayer
 import indigo.shared.scenegraph.Graphic
 import indigo.shared.scenegraph.Sprite
 import indigo.shared.platform.ProcessedSceneData
+import indigo.shared.scenegraph.Shape
 
 final class SceneProcessor(
     boundaryLocator: BoundaryLocator,
@@ -20,59 +21,26 @@ final class SceneProcessor(
 ) {
   private val displayObjectConverter: DisplayObjectConversions =
     new DisplayObjectConversions(boundaryLocator, animationsRegister, fontRegister)
-  // private val displayObjectConverterGame: DisplayObjectConversions =
-  //   new DisplayObjectConversions(boundaryLocator, animationsRegister, fontRegister)
-  // private val displayObjectConverterLighting: DisplayObjectConversions =
-  //   new DisplayObjectConversions(boundaryLocator, animationsRegister, fontRegister)
-  // private val displayObjectConverterDistortion: DisplayObjectConversions =
-  //   new DisplayObjectConversions(boundaryLocator, animationsRegister, fontRegister)
-  // private val displayObjectConverterUi: DisplayObjectConversions =
-  //   new DisplayObjectConversions(boundaryLocator, animationsRegister, fontRegister)
   private val displayObjectConverterClone: DisplayObjectConversions =
     new DisplayObjectConversions(boundaryLocator, animationsRegister, fontRegister)
 
   def purgeCaches(): Unit = {
     displayObjectConverter.purgeCaches()
-    // displayObjectConverterGame.purgeCaches()
-    // displayObjectConverterLighting.purgeCaches()
-    // displayObjectConverterDistortion.purgeCaches()
-    // displayObjectConverterUi.purgeCaches()
     displayObjectConverterClone.purgeCaches()
   }
 
   def processScene(
       gameTime: GameTime,
       scene: SceneUpdateFragment,
-      assetMapping: AssetMapping //,
-      // screenWidth: Double,
-      // screenHeight: Double,
-      // orthographicProjectionMatrix: CheapMatrix4
+      assetMapping: AssetMapping
   ): ProcessedSceneData = {
-
-    // val gameProjection =
-    //   scene.gameLayer.magnification
-    //     .map { m =>
-    //       calculateProjectionMatrix(screenWidth, screenHeight, m.toDouble)
-    //     }
-    //     .getOrElse(orthographicProjectionMatrix)
-
-    // val lightingProjection =
-    //   scene.lightingLayer.magnification
-    //     .map { m =>
-    //       calculateProjectionMatrix(screenWidth, screenHeight, m.toDouble)
-    //     }
-    //     .getOrElse(orthographicProjectionMatrix)
-
-    // val uiProjection =
-    //   scene.uiLayer.magnification
-    //     .map { m =>
-    //       calculateProjectionMatrix(screenWidth, screenHeight, m.toDouble)
-    //     }
-    //     .getOrElse(orthographicProjectionMatrix)
 
     val cloneBlankDisplayObjects =
       scene.cloneBlanks.foldLeft(Map.empty[String, DisplayObject]) { (acc, blank) =>
         blank.cloneable match {
+          case s: Shape =>
+            acc + (blank.id.value -> displayObjectConverterClone.shapeToDisplayObject(s))
+
           case g: Graphic =>
             acc + (blank.id.value -> displayObjectConverterClone.graphicToDisplayObject(g, assetMapping))
 
@@ -103,38 +71,11 @@ final class SceneProcessor(
         }
         .sortBy(_.depth)
 
-    // val gameLayerDisplayObjects =
-    //   displayObjectConverterGame.sceneNodesToDisplayObjects(scene.gameLayer.nodes, gameTime, assetMapping, cloneBlankDisplayObjects)
-
-    // val lightingLayerDisplayObjects =
-    //   displayObjectConverterLighting.sceneNodesToDisplayObjects(scene.lightingLayer.nodes, gameTime, assetMapping, cloneBlankDisplayObjects)
-
-    // val distortionLayerDisplayObjects =
-    //   displayObjectConverterDistortion.sceneNodesToDisplayObjects(scene.distortionLayer.nodes, gameTime, assetMapping, cloneBlankDisplayObjects)
-
-    // val uiLayerDisplayObjects =
-    //   displayObjectConverterUi.sceneNodesToDisplayObjects(scene.uiLayer.nodes, gameTime, assetMapping, cloneBlankDisplayObjects)
-
     new ProcessedSceneData(
       displayLayers,
-      // gameProjection,
-      // lightingProjection,
-      // uiProjection,
-      // gameLayerDisplayObjects,
-      // lightingLayerDisplayObjects,
-      // distortionLayerDisplayObjects,
-      // uiLayerDisplayObjects,
       cloneBlankDisplayObjects,
       scene.lights,
-      scene.ambientLight //,
-      // scene.screenEffects.gameColorOverlay,
-      // scene.screenEffects.uiColorOverlay,
-      // scene.gameLayer.tint,
-      // scene.lightingLayer.tint,
-      // scene.uiLayer.tint,
-      // scene.gameLayer.saturation,
-      // scene.lightingLayer.saturation,
-      // scene.uiLayer.saturation
+      scene.ambientLight
     )
   }
 
