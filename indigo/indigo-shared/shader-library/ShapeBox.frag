7d1f7836cbe1b6997f61d074bb8dcc938a806b25
Dave Smith <davesmith.00000@gmail.com>
2021-02-28 23:23:48 +0000

    Shapes are all working

--- indigo/indigo-shared/shader-library/ShapeBox.frag
+++ indigo/indigo-shared/shader-library/ShapeBox.frag
@@ -12,8 +12,11 @@ vec2 SIZE;
 layout (std140) uniform CustomData {
   vec2 ASPECT_RATIO;
   float STROKE_WIDTH;
+  float FILL_TYPE;
   vec4 STROKE_COLOR;
-  vec4 FILL_COLOR;
+  vec4 GRADIENT_FROM_TO;
+  vec4 GRADIENT_FROM_COLOR;
+  vec4 GRADIENT_TO_COLOR;
 };
 
 // Borrowed with thanks! https://www.iquilezles.org/www/articles/distfunctions2d/distfunctions2d.htm
@@ -22,17 +25,73 @@ float sdfCalc(vec2 p, vec2 b){
   return length(max(d, 0.0)) + min(max(d.x, d.y), 0.0);
 }
 
+vec4 calculateColor() {
+  return vec4(GRADIENT_FROM_COLOR.rgb * GRADIENT_FROM_COLOR.a, GRADIENT_FROM_COLOR.a);
+}
+
+vec4 calculateLinearGradient() {
+  vec2 pointA = GRADIENT_FROM_TO.xy;
+  vec2 pointB = GRADIENT_FROM_TO.zw;
+  vec2 pointP = UV * SIZE;
+
+  // `h` is the distance along the gradient 0 at A, 1 at B
+  float h = min(1.0, max(0.0, dot(pointP - pointA, pointB - pointA) / dot(pointB - pointA, pointB - pointA)));
+
+  vec4 gradient = mix(GRADIENT_FROM_COLOR, GRADIENT_TO_COLOR, h);
+
+  return vec4(gradient.rgb * gradient.a, gradient.a);
+}
+
+vec4 calculateRadialGradient() {
+  vec2 pointA = GRADIENT_FROM_TO.xy;
+  vec2 pointB = GRADIENT_FROM_TO.zw;
+  vec2 pointP = UV * SIZE;
+
+  float radius = length(pointB - pointA);
+  float distanceToP = length(pointP - pointA);
+
+  float sdf = clamp(-((distanceToP - radius) / radius), 0.0, 1.0);
+
+  vec4 gradient = mix(GRADIENT_TO_COLOR, GRADIENT_FROM_COLOR, sdf);
+
+  return vec4(gradient.rgb * gradient.a, gradient.a);
+}
+
 void fragment() {
 
-  float strokeWidthHalf = max(0.0, STROKE_WIDTH / SIZE.x / 2.0); // TODO... 
+  float strokeWidthHalf = max(0.0, STROKE_WIDTH / SIZE.x / 2.0);
+
+  //--- Fill
+  // 0 = color; 1 = linear gradient; 2 = radial gradient
+  int fillType = int(round(FILL_TYPE));
+  vec4 fill;
+
+  switch(fillType) {
+    case 0:
+      fill = calculateColor();
+      break;
+
+    case 1:
+      fill = calculateLinearGradient();
+      break;
+
+    case 2:
+      fill = calculateRadialGradient();
+      break;
+
+    default:
+      fill = calculateColor();
+      break;
+  }
+  //---
 
   float sdf = sdfCalc(UV - 0.5, (vec2(0.5) * ASPECT_RATIO) - strokeWidthHalf);
   float annularSdf = abs(sdf) - strokeWidthHalf;
 
-  float fillAmount = (1.0 - step(0.0, sdf)) * FILL_COLOR.a;
+  float fillAmount = (1.0 - step(0.0, sdf)) * fill.a;
   float strokeAmount = (1.0 - step(0.0, annularSdf)) * STROKE_COLOR.a;
 
-  vec4 fillColor = vec4(FILL_COLOR.rgb * fillAmount, fillAmount);
+  vec4 fillColor = vec4(fill.rgb * fillAmount, fillAmount);
   vec4 strokeColor = vec4(STROKE_COLOR.rgb * strokeAmount, strokeAmount);
 
   COLOR = mix(fillColor, strokeColor, strokeAmount);
--- indigo/sandbox/src/main/scala/com/example/sandbox/scenes/ShapesScene.scala
+++ indigo/sandbox/src/main/scala/com/example/sandbox/scenes/ShapesScene.scala
@@ -34,29 +34,29 @@ object ShapesScene extends Scene[SandboxStartupData, SandboxGameModel, SandboxVi
 
   def present(context: FrameContext[SandboxStartupData], model: SandboxGameModel, viewModel: SandboxViewModel): Outcome[SceneUpdateFragment] = {
 
-    // val circleBorder: Int =
-    //   Signal.SmoothPulse.map(d => 2 + (10 * d).toInt).affectTime(0.5).at(context.running)
+    val circleGradient: Int =
+      Signal.SmoothPulse.map(d => 2 + (10 * d).toInt).at(context.running)
 
-    // val circlePosition: Point =
-    //   Signal.Orbit(Point(200, 150), 10).map(_.toPoint).affectTime(0.25).at(context.running)
+    val circlePosition: Point =
+      Signal.Orbit(Point(200, 150), 10).map(_.toPoint).affectTime(0.25).at(context.running)
 
-    // val lineThickness: Int =
-    //   Signal.SmoothPulse.map(d => (10 * d).toInt).at(context.running)
+    val lineThickness: Int =
+      Signal.SmoothPulse.map(d => (10 * d).toInt).at(context.running)
 
-    // val squareSize: Point =
-    //   Point(Signal.SmoothPulse.map(d => 20 + (80 * d).toInt).affectTime(0.25).at(context.running))
+    val squareSize: Point =
+      Point(Signal.SmoothPulse.map(d => 20 + (80 * d).toInt).affectTime(0.25).at(context.running))
 
     Outcome(
       SceneUpdateFragment.empty
         .addLayer(
-          // Shape.Circle(circlePosition, 20, Fill.Color(RGBA.Red), Stroke(circleBorder, RGBA.White)),
-          // Shape.Line(Point(30, 80), Point(100, 20), Stroke(lineThickness, RGBA.Cyan)),
-          // Shape.Box(Rectangle(Point(100, 100), squareSize), Fill.Color(RGBA.White), Stroke(10, RGBA.Black.withAlpha(0.75))),
-          // Clone(CloneId("shape clone")).withPosition(Point(10, 10)),
-          // Clone(CloneId("shape clone")).withPosition(Point(20, 10)),
-          // Clone(CloneId("shape clone")).withPosition(Point(30, 10)),
+          Shape.Circle(circlePosition, 20, Fill.RadialGradient(Point(20), RGBA.Red.withAlpha(0.25), Point.zero - circleGradient, RGBA.Red), Stroke(1, RGBA.Red)),
+          Shape.Line(Point(30, 80), Point(100, 20), Stroke(lineThickness, RGBA.Cyan)),
+          Shape.Box(Rectangle(Point(100, 100), squareSize), Fill.Color(RGBA.White), Stroke(10, RGBA.Black.withAlpha(0.75))),
+          Clone(CloneId("shape clone")).withPosition(Point(10, 10)),
+          Clone(CloneId("shape clone")).withPosition(Point(20, 10)),
+          Clone(CloneId("shape clone")).withPosition(Point(30, 10)),
           Shape
-            .Polygon(Fill.RadialGradient(Point(0), RGBA.Magenta, Point(45), RGBA.Cyan), Stroke(4, RGBA.Black.withAlpha(0.75)))(
+            .Polygon(Fill.LinearGradient(Point(0), RGBA.Magenta, Point(45), RGBA.Cyan), Stroke(4, RGBA.Black.withAlpha(0.75)))(
               Point(10, 10) - (Math.cos(Radians.fromSeconds(context.running).value) * 5).toInt,
               Point(20, 70) + (Math.sin(Radians.fromSeconds(context.running * Seconds(1.2)).value) * 10).toInt,
               Point(90, 90) + (Math.sin(Radians.fromSeconds(context.running * Seconds(0.8)).value) * 6).toInt,
