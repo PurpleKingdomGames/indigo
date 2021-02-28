76d595017eb724d297cdf5f4bfdc9979b49e074f
Dave Smith <davesmith.00000@gmail.com>
2021-02-28 23:07:34 +0000

    Polygons now support Fills

---
+++ indigo/indigo-shared/src/main/scala/indigo/shared/datatypes/Stroke.scala
@@ -0,0 +1,23 @@
+package indigo.shared.datatypes
+
+final case class Stroke(width: Int, color: RGBA) {
+
+  def withWidth(newWidth: Int): Stroke =
+    this.copy(width = newWidth)
+
+  def withColor(newColor: RGBA): Stroke =
+    this.copy(color = newColor)
+
+}
+object Stroke {
+
+  val Black: Stroke =
+     Stroke(1, RGBA.Black)
+
+  val None: Stroke =
+    Stroke(0, RGBA.Zero)
+
+  def apply(width: Int): Stroke =
+    Stroke(width, RGBA.Black)
+
+}
