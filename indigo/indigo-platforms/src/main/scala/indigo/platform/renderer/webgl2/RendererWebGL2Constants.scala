4f1f34da3007de6374710ac837712cab79caad78
Dave Smith <davesmith.00000@gmail.com>
2021-03-06 14:18:33 +0000

    Using "u_projection2", must remove "u_projection"

--- indigo/indigo-platforms/src/main/scala/indigo/platform/renderer/webgl2/RendererWebGL2Constants.scala
+++ indigo/indigo-platforms/src/main/scala/indigo/platform/renderer/webgl2/RendererWebGL2Constants.scala
@@ -2,7 +2,7 @@ package indigo.platform.renderer.webgl2
 
 object RendererWebGL2Constants {
 
-  // val projectionBlockPointer: Int = 1
+  val projectionBlockPointer: Int = 1
   val frameDataBlockPointer: Int  = 2
   val customDataBlockPointer: Int = 3
 
