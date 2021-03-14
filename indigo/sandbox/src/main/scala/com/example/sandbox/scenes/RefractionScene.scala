8348167a9ffe5fa03980e2a86b885cc95cf1c513
Dave Smith <davesmith.00000@gmail.com>
2021-03-14 15:28:43 +0000

    RefractionScene drawing, problem with lighting.

--- indigo/sandbox/src/main/scala/com/example/sandbox/scenes/RefractionScene.scala
+++ indigo/sandbox/src/main/scala/com/example/sandbox/scenes/RefractionScene.scala
@@ -5,6 +5,7 @@ import indigo.scenes._
 import com.example.sandbox.SandboxStartupData
 import com.example.sandbox.SandboxGameModel
 import com.example.sandbox.SandboxViewModel
+import com.example.sandbox.SandboxAssets
 
 object RefractionScene extends Scene[SandboxStartupData, SandboxGameModel, SandboxViewModel] {
 
@@ -32,9 +33,47 @@ object RefractionScene extends Scene[SandboxStartupData, SandboxGameModel, Sandb
   def updateViewModel(context: FrameContext[SandboxStartupData], model: SandboxGameModel, viewModel: SandboxViewModel): GlobalEvent => Outcome[SandboxViewModel] =
     _ => Outcome(viewModel)
 
-  def present(context: FrameContext[SandboxStartupData], model: SandboxGameModel, viewModel: SandboxViewModel): Outcome[SceneUpdateFragment] =
+  val graphic: Graphic =
+    Graphic(Rectangle(0, 0, 64, 64), 1, SandboxAssets.junctionBoxMaterial)
+      .withRef(20, 20)
+
+  val imageLight: Graphic =
+    Graphic(Rectangle(0, 0, 320, 240), 1, SandboxAssets.imageLightMaterial)
+      .moveBy(-14, -60)
+
+  val distortion: Graphic =
+    Graphic(Rectangle(0, 0, 64, 64), 1, SandboxAssets.smoothBumpMaterial)
+      .withRef(32, 32)
+
+  val background: Graphic =
+    Graphic(Rectangle(0, 0, 790, 380), 1, SandboxAssets.foliageMaterial)
+
+  def orbiting(distance: Int, around: Point): Signal[Graphic] =
+    Signal.Orbit(around, distance.toDouble).map { vec =>
+      distortion.moveTo(vec.toPoint)
+    }
+
+  def present(context: FrameContext[SandboxStartupData], model: SandboxGameModel, viewModel: SandboxViewModel): Outcome[SceneUpdateFragment] = {
+    val viewCenter: Point = context.startUpData.viewportCenter
+
     Outcome(
       SceneUpdateFragment.empty
+        .addLayers(
+          Layer(
+            background,
+            graphic.moveTo(viewCenter),
+            graphic.moveTo(viewCenter).moveBy(-60, 0).withMaterial(SandboxAssets.junctionBoxMaterial),
+            graphic.moveTo(viewCenter).moveBy(-30, 0).withMaterial(SandboxAssets.junctionBoxMaterial),
+            graphic.moveTo(viewCenter).moveBy(30, 0).withMaterial(SandboxAssets.junctionBoxMaterial),
+            graphic.moveTo(viewCenter).moveBy(60, 0).withMaterial(SandboxAssets.junctionBoxMaterial)
+          ).withMagnification(2),
+          Layer(imageLight).withBlending(Blending.Lighting),
+          Layer(
+            distortion.moveTo(viewCenter),
+            orbiting(40, viewCenter).affectTime(0.25).at(context.gameTime.running)
+          )
+        )
     )
+  }
 
 }
