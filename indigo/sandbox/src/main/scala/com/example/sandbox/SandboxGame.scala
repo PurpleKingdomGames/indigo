34e8b0dc4a05e5441cdd5170c33eb4ba2cd2a85e
Dave Smith <davesmith.00000@gmail.com>
2021-03-14 12:40:45 +0000

    Added Lighting/Refraction assets to sandbox

--- indigo/sandbox/src/main/scala/com/example/sandbox/SandboxGame.scala
+++ indigo/sandbox/src/main/scala/com/example/sandbox/SandboxGame.scala
@@ -25,7 +25,7 @@ object SandboxGame extends IndigoGame[SandboxBootData, SandboxStartupData, Sandb
   private val viewportHeight: Int     = 128 * magnificationLevel
 
   def initialScene(bootData: SandboxBootData): Option[SceneName] =
-    Some(RefractionScene.name)
+    Some(OriginalScene.name)
 
   def scenes(bootData: SandboxBootData): NonEmptyList[Scene[SandboxStartupData, SandboxGameModel, SandboxViewModel]] =
     NonEmptyList(OriginalScene, ShapesScene, LightsScene, RefractionScene)
