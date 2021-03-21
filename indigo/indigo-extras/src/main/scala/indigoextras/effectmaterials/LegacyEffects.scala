dacbe69133685d5119c583f95a29f1bc4f80ea07
Dave Smith <davesmith.00000@gmail.com>
2021-03-21 17:00:32 +0000

    Standard effects examples all working.

---
+++ indigo/indigo-extras/src/main/scala/indigoextras/effectmaterials/LegacyEffects.scala
@@ -0,0 +1,100 @@
+package indigoextras.effectmaterials
+
+import indigo.shared.assets.AssetName
+import indigo.shared.materials.Material
+import indigo.shared.materials.ShaderData
+import indigoextras.shaders.ExtrasShaderLibrary
+import indigo.shared.shader.ShaderPrimitive.{vec3, vec4}
+import indigo.shared.datatypes.RGBA
+import indigo.shared.shader.UniformBlock
+import indigo.shared.shader.Uniform
+import indigo.shared.shader.EntityShader
+import indigo.shaders.ShaderLibrary
+import indigo.shared.shader.ShaderId
+import indigo.shared.shader.ShaderPrimitive
+import indigo.shared.datatypes.Fill
+import indigo.shared.datatypes.RGB
+
+final case class LegacyEffects(diffuse: AssetName, alpha: Double, tint: RGBA, overlay: Fill, saturation: Double) extends Material {
+
+  def withAlpha(newAlpha: Double): LegacyEffects =
+    this.copy(alpha = newAlpha)
+
+  def withTint(newTint: RGBA): LegacyEffects =
+    this.copy(tint = newTint)
+  def withTint(newTint: RGB): LegacyEffects =
+    this.copy(tint = newTint.toRGBA)
+
+  def withOverlay(newOverlay: Fill): LegacyEffects =
+    this.copy(overlay = newOverlay)
+
+  def withSaturation(newSaturation: Double): LegacyEffects =
+    this.copy(saturation = newSaturation)
+
+  def toShaderData: ShaderData = {
+    val gradientUniforms: List[(Uniform, ShaderPrimitive)] =
+      overlay match {
+        case Fill.Color(color) =>
+          val c = vec4(color.r, color.g, color.b, color.a)
+          List(
+            Uniform("GRADIENT_FROM_TO")    -> vec4(0.0d),
+            Uniform("GRADIENT_FROM_COLOR") -> c,
+            Uniform("GRADIENT_TO_COLOR")   -> c
+          )
+
+        case Fill.LinearGradient(fromPoint, fromColor, toPoint, toColor) =>
+          List(
+            Uniform("GRADIENT_FROM_TO")    -> vec4(fromPoint.x.toDouble, fromPoint.y.toDouble, toPoint.x.toDouble, toPoint.y.toDouble),
+            Uniform("GRADIENT_FROM_COLOR") -> vec4(fromColor.r, fromColor.g, fromColor.b, fromColor.a),
+            Uniform("GRADIENT_TO_COLOR")   -> vec4(toColor.r, toColor.g, toColor.b, toColor.a)
+          )
+
+        case Fill.RadialGradient(fromPoint, fromColor, toPoint, toColor) =>
+          List(
+            Uniform("GRADIENT_FROM_TO")    -> vec4(fromPoint.x.toDouble, fromPoint.y.toDouble, toPoint.x.toDouble, toPoint.y.toDouble),
+            Uniform("GRADIENT_FROM_COLOR") -> vec4(fromColor.r, fromColor.g, fromColor.b, fromColor.a),
+            Uniform("GRADIENT_TO_COLOR")   -> vec4(toColor.r, toColor.g, toColor.b, toColor.a)
+          )
+      }
+
+    val overlayType: Double =
+      overlay match {
+        case _: Fill.Color          => 0.0
+        case _: Fill.LinearGradient => 1.0
+        case _: Fill.RadialGradient => 2.0
+      }
+
+    ShaderData(
+      LegacyEffects.entityShader.id,
+      Some(
+        UniformBlock(
+          "IndigoImageEffectsData",
+          List(
+            Uniform("ALPHA_SATURATION_OVERLAYTYPE") -> vec3(alpha, saturation, overlayType),
+            Uniform("TINT")                         -> vec4(tint.r, tint.g, tint.b, tint.a)
+          ) ++ gradientUniforms
+        )
+      ),
+      Some(diffuse),
+      None,
+      None,
+      None
+    )
+  }
+}
+object LegacyEffects {
+
+  val entityShader: EntityShader.Source =
+    EntityShader.Source(
+      id = ShaderId("[indigoextras_engine_legacy_effects]"),
+      vertex = ShaderLibrary.NoOpVertex,
+      fragment = ExtrasShaderLibrary.LegacyEffectsFragment,
+      light = ShaderLibrary.NoOpLight
+    )
+
+  def apply(diffuse: AssetName): LegacyEffects =
+    LegacyEffects(diffuse, 1.0, RGBA.None, Fill.Color.default, 1.0)
+
+  def apply(diffuse: AssetName, alpha: Double): LegacyEffects =
+    LegacyEffects(diffuse, alpha, RGBA.None, Fill.Color.default, 1.0)
+}
---
+++ indigo/sandbox/src/main/scala/com/example/sandbox/scenes/LegacyEffectsScene.scala
@@ -0,0 +1,118 @@
+package com.example.sandbox.scenes
+
+import indigo._
+import indigo.scenes._
+import com.example.sandbox.SandboxStartupData
+import com.example.sandbox.SandboxGameModel
+import com.example.sandbox.SandboxViewModel
+import com.example.sandbox.SandboxAssets
+import indigoextras.effectmaterials.LegacyEffects
+
+object LegacyEffectsScene extends Scene[SandboxStartupData, SandboxGameModel, SandboxViewModel] {
+
+  type SceneModel     = SandboxGameModel
+  type SceneViewModel = SandboxViewModel
+
+  def eventFilters: EventFilters =
+    EventFilters.Restricted
+
+  def modelLens: indigo.scenes.Lens[SandboxGameModel, SandboxGameModel] =
+    Lens.keepOriginal
+
+  def viewModelLens: Lens[SandboxViewModel, SandboxViewModel] =
+    Lens.keepOriginal
+
+  def name: SceneName =
+    SceneName("legacy effects")
+
+  def subSystems: Set[SubSystem] =
+    Set()
+
+  def updateModel(context: FrameContext[SandboxStartupData], model: SandboxGameModel): GlobalEvent => Outcome[SandboxGameModel] =
+    _ => Outcome(model)
+
+  def updateViewModel(context: FrameContext[SandboxStartupData], model: SandboxGameModel, viewModel: SandboxViewModel): GlobalEvent => Outcome[SandboxViewModel] =
+    _ => Outcome(viewModel)
+
+  val graphic: Graphic =
+    Graphic(Rectangle(0, 0, 40, 40), 1, SandboxAssets.junctionBoxEffectsMaterial)
+      .withRef(20, 20)
+
+  def present(context: FrameContext[SandboxStartupData], model: SandboxGameModel, viewModel: SandboxViewModel): Outcome[SceneUpdateFragment] = {
+    val viewCenter: Point = context.startUpData.viewportCenter + Point(0, -25)
+
+    Outcome(
+      SceneUpdateFragment(
+        graphic // tint - identical to ImageEffects material
+          .moveTo(viewCenter)
+          .moveBy(0, -40)
+          .modifyMaterial {
+            case m: LegacyEffects => m.withTint(RGBA.Red)
+            case m                => m
+          },
+        graphic // alpha - identical to ImageEffects material
+          .moveTo(viewCenter)
+          .moveBy(-60, -40)
+          .modifyMaterial {
+            case m: LegacyEffects => m.withAlpha(0.5)
+            case m                => m
+          },
+        graphic // saturation - identical to ImageEffects material
+          .moveTo(viewCenter)
+          .moveBy(-30, -40)
+          .modifyMaterial {
+            case m: LegacyEffects => m.withSaturation(0.0)
+            case m                => m
+          },
+        graphic //color overlay - identical to ImageEffects material
+          .moveTo(viewCenter)
+          .moveBy(30, -40)
+          .modifyMaterial {
+            case m: LegacyEffects => m.withOverlay(Fill.Color(RGBA.Magenta.withAmount(0.75)))
+            case m                => m
+          },
+        graphic // linear gradient overlay - identical to ImageEffects material
+          .moveTo(viewCenter)
+          .moveBy(60, -40)
+          .modifyMaterial {
+            case m: LegacyEffects => m.withOverlay(Fill.LinearGradient(Point.zero, RGBA.Magenta, Point(40), RGBA.Cyan.withAmount(0.5)))
+            case m                => m
+          },
+        graphic // radial gradient overlay - identical to ImageEffects material
+          .moveTo(viewCenter)
+          .moveBy(-60, 10)
+          .modifyMaterial {
+            case m: LegacyEffects => m.withOverlay(Fill.RadialGradient(Point(20), 10, RGBA.Magenta.withAmount(0.5), RGBA.Cyan.withAmount(0.25)))
+            case m                => m
+          },
+        graphic // inner glow
+          .moveTo(viewCenter)
+          .moveBy(0, 10),
+        // .withGlow(Glow(RGBA.Green, 2.0, 0.0)),
+        graphic // outer glow
+          .moveTo(viewCenter)
+          .moveBy(-30, 10),
+        // .withGlow(Glow(RGBA.Blue, 0.0, 2.0)),
+        graphic // inner border
+          .moveTo(viewCenter)
+          .moveBy(30, 60),
+        // .withBorder(Border(RGBA(1.0, 0.5, 0.0, 1.0), Thickness.Thick, Thickness.Thick)),
+        graphic // outer border
+          .moveTo(viewCenter)
+          .moveBy(60, 60),
+        // .withBorder(Border(RGBA.Yellow, Thickness.Thick, Thickness.None)),
+        graphic // rotate & scale - standard transform
+          .moveTo(viewCenter)
+          .moveBy(30, 10)
+          .rotateBy(Radians(0.2))
+          .scaleBy(1.25, 1.25),
+        graphic // flipped - standard transform
+          .moveTo(viewCenter)
+          .moveBy(60, 10)
+          .flipHorizontal(true)
+          .flipVertical(true)
+      )
+    )
+  }
+
+}
