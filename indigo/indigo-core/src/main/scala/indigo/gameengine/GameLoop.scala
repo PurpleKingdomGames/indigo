b0c024505f9d6bb2dbcd74e30f81f48f93aacb4b
Dave Smith <davesmith.00000@gmail.com>
2021-04-14 23:16:52 +0100

    Removing old commented out code

--- indigo/indigo-core/src/main/scala/indigo/gameengine/GameLoop.scala
+++ indigo/indigo-core/src/main/scala/indigo/gameengine/GameLoop.scala
@@ -80,9 +80,6 @@ class GameLoop[StartUpData, GameModel, ViewModel](
             }
 
           // Process events
-          // SceneGraphViewEvents.collectViewEvents(boundaryLocator, scene.gameLayer.nodes, collectedEvents, gameEngine.globalEventStream.pushGlobalEvent)
-          // SceneGraphViewEvents.collectViewEvents(boundaryLocator, scene.lightingLayer.nodes, collectedEvents, gameEngine.globalEventStream.pushGlobalEvent)
-          // SceneGraphViewEvents.collectViewEvents(boundaryLocator, scene.uiLayer.nodes, collectedEvents, gameEngine.globalEventStream.pushGlobalEvent)
           scene.layers.foreach { layer =>
             SceneGraphViewEvents.collectViewEvents(boundaryLocator, layer.nodes, collectedEvents, gameEngine.globalEventStream.pushGlobalEvent)
           }
@@ -94,10 +91,7 @@ class GameLoop[StartUpData, GameModel, ViewModel](
           val sceneData = sceneProcessor.processScene(
             gameTime,
             scene,
-            gameEngine.assetMapping //,
-            // gameEngine.renderer.screenWidth.toDouble,
-            // gameEngine.renderer.screenHeight.toDouble,
-            // gameEngine.renderer.orthographicProjectionMatrix
+            gameEngine.assetMapping
           )
 
           // Render scene
