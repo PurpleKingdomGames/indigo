a06e5f2498c6683e087f6b32f8965e41c1e72cb1
Dave Smith <davesmith.00000@gmail.com>
2021-05-12 23:44:26 +0100

    Split primitives entities into separate files

---
+++ indigo/indigo/src/main/scala/indigo/shared/scenegraph/Clone.scala
@@ -0,0 +1,143 @@
+package indigo.shared.scenegraph
+
+import indigo.shared.datatypes._
+
+/** A single cloned instance of a cloneblank
+  *
+  * @param id
+  * @param depth
+  * @param transform
+  */
+final case class Clone(id: CloneId, depth: Depth, transform: CloneTransformData)
+    extends DependentNode
+    with BasicSpatialModifiers[Clone] derives CanEqual {
+  lazy val x: Int                  = transform.position.x
+  lazy val y: Int                  = transform.position.y
+  lazy val rotation: Radians       = transform.rotation
+  lazy val scale: Vector2          = transform.scale
+  lazy val flipHorizontal: Boolean = transform.flipHorizontal
+  lazy val flipVertical: Boolean   = transform.flipVertical
+
+  def position: Point = Point(transform.position.x, transform.position.y)
+  def flip: Flip      = Flip(transform.flipHorizontal, transform.flipVertical)
+
+  def withCloneId(newCloneId: CloneId): Clone =
+    this.copy(id = newCloneId)
+
+  def withDepth(newDepth: Depth): Clone =
+    this.copy(depth = newDepth)
+
+  def withTransforms(
+      newPosition: Point,
+      newRotation: Radians,
+      newScale: Vector2,
+      flipHorizontal: Boolean,
+      flipVertical: Boolean
+  ): Clone =
+    this.copy(transform = CloneTransformData(newPosition, newRotation, newScale, flipHorizontal, flipVertical))
+
+  def withPosition(newPosition: Point): Clone =
+    this.copy(transform = transform.withPosition(newPosition))
+
+  def withRotation(newRotation: Radians): Clone =
+    this.copy(transform = transform.withRotation(newRotation))
+
+  def withScale(newScale: Vector2): Clone =
+    this.copy(transform = transform.withScale(newScale))
+
+  def withHorizontalFlip(isFlipped: Boolean): Clone =
+    this.copy(transform = transform.withHorizontalFlip(isFlipped))
+
+  def withVerticalFlip(isFlipped: Boolean): Clone =
+    this.copy(transform = transform.withVerticalFlip(isFlipped))
+
+  def withFlip(newFlip: Flip): Clone =
+    this.copy(
+      transform = transform
+        .withVerticalFlip(newFlip.vertical)
+        .withHorizontalFlip(newFlip.horizontal)
+    )
+}
+object Clone {
+  def apply(id: CloneId): Clone =
+    Clone(id, Depth(1), CloneTransformData.identity)
+}
+
+/** Represents many clones of the same cloneblank, differentiated only by their transform data.
+  *
+  * @param id
+  *   @param depth
+  * @param transform
+  *   @param clones
+  * @param staticBatchKey
+  */
+final case class CloneBatch(
+    id: CloneId,
+    depth: Depth,
+    transform: CloneTransformData,
+    clones: List[CloneTransformData],
+    staticBatchKey: Option[BindingKey]
+) extends DependentNode
+    with BasicSpatialModifiers[CloneBatch] derives CanEqual {
+  lazy val x: Int                  = transform.position.x
+  lazy val y: Int                  = transform.position.y
+  lazy val rotation: Radians       = transform.rotation
+  lazy val scale: Vector2          = transform.scale
+  lazy val flipHorizontal: Boolean = transform.flipHorizontal
+  lazy val flipVertical: Boolean   = transform.flipVertical
+
+  def position: Point = Point(transform.position.x, transform.position.y)
+  def flip: Flip      = Flip(transform.flipHorizontal, transform.flipVertical)
+
+  def withCloneId(newCloneId: CloneId): CloneBatch =
+    this.copy(id = newCloneId)
+
+  def withDepth(newDepth: Depth): CloneBatch =
+    this.copy(depth = newDepth)
+
+  def withTransforms(
+      newPosition: Point,
+      newRotation: Radians,
+      newScale: Vector2,
+      flipHorizontal: Boolean,
+      flipVertical: Boolean
+  ): CloneBatch =
+    this.copy(transform = CloneTransformData(newPosition, newRotation, newScale, flipHorizontal, flipVertical))
+
+  def withPosition(newPosition: Point): CloneBatch =
+    this.copy(transform = transform.withPosition(newPosition))
+
+  def withRotation(newRotation: Radians): CloneBatch =
+    this.copy(transform = transform.withRotation(newRotation))
+
+  def withScale(newScale: Vector2): CloneBatch =
+    this.copy(transform = transform.withScale(newScale))
+
+  def withHorizontalFlip(isFlipped: Boolean): CloneBatch =
+    this.copy(transform = transform.withHorizontalFlip(isFlipped))
+
+  def withVerticalFlip(isFlipped: Boolean): CloneBatch =
+    this.copy(transform = transform.withVerticalFlip(isFlipped))
+
+  def withFlip(newFlip: Flip): CloneBatch =
+    this.copy(
+      transform = transform
+        .withVerticalFlip(newFlip.vertical)
+        .withHorizontalFlip(newFlip.horizontal)
+    )
+
+  def withClones(newClones: List[CloneTransformData]): CloneBatch =
+    this.copy(clones = newClones)
+
+  def addClones(additionalClones: List[CloneTransformData]): CloneBatch =
+    this.copy(clones = clones ++ additionalClones)
+
+  def withMaybeStaticBatchKey(maybeKey: Option[BindingKey]): CloneBatch =
+    this.copy(staticBatchKey = maybeKey)
+
+  def withStaticBatchKey(key: BindingKey): CloneBatch =
+    withMaybeStaticBatchKey(Option(key))
+
+  def clearStaticBatchKey: CloneBatch =
+    withMaybeStaticBatchKey(None)
+}
---
+++ indigo/indigo/src/main/scala/indigo/shared/scenegraph/Text.scala
@@ -0,0 +1,139 @@
+package indigo.shared.scenegraph
+
+import indigo.shared.datatypes._
+import indigo.shared.materials.Material
+import indigo.shared.materials.ShaderData
+import indigo.shared.events.GlobalEvent
+import indigo.shared.BoundaryLocator
+
+/** Used to draw text onto the screen.
+  */
+final case class Text(
+    text: String,
+    alignment: TextAlignment,
+    fontKey: FontKey,
+    material: Material,
+    eventHandler: ((Rectangle, GlobalEvent)) => List[GlobalEvent],
+    position: Point,
+    rotation: Radians,
+    scale: Vector2,
+    depth: Depth,
+    ref: Point,
+    flip: Flip
+) extends CompositeNode
+    with EventHandler
+    with SpatialModifiers[Text] derives CanEqual {
+
+  def calculatedBounds(locator: BoundaryLocator): Option[Rectangle] =
+    locator.findBounds(this)
+
+  lazy val x: Int = position.x
+  lazy val y: Int = position.y
+
+  def withMaterial(newMaterial: Material): Text =
+    this.copy(material = newMaterial)
+
+  def modifyMaterial(alter: Material => Material): Text =
+    this.copy(material = alter(material))
+
+  def moveTo(pt: Point): Text =
+    this.copy(position = pt)
+  def moveTo(x: Int, y: Int): Text =
+    moveTo(Point(x, y))
+  def withPosition(newPosition: Point): Text =
+    moveTo(newPosition)
+
+  def moveBy(pt: Point): Text =
+    this.copy(position = position + pt)
+  def moveBy(x: Int, y: Int): Text =
+    moveBy(Point(x, y))
+
+  def rotateTo(angle: Radians): Text =
+    this.copy(rotation = angle)
+  def rotateBy(angle: Radians): Text =
+    rotateTo(rotation + angle)
+  def withRotation(newRotation: Radians): Text =
+    rotateTo(newRotation)
+
+  def scaleBy(amount: Vector2): Text =
+    this.copy(scale = scale * amount)
+  def scaleBy(x: Double, y: Double): Text =
+    scaleBy(Vector2(x, y))
+  def withScale(newScale: Vector2): Text =
+    this.copy(scale = newScale)
+
+  def transformTo(newPosition: Point, newRotation: Radians, newScale: Vector2): Text =
+    this.copy(position = newPosition, rotation = newRotation, scale = newScale)
+
+  def transformBy(positionDiff: Point, rotationDiff: Radians, scaleDiff: Vector2): Text =
+    transformTo(position + positionDiff, rotation + rotationDiff, scale * scaleDiff)
+
+  def withDepth(newDepth: Depth): Text =
+    this.copy(depth = newDepth)
+
+  def withRef(newRef: Point): Text =
+    this.copy(ref = newRef)
+  def withRef(x: Int, y: Int): Text =
+    withRef(Point(x, y))
+
+  def flipHorizontal(isFlipped: Boolean): Text =
+    this.copy(flip = flip.withHorizontalFlip(isFlipped))
+  def flipVertical(isFlipped: Boolean): Text =
+    this.copy(flip = flip.withVerticalFlip(isFlipped))
+  def withFlip(newFlip: Flip): Text =
+    this.copy(flip = newFlip)
+
+  def withAlignment(newAlignment: TextAlignment): Text =
+    this.copy(alignment = newAlignment)
+
+  def alignLeft: Text =
+    this.copy(alignment = TextAlignment.Left)
+  def alignCenter: Text =
+    this.copy(alignment = TextAlignment.Center)
+  def alignRight: Text =
+    this.copy(alignment = TextAlignment.Right)
+
+  def withText(newText: String): Text =
+    this.copy(text = newText)
+
+  def withFontKey(newFontKey: FontKey): Text =
+    this.copy(fontKey = newFontKey)
+
+  def onEvent(e: ((Rectangle, GlobalEvent)) => List[GlobalEvent]): Text =
+    this.copy(eventHandler = e)
+
+}
+
+object Text {
+
+  def apply(text: String, x: Int, y: Int, depth: Int, fontKey: FontKey, material: Material): Text =
+    Text(
+      position = Point(x, y),
+      rotation = Radians.zero,
+      scale = Vector2.one,
+      depth = Depth(depth),
+      ref = Point.zero,
+      flip = Flip.default,
+      text = text,
+      alignment = TextAlignment.Left,
+      fontKey = fontKey,
+      eventHandler = (_: (Rectangle, GlobalEvent)) => Nil,
+      material = material
+    )
+
+  def apply(text: String, fontKey: FontKey, material: Material): Text =
+    Text(
+      position = Point.zero,
+      rotation = Radians.zero,
+      scale = Vector2.one,
+      depth = Depth(1),
+      ref = Point.zero,
+      flip = Flip.default,
+      text = text,
+      alignment = TextAlignment.Left,
+      fontKey = fontKey,
+      eventHandler = (_: (Rectangle, GlobalEvent)) => Nil,
+      material = material
+    )
+
+}
