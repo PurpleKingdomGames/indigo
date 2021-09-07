f34411434f11949610c85ef3ef1c808eb30b1f07
Dave Smith <davesmith.00000@gmail.com>
2021-09-07 20:35:28 +0100

    New shader id constructors

--- indigo/indigo/src/main/scala/indigo/shared/shader/ShaderId.scala
+++ indigo/indigo/src/main/scala/indigo/shared/shader/ShaderId.scala
@@ -1,7 +1,14 @@
 package indigo.shared.shader
 
 opaque type ShaderId = String
+
 object ShaderId:
-  def apply(value: String): ShaderId = value
-  given CanEqual[ShaderId, ShaderId] = CanEqual.derived
+  def apply(value: String): ShaderId =
+    value
+  def fromDice(dice: Dice): ShaderId =
+    ShaderId(dice.rollAlphaNumeric)
+  def generate(dice: Dice): ShaderId =
+    fromDice(dice)
+
+  given CanEqual[ShaderId, ShaderId]                 = CanEqual.derived
   given CanEqual[Option[ShaderId], Option[ShaderId]] = CanEqual.derived
