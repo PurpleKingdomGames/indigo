0233ba6cbc7423c74eab3cbbc019969055d23b82
Dave Smith <davesmith.00000@gmail.com>
2021-09-07 20:35:47 +0100

    Improved BindingKey constructors

--- indigo/indigo/src/main/scala/indigo/shared/datatypes/BindingKey.scala
+++ indigo/indigo/src/main/scala/indigo/shared/datatypes/BindingKey.scala
@@ -5,9 +5,12 @@ import indigo.shared.dice.Dice
 opaque type BindingKey = String
 
 object BindingKey:
-  def apply(value: String): BindingKey = value
+  def apply(value: String): BindingKey =
+    value
   def fromDice(dice: Dice): BindingKey =
     BindingKey(dice.rollAlphaNumeric)
+  def generate(dice: Dice): BindingKey =
+    fromDice(dice)
 
   given CanEqual[BindingKey, BindingKey] = CanEqual.derived
   given CanEqual[Option[BindingKey], Option[BindingKey]] = CanEqual.derived
