e3fe894c2f4acdd234a621665e5e7561a216888c
Dave Smith <davesmith.00000@gmail.com>
2021-10-02 16:31:53 +0100

    Matrix4 is opaque type

--- indigo/indigo/src/main/scala/indigo/shared/datatypes/Matrix4.scala
+++ indigo/indigo/src/main/scala/indigo/shared/datatypes/Matrix4.scala
@@ -2,79 +2,201 @@ package indigo.shared.datatypes
 
 import util.control.Breaks._
 
-final case class Matrix4(private val mat: List[Double]) derives CanEqual {
+opaque type Matrix4 = List[Double]
 
-  lazy val row1: List[Double] = List(mat(0), mat(1), mat(2), mat(3))
-  lazy val row2: List[Double] = List(mat(4), mat(5), mat(6), mat(7))
-  lazy val row3: List[Double] = List(mat(8), mat(9), mat(10), mat(11))
-  lazy val row4: List[Double] = List(mat(12), mat(13), mat(14), mat(15))
+object Matrix4:
 
-  lazy val col1: List[Double] = List(mat(0), mat(4), mat(8), mat(12))
-  lazy val col2: List[Double] = List(mat(1), mat(5), mat(9), mat(13))
-  lazy val col3: List[Double] = List(mat(2), mat(6), mat(10), mat(14))
-  lazy val col4: List[Double] = List(mat(3), mat(7), mat(11), mat(15))
+  extension (m: Matrix4)
+    inline def toList: List[Double] =
+      m
 
-  lazy val x: Double = mat(12)
-  lazy val y: Double = mat(13)
-  lazy val z: Double = mat(14)
+    inline def row1: List[Double] = List(m(0), m(1), m(2), m(3))
+    inline def row2: List[Double] = List(m(4), m(5), m(6), m(7))
+    inline def row3: List[Double] = List(m(8), m(9), m(10), m(11))
+    inline def row4: List[Double] = List(m(12), m(13), m(14), m(15))
+    inline def col1: List[Double] = List(m(0), m(4), m(8), m(12))
+    inline def col2: List[Double] = List(m(1), m(5), m(9), m(13))
+    inline def col3: List[Double] = List(m(2), m(6), m(10), m(14))
+    inline def col4: List[Double] = List(m(3), m(7), m(11), m(15))
+    inline def x: Double          = m(12)
+    inline def y: Double          = m(13)
+    inline def z: Double          = m(14)
+    inline def data: (List[Double], List[Double]) =
+      (List(m(0), m(1), m(4), m(5)), List(m(12), m(13), m(14)))
 
-  lazy val data: (List[Double], List[Double]) =
-    (List(mat(0), mat(1), mat(4), mat(5)), List(mat(12), mat(13), mat(14)))
+    def translate(by: Vector3): Matrix4 =
+      translate(by.x, by.y, by.z)
+    def translate(byX: Double, byY: Double, byZ: Double): Matrix4 =
+      m * Matrix4(
+        1,
+        0,
+        0,
+        0,
+        0,
+        1,
+        0,
+        0,
+        0,
+        0,
+        1,
+        0,
+        byX,
+        byY,
+        byZ,
+        1
+      )
 
-  def translate(by: Vector3): Matrix4 =
-    translate(by.x, by.y, by.z)
-  def translate(byX: Double, byY: Double, byZ: Double): Matrix4 =
-    this * Matrix4.translate(byX, byY, byZ)
+    def rotate(angle: Radians): Matrix4 =
+      val c = Math.cos(angle.toDouble)
+      val s = Math.sin(angle.toDouble)
 
-  def rotate(angle: Radians): Matrix4 =
-    this * Matrix4.rotation(angle)
+      m * Matrix4(
+        c,
+        s,
+        0,
+        0,
+        -s,
+        c,
+        0,
+        0,
+        0,
+        0,
+        1,
+        0,
+        0,
+        0,
+        0,
+        1
+      )
 
-  def scale(by: Vector2): Matrix4 =
-    scale(by.x, by.y, 1.0d)
-  def scale(by: Vector3): Matrix4 =
-    scale(by.x, by.y, by.z)
-  def scale(byX: Double, byY: Double, byZ: Double): Matrix4 =
-    this * Matrix4.scale(byX, byY, byZ)
+    def scale(by: Vector2): Matrix4 =
+      scale(by.x, by.y, 1.0d)
+    def scale(by: Vector3): Matrix4 =
+      scale(by.x, by.y, by.z)
+    def scale(byX: Double, byY: Double, byZ: Double): Matrix4 =
+      m * Matrix4(
+        byX,
+        0,
+        0,
+        0,
+        0,
+        byY,
+        0,
+        0,
+        0,
+        0,
+        byZ,
+        0,
+        0,
+        0,
+        0,
+        1
+      )
 
-  def transpose: Matrix4 = Matrix4.transpose(this)
+    def transpose: Matrix4 =
+      Matrix4(
+        m(0),
+        m(4),
+        m(8),
+        m(12),
+        m(1),
+        m(5),
+        m(9),
+        m(13),
+        m(2),
+        m(6),
+        m(10),
+        m(14),
+        m(3),
+        m(7),
+        m(11),
+        m(15)
+      )
 
-  def *(other: Matrix4): Matrix4 = Matrix4.multiply(this, other)
+    def *(other: Matrix4): Matrix4 =
+      val listA = m.toList
+      val listB = other.toList
 
-  def toList: List[Double] =
-    mat
+      val a00 = listA(0 * 4 + 0)
+      val a01 = listA(0 * 4 + 1)
+      val a02 = listA(0 * 4 + 2)
+      val a03 = listA(0 * 4 + 3)
+      val a10 = listA(1 * 4 + 0)
+      val a11 = listA(1 * 4 + 1)
+      val a12 = listA(1 * 4 + 2)
+      val a13 = listA(1 * 4 + 3)
+      val a20 = listA(2 * 4 + 0)
+      val a21 = listA(2 * 4 + 1)
+      val a22 = listA(2 * 4 + 2)
+      val a23 = listA(2 * 4 + 3)
+      val a30 = listA(3 * 4 + 0)
+      val a31 = listA(3 * 4 + 1)
+      val a32 = listA(3 * 4 + 2)
+      val a33 = listA(3 * 4 + 3)
 
-  def transform(vector: Vector3): Vector3 =
-    Vector3(
-      x = col1(0) * vector.x + col1(1) * vector.y + col1(2) * vector.z + col1(3),
-      y = col2(0) * vector.x + col2(1) * vector.y + col2(2) * vector.z + col2(3),
-      z = col3(0) * vector.x + col3(1) * vector.y + col3(2) * vector.z + col3(3)
-    )
+      val b00 = listB(0 * 4 + 0)
+      val b01 = listB(0 * 4 + 1)
+      val b02 = listB(0 * 4 + 2)
+      val b03 = listB(0 * 4 + 3)
+      val b10 = listB(1 * 4 + 0)
+      val b11 = listB(1 * 4 + 1)
+      val b12 = listB(1 * 4 + 2)
+      val b13 = listB(1 * 4 + 3)
+      val b20 = listB(2 * 4 + 0)
+      val b21 = listB(2 * 4 + 1)
+      val b22 = listB(2 * 4 + 2)
+      val b23 = listB(2 * 4 + 3)
+      val b30 = listB(3 * 4 + 0)
+      val b31 = listB(3 * 4 + 1)
+      val b32 = listB(3 * 4 + 2)
+      val b33 = listB(3 * 4 + 3)
 
-  def prettyPrint: String =
-    row1.mkString("(", ",\t", ")") + "\n" +
-      row2.mkString("(", ",\t", ")") + "\n" +
-      row3.mkString("(", ",\t", ")") + "\n" +
-      row4.mkString("(", ",\t", ")")
+      Matrix4(
+        a00 * b00 + a01 * b10 + a02 * b20 + a03 * b30,
+        a00 * b01 + a01 * b11 + a02 * b21 + a03 * b31,
+        a00 * b02 + a01 * b12 + a02 * b22 + a03 * b32,
+        a00 * b03 + a01 * b13 + a02 * b23 + a03 * b33,
+        a10 * b00 + a11 * b10 + a12 * b20 + a13 * b30,
+        a10 * b01 + a11 * b11 + a12 * b21 + a13 * b31,
+        a10 * b02 + a11 * b12 + a12 * b22 + a13 * b32,
+        a10 * b03 + a11 * b13 + a12 * b23 + a13 * b33,
+        a20 * b00 + a21 * b10 + a22 * b20 + a23 * b30,
+        a20 * b01 + a21 * b11 + a22 * b21 + a23 * b31,
+        a20 * b02 + a21 * b12 + a22 * b22 + a23 * b32,
+        a20 * b03 + a21 * b13 + a22 * b23 + a23 * b33,
+        a30 * b00 + a31 * b10 + a32 * b20 + a33 * b30,
+        a30 * b01 + a31 * b11 + a32 * b21 + a33 * b31,
+        a30 * b02 + a31 * b12 + a32 * b22 + a33 * b32,
+        a30 * b03 + a31 * b13 + a32 * b23 + a33 * b33
+      )
 
-  @SuppressWarnings(Array("scalafix:DisableSyntax.var"))
-  def ~==(other: Matrix4): Boolean =
-    if (mat.length == other.mat.length) {
-      var count = mat.length - 1
-      var same  = true
-      while (count > 0) {
-        breakable {
-          if (Math.abs(mat(count) - other.mat(count)) > 0.001)
-            same = false
-          break()
+    def transform(vector: Vector3): Vector3 =
+      Vector3(
+        x = col1(0) * vector.x + col1(1) * vector.y + col1(2) * vector.z + col1(3),
+        y = col2(0) * vector.x + col2(1) * vector.y + col2(2) * vector.z + col2(3),
+        z = col3(0) * vector.x + col3(1) * vector.y + col3(2) * vector.z + col3(3)
+      )
+
+    def prettyPrint: String =
+      row1.mkString("(", ",\t", ")") + "\n" +
+        row2.mkString("(", ",\t", ")") + "\n" +
+        row3.mkString("(", ",\t", ")") + "\n" +
+        row4.mkString("(", ",\t", ")")
+
+    def ~==(other: Matrix4): Boolean =
+      if (m.length == other.toList.length) {
+        var count = m.length - 1
+        var same  = true
+        while (count > 0) {
+          breakable {
+            if (Math.abs(m(count) - other.toList(count)) > 0.001)
+              same = false
+            break()
+          }
+          count = count - 1
         }
-        count = count - 1
-      }
-      same
-    } else
-      false
-}
-
-object Matrix4 {
+        same
+      } else false
 
   val identity: Matrix4 =
     Matrix4(
@@ -92,10 +214,10 @@ object Matrix4 {
       (1, 1, 1, 1)
     )
 
-  /**
-    * Usage Matrix.projection(2 * aspectRatio, 2, 2) (assuming width > height) because the screen by default is 2 x 2 units: -1 to 1
+  /** Usage Matrix.projection(2 * aspectRatio, 2, 2) (assuming width > height) because the screen by default is 2 x 2
+    * units: -1 to 1
     */
-  def projection(width: Double, height: Double, depth: Double): Matrix4 =
+  inline def projection(width: Double, height: Double, depth: Double): Matrix4 =
     Matrix4(
       2 / width,
       0,
@@ -115,7 +237,7 @@ object Matrix4 {
       1
     )
 
-  def orthographic(left: Double, right: Double, bottom: Double, top: Double, near: Double, far: Double): Matrix4 =
+  inline def orthographic(left: Double, right: Double, bottom: Double, top: Double, near: Double, far: Double): Matrix4 =
     Matrix4(
       2 / (right - left),
       0,
@@ -135,163 +257,14 @@ object Matrix4 {
       1
     )
 
-  def orthographic(width: Double, height: Double): Matrix4 =
+  inline def orthographic(width: Double, height: Double): Matrix4 =
     orthographic(0, width, height, 0, -10000, 10000)
 
-  def translate(amount: Vector3): Matrix4 =
-    translate(amount.x, amount.y, amount.z)
-  def translate(tx: Double, ty: Double, tz: Double): Matrix4 =
-    Matrix4(
-      1,
-      0,
-      0,
-      0,
-      0,
-      1,
-      0,
-      0,
-      0,
-      0,
-      1,
-      0,
-      tx,
-      ty,
-      tz,
-      1
-    )
+  inline def apply(): Matrix4 = identity
+  inline def apply(matrix: List[Double]): Matrix4 =
+    matrix
 
-  /**
-    * This is really z-rotation
-    */
-  def rotation(angleInRadians: Radians): Matrix4 = {
-    val c = Math.cos(angleInRadians.toDouble)
-    val s = Math.sin(angleInRadians.toDouble)
-
-    Matrix4(
-      c,
-      s,
-      0,
-      0,
-      -s,
-      c,
-      0,
-      0,
-      0,
-      0,
-      1,
-      0,
-      0,
-      0,
-      0,
-      1
-    )
-  }
-
-  def scale(amount: Vector3): Matrix4 =
-    scale(amount.x, amount.y, amount.z)
-  def scale(sx: Double, sy: Double, sz: Double): Matrix4 =
-    Matrix4(
-      sx,
-      0,
-      0,
-      0,
-      0,
-      sy,
-      0,
-      0,
-      0,
-      0,
-      sz,
-      0,
-      0,
-      0,
-      0,
-      1
-    )
-
-  def multiply(a: Matrix4, b: Matrix4): Matrix4 = {
-    val listA = a.mat
-    val listB = b.mat
-
-    val a00 = listA(0 * 4 + 0)
-    val a01 = listA(0 * 4 + 1)
-    val a02 = listA(0 * 4 + 2)
-    val a03 = listA(0 * 4 + 3)
-    val a10 = listA(1 * 4 + 0)
-    val a11 = listA(1 * 4 + 1)
-    val a12 = listA(1 * 4 + 2)
-    val a13 = listA(1 * 4 + 3)
-    val a20 = listA(2 * 4 + 0)
-    val a21 = listA(2 * 4 + 1)
-    val a22 = listA(2 * 4 + 2)
-    val a23 = listA(2 * 4 + 3)
-    val a30 = listA(3 * 4 + 0)
-    val a31 = listA(3 * 4 + 1)
-    val a32 = listA(3 * 4 + 2)
-    val a33 = listA(3 * 4 + 3)
-
-    val b00 = listB(0 * 4 + 0)
-    val b01 = listB(0 * 4 + 1)
-    val b02 = listB(0 * 4 + 2)
-    val b03 = listB(0 * 4 + 3)
-    val b10 = listB(1 * 4 + 0)
-    val b11 = listB(1 * 4 + 1)
-    val b12 = listB(1 * 4 + 2)
-    val b13 = listB(1 * 4 + 3)
-    val b20 = listB(2 * 4 + 0)
-    val b21 = listB(2 * 4 + 1)
-    val b22 = listB(2 * 4 + 2)
-    val b23 = listB(2 * 4 + 3)
-    val b30 = listB(3 * 4 + 0)
-    val b31 = listB(3 * 4 + 1)
-    val b32 = listB(3 * 4 + 2)
-    val b33 = listB(3 * 4 + 3)
-
-    Matrix4(
-      a00 * b00 + a01 * b10 + a02 * b20 + a03 * b30,
-      a00 * b01 + a01 * b11 + a02 * b21 + a03 * b31,
-      a00 * b02 + a01 * b12 + a02 * b22 + a03 * b32,
-      a00 * b03 + a01 * b13 + a02 * b23 + a03 * b33,
-      a10 * b00 + a11 * b10 + a12 * b20 + a13 * b30,
-      a10 * b01 + a11 * b11 + a12 * b21 + a13 * b31,
-      a10 * b02 + a11 * b12 + a12 * b22 + a13 * b32,
-      a10 * b03 + a11 * b13 + a12 * b23 + a13 * b33,
-      a20 * b00 + a21 * b10 + a22 * b20 + a23 * b30,
-      a20 * b01 + a21 * b11 + a22 * b21 + a23 * b31,
-      a20 * b02 + a21 * b12 + a22 * b22 + a23 * b32,
-      a20 * b03 + a21 * b13 + a22 * b23 + a23 * b33,
-      a30 * b00 + a31 * b10 + a32 * b20 + a33 * b30,
-      a30 * b01 + a31 * b11 + a32 * b21 + a33 * b31,
-      a30 * b02 + a31 * b12 + a32 * b22 + a33 * b32,
-      a30 * b03 + a31 * b13 + a32 * b23 + a33 * b33
-    )
-  }
-
-  def transpose(matrix4: Matrix4): Matrix4 = {
-    val m = matrix4.mat
-    Matrix4(
-      m(0),
-      m(4),
-      m(8),
-      m(12),
-      m(1),
-      m(5),
-      m(9),
-      m(13),
-      m(2),
-      m(6),
-      m(10),
-      m(14),
-      m(3),
-      m(7),
-      m(11),
-      m(15)
-    )
-  }
-
-  def apply(): Matrix4 = identity
-
-  def apply(
+  inline def apply(
       row0: (Double, Double, Double, Double),
       row1: (Double, Double, Double, Double),
       row2: (Double, Double, Double, Double),
@@ -304,7 +277,7 @@ object Matrix4 {
         List(row3._1, row3._2, row3._3, row3._4)
     )
 
-  def apply(
+  inline def apply(
       a1: Double,
       a2: Double,
       a3: Double,
@@ -342,4 +315,4 @@ object Matrix4 {
         d4
       )
     )
-}
+end Matrix4
