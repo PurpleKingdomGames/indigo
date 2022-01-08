fbfb6f2f4476083cd3ba337b731cebbb26e04937
Dave Smith <davesmith.00000@gmail.com>
2022-01-08 10:56:24 +0000

    Fiexed #271: FPSCounter: Add move event

--- indigo/indigo-extras/src/main/scala/indigoextras/subsystems/FPSCounter.scala
+++ indigo/indigo-extras/src/main/scala/indigoextras/subsystems/FPSCounter.scala
@@ -20,119 +20,97 @@ import indigo.shared.subsystems.SubSystemFrameContext
 import indigo.shared.time.FPS
 import indigo.shared.time.Seconds
 
-object FPSCounter {
+final case class FPSCounter(
+    startPosition: Point,
+    targetFPS: FPS,
+    layerKey: Option[BindingKey],
+    fontFamily: FontFamily,
+    fontSize: Pixels
+) extends SubSystem:
+  type EventType      = GlobalEvent
+  type SubSystemModel = FPSCounterState
 
-  def apply(
-      position: Point,
-      targetFPS: FPS,
-      layerKey: Option[BindingKey],
-      fontFamily: FontFamily,
-      fontSize: Pixels
-  ): SubSystem =
-    SubSystem[GlobalEvent, FPSCounterState](
-      _eventFilter = eventFilter,
-      _initialModel = Outcome(FPSCounterState.default),
-      _update = update(targetFPS),
-      _present = present(position, targetFPS, layerKey, fontFamily, fontSize)
-    )
-
-  def apply(position: Point, targetFPS: FPS, layerKey: Option[BindingKey]): SubSystem =
-    SubSystem[GlobalEvent, FPSCounterState](
-      _eventFilter = eventFilter,
-      _initialModel = Outcome(FPSCounterState.default),
-      _update = update(targetFPS),
-      _present = present(position, targetFPS, layerKey, FontFamily.sansSerif, Pixels(12))
-    )
-
-  def apply(position: Point, targetFPS: FPS, layerKey: BindingKey): SubSystem =
-    SubSystem[GlobalEvent, FPSCounterState](
-      _eventFilter = eventFilter,
-      _initialModel = Outcome(FPSCounterState.default),
-      _update = update(targetFPS),
-      _present = present(position, targetFPS, Option(layerKey), FontFamily.sansSerif, Pixels(12))
-    )
-
-  def apply(position: Point, targetFPS: FPS): SubSystem =
-    SubSystem[GlobalEvent, FPSCounterState](
-      _eventFilter = eventFilter,
-      _initialModel = Outcome(FPSCounterState.default),
-      _update = update(targetFPS),
-      _present = present(position, targetFPS, None, FontFamily.sansSerif, Pixels(12))
-    )
-
-  lazy val eventFilter: GlobalEvent => Option[GlobalEvent] = {
-    case FrameTick => Option(FrameTick)
-    case _         => None
+  def eventFilter: GlobalEvent => Option[EventType] = {
+    case FrameTick          => Some(FrameTick)
+    case e: FPSCounter.Move => Some(e)
+    case _                  => None
   }
 
-  def update(targetFPS: FPS): (SubSystemFrameContext, FPSCounterState) => GlobalEvent => Outcome[FPSCounterState] =
-    (frameContext, model) => {
-      case FrameTick =>
-        if (frameContext.gameTime.running >= (model.lastInterval + Seconds(1)))
-          Outcome(
-            FPSCounterState(
-              fps = Math.min(targetFPS.toInt, model.frameCountSinceInterval + 1),
-              lastInterval = frameContext.gameTime.running,
-              frameCountSinceInterval = 0
-            )
+  def initialModel: Outcome[SubSystemModel] =
+    Outcome(FPSCounterState.initial(startPosition))
+
+  def update(context: SubSystemFrameContext, model: FPSCounterState): GlobalEvent => Outcome[FPSCounterState] = {
+    case FrameTick =>
+      if (context.gameTime.running >= (model.lastInterval + Seconds(1)))
+        Outcome(
+          model.copy(
+            fps = Math.min(targetFPS.toInt, model.frameCountSinceInterval + 1),
+            lastInterval = context.gameTime.running,
+            frameCountSinceInterval = 0
           )
-        else
-          Outcome(model.copy(frameCountSinceInterval = model.frameCountSinceInterval + 1))
-
-      case _ =>
-        Outcome(model)
-    }
-
-  def present(
-      position: Point,
-      targetFPS: FPS,
-      layerKey: Option[BindingKey],
-      fontFamily: FontFamily,
-      fontSize: Pixels
-  ): (SubSystemFrameContext, FPSCounterState) => Outcome[SceneUpdateFragment] =
-    (ctx, model) => {
-      val text: TextBox =
-        TextBox(s"""FPS ${model.fps.toString}""")
-          .withFontFamily(fontFamily)
-          .withColor(pickTint(targetFPS.toInt, model.fps))
-          .withFontSize(fontSize)
-          .moveTo(position + 2)
-
-      val size: Rectangle =
-        ctx.boundaryLocator
-          .measureText(text)
-
-      val boxSize =
-        ({ (s: Size) =>
-          Size(
-            if s.width  % 2 == 0 then s.width else s.width + 1,
-            if s.height % 2 == 0 then s.height else s.height + 1
-          )
-        })(size.expand(2).size)
-
-      val bg: Shape.Box =
-        Shape
-          .Box(Rectangle(position, boxSize), Fill.Color(RGBA.Black.withAlpha(0.5)))
-
-      Outcome(
-        SceneUpdateFragment(
-          layerKey match {
-            case None      => Layer(bg, text.withSize(size.size))
-            case Some(key) => Layer(key, bg, text.withSize(size.size))
-          }
         )
+      else
+        Outcome(model.copy(frameCountSinceInterval = model.frameCountSinceInterval + 1))
+
+    case FPSCounter.Move(to) =>
+      Outcome(model.copy(position = to))
+
+    case _ =>
+      Outcome(model)
+  }
+
+  def present(context: SubSystemFrameContext, model: FPSCounterState): Outcome[SceneUpdateFragment] =
+    val text: TextBox =
+      TextBox(s"""FPS ${model.fps.toString}""")
+        .withFontFamily(fontFamily)
+        .withColor(pickTint(targetFPS.toInt, model.fps))
+        .withFontSize(fontSize)
+        .moveTo(model.position + 2)
+
+    val size: Rectangle =
+      context.boundaryLocator
+        .measureText(text)
+
+    val boxSize =
+      ({ (s: Size) =>
+        Size(
+          if s.width  % 2 == 0 then s.width else s.width + 1,
+          if s.height % 2 == 0 then s.height else s.height + 1
+        )
+      })(size.expand(2).size)
+
+    val bg: Shape.Box =
+      Shape
+        .Box(Rectangle(model.position, boxSize), Fill.Color(RGBA.Black.withAlpha(0.5)))
+
+    Outcome(
+      SceneUpdateFragment(
+        layerKey match {
+          case None      => Layer(bg, text.withSize(size.size))
+          case Some(key) => Layer(key, bg, text.withSize(size.size))
+        }
       )
-    }
+    )
 
   def pickTint(targetFPS: Int, fps: Int): RGBA =
     if (fps > targetFPS - (targetFPS * 0.05)) RGBA.Green
     else if (fps > targetFPS / 2) RGBA.Yellow
     else RGBA.Red
 
-}
+object FPSCounter:
 
-final case class FPSCounterState(fps: Int, lastInterval: Seconds, frameCountSinceInterval: Int)
-object FPSCounterState {
-  def default: FPSCounterState =
-    FPSCounterState(0, Seconds.zero, 0)
-}
+  def apply(position: Point, targetFPS: FPS, layerKey: Option[BindingKey]): SubSystem =
+    FPSCounter(position, targetFPS, layerKey, FontFamily.sansSerif, Pixels(12))
+
+  def apply(position: Point, targetFPS: FPS, layerKey: BindingKey): SubSystem =
+    FPSCounter(position, targetFPS, Option(layerKey), FontFamily.sansSerif, Pixels(12))
+
+  def apply(position: Point, targetFPS: FPS): SubSystem =
+    FPSCounter(position, targetFPS, None, FontFamily.sansSerif, Pixels(12))
+
+  final case class Move(to: Point) extends GlobalEvent
+
+final case class FPSCounterState(position: Point, fps: Int, lastInterval: Seconds, frameCountSinceInterval: Int)
+object FPSCounterState:
+  def initial(initialPosition: Point): FPSCounterState =
+    FPSCounterState(initialPosition, 0, Seconds.zero, 0)
