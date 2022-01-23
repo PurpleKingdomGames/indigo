dc0aa728d5042e1e18c8df7e4bfb2a1446d92efd
Angelo Oparah <angelo.oparah@itv.com>
2022-01-23 09:42:55 +0000

    minor

--- indigo/project/Misc.scala
+++ indigo/project/Misc.scala
@@ -9,10 +9,6 @@ object Misc {
   lazy val code =
     taskKey[Unit]("Launch VSCode in the current directory")
 
-  // Define task to copy html files
-  lazy val copyApiDocs =
-    taskKey[Unit]("Copy html files from src/main/html to cross-version target directory")
-
   def codeTaskDefinition: Int = {
     val command = Seq("code", ".")
     val run = sys.props("os.name").toLowerCase match {
@@ -20,16 +16,6 @@ object Misc {
       case _                         => command
     }
     run.!
-  }
-
-  def copyApiDocsTaskDefinition(file: File): Unit = {
-
-    println("Copy docs from 'target/scala-3.1.0/unidoc' to 'target/scala-3.1.0/site-docs/api'")
-
-    val src = file / "scala-3.1.0" / "unidoc"
-    val dst = file / "scala-3.1.0" / "site-docs" / "api"
-
-    IO.copyDirectory(src, dst)
   }
 
   lazy val customTasksAliases = Seq(
