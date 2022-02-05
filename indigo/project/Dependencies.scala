341f280dbae12f000e6e98135adf2d0df334f990
Dave Smith <davesmith.00000@gmail.com>
2022-02-05 21:17:58 +0000

    Upgrading Circe, Scalacheck, and scalajs-dom

--- indigo/project/Dependencies.scala
+++ indigo/project/Dependencies.scala
@@ -5,9 +5,9 @@ import sbt.{Def, _}
 object Dependencies {
 
   object Versions {
-    val circe            = "0.14.0-M7"
-    val scalaCheck       = "1.15.3"
-    val scalajsDom       = "2.0.0"
+    val circe            = "0.14.1"
+    val scalaCheck       = "1.15.4"
+    val scalajsDom       = "2.1.0"
     val scalajsMacroTask = "1.0.0"
     val scalajsBenchmark = "0.10.0"
     val chartjs          = "1.0.2"
