935a69866007dd9738f6dfe923165035cec7d41d
Dave Smith <davesmith.00000@gmail.com>
2022-02-11 08:51:41 +0000

    SpriteSheetFrame clean up

--- indigo/indigo/src/main/scala/indigo/shared/display/SpriteSheetFrame.scala
+++ indigo/indigo/src/main/scala/indigo/shared/display/SpriteSheetFrame.scala
@@ -3,24 +3,28 @@ package indigo.shared.display
 import indigo.shared.datatypes.Rectangle
 import indigo.shared.datatypes.Vector2
 
-object SpriteSheetFrame {
+object SpriteSheetFrame:
 
-  def calculateFrameOffset(atlasSize: Vector2, frameCrop: Rectangle, textureOffset: Vector2): SpriteSheetFrameCoordinateOffsets = {
+  def calculateFrameOffset(
+      atlasSize: Vector2,
+      frameCrop: Rectangle,
+      textureOffset: Vector2
+  ): SpriteSheetFrameCoordinateOffsets =
     val frameSize   = frameCrop.size.toVector
     val scaleFactor = frameSize / atlasSize
 
     val translator: Vector2 => Vector2 =
       texturePosition => scaleFactor * ((frameCrop.position.toVector + texturePosition) / frameSize)
 
-    new SpriteSheetFrameCoordinateOffsets(scaleFactor, translator(textureOffset), translator)
-  }
+    SpriteSheetFrameCoordinateOffsets(scaleFactor, translator(textureOffset), translator)
 
   val defaultOffset: SpriteSheetFrameCoordinateOffsets =
     calculateFrameOffset(Vector2(1.0, 1.0), Rectangle(0, 0, 1, 1), Vector2.zero)
 
-  final case class SpriteSheetFrameCoordinateOffsets(scale: Vector2, translate: Vector2, translateCoords: Vector2 => Vector2) derives CanEqual {
-    def offsetToCoords(textureOffset: Vector2): Vector2 =
+  final case class SpriteSheetFrameCoordinateOffsets(
+      scale: Vector2,
+      translate: Vector2,
+      translateCoords: Vector2 => Vector2
+  ) derives CanEqual:
+    inline def offsetToCoords(textureOffset: Vector2): Vector2 =
       translateCoords(textureOffset)
-  }
-
-}
