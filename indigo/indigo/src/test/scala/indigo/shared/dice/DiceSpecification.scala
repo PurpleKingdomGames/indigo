e389cbbefd25fb829768f075c62bd80cda9bf307
Dave Smith <davesmith.00000@gmail.com>
2022-02-12 23:23:49 +0000

    Fixed #327: Dice roll from zero bug

--- indigo/indigo/src/test/scala/indigo/shared/dice/DiceSpecification.scala
+++ indigo/indigo/src/test/scala/indigo/shared/dice/DiceSpecification.scala
@@ -4,16 +4,28 @@ import org.scalacheck._
 
 class DiceSpecification extends Properties("Dice") {
 
-  property("all dice rolls are in range") = Prop.forAll(Gen.choose(1, Int.MaxValue)) { noOfSides =>
+  property("all dice rolls are in range (fixed sides)") = Prop.forAll(Gen.choose(1, 500)) { noOfSides =>
     val value = Dice.diceSidesN(noOfSides, 0).roll
 
     value >= 1 && value <= noOfSides
   }
 
-  property("all arbitrary dice rolls are in range") = Prop.forAll(Gen.choose(10, Int.MaxValue)) { noOfSides =>
-    val value = Dice.arbitrary(10, noOfSides, 0).roll
+  property("all dice rolls are in range (given sides)") = Prop.forAll(Gen.choose(1, 500)) { noOfSides =>
+    val value = Dice.diceSidesN(noOfSides, 0).roll(noOfSides)
 
-    value >= 10 && value <= noOfSides
+    value >= 1 && value <= noOfSides
+  }
+
+  property("all dice rolls from zero are in range (fixed sides)") = Prop.forAll(Gen.choose(1, 20)) { noOfSides =>
+    val value = Dice.diceSidesN(noOfSides, 0).rollFromZero
+
+    value >= 0 && value <= noOfSides - 1
+  }
+
+  property("all dice rolls from zero are in range (given sides)") = Prop.forAll(Gen.choose(1, 20)) { noOfSides =>
+    val value = Dice.diceSidesN(noOfSides, 0).rollFromZero(noOfSides)
+
+    value >= 0 && value <= noOfSides - 1
   }
 
 }
