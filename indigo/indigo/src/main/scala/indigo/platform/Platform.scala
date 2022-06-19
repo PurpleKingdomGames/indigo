233a8d7baca511b7daaac2cefec96eb3a7ead7cf
Nikolay Kushin <zikolach@gmail.com>
2022-06-19 19:32:08 +0200

    Remove unused warning suppression

--- indigo/indigo/src/main/scala/indigo/platform/Platform.scala
+++ indigo/indigo/src/main/scala/indigo/platform/Platform.scala
@@ -44,7 +44,7 @@ class Platform(
   val rendererInit: RendererInitialiser =
     new RendererInitialiser(gameConfig.advanced.renderingTechnology, globalEventStream, dynamicText)
 
-  @SuppressWarnings(Array("scalafix:DisableSyntax.null", "scalafix:DisableSyntax.var"))
+  @SuppressWarnings(Array("scalafix:DisableSyntax.var"))
   private var _canvas: Canvas = _
   @SuppressWarnings(Array("scalafix:DisableSyntax.var"))
   private var _running: Boolean         = true
