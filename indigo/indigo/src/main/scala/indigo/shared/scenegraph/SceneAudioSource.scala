0ed2aa8af205f2623a8d2052771882a0e9dfc5ed
☀️ <sol@solfox.online>
2022-08-28 12:17:37 -0400

    remove SceneAudio.None

--- indigo/indigo/src/main/scala/indigo/shared/scenegraph/SceneAudioSource.scala
+++ indigo/indigo/src/main/scala/indigo/shared/scenegraph/SceneAudioSource.scala
@@ -6,28 +6,11 @@ import indigo.shared.datatypes.BindingKey
 /** Represents a single audio source, how it is being played, and at what volume. You could implement a cross fade
   * between two audio sources.
   */
-final case class SceneAudioSource(bindingKey: BindingKey, playbackPattern: PlaybackPattern, masterVolume: Volume)
-    derives CanEqual {
-  def |+|(other: SceneAudioSource): SceneAudioSource =
-    SceneAudioSource.combine(this, other)
-}
+final case class SceneAudioSource(bindingKey: BindingKey, playbackPattern: PlaybackPattern, masterVolume: Volume) derives CanEqual
+
 object SceneAudioSource {
 
   def apply(bindingKey: BindingKey, playbackPattern: PlaybackPattern): SceneAudioSource =
     SceneAudioSource(bindingKey, playbackPattern, Volume.Max)
 
-  val None: SceneAudioSource =
-    SceneAudioSource(BindingKey("none"), PlaybackPattern.Silent, Volume.Min)
-
-  def combine(a: SceneAudioSource, b: SceneAudioSource): SceneAudioSource =
-    (a, b) match {
-      case (None, y) =>
-        y
-
-      case (x, None) =>
-        x
-
-      case (_, y) =>
-        y
-    }
 }
