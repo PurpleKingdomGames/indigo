3c61eb29a2be79535b1d4a99a933a728fb5d46a9
Dave Smith <davesmith.00000@gmail.com>
2022-09-18 21:44:55 +0100

    Added a TimelineScene to Sandbox

---
+++ indigo/sandbox/src/main/scala/com/example/sandbox/scenes/TimelineScene.scala
@@ -0,0 +1,58 @@
+package com.example.sandbox.scenes
+
+import com.example.sandbox.SandboxAssets
+import com.example.sandbox.SandboxGameModel
+import com.example.sandbox.SandboxStartupData
+import com.example.sandbox.SandboxViewModel
+import indigo.*
+import indigo.scenes.*
+
+object TimelineScene extends Scene[SandboxStartupData, SandboxGameModel, SandboxViewModel]:
+
+  type SceneModel     = Unit
+  type SceneViewModel = Unit
+
+  def eventFilters: EventFilters =
+    EventFilters.Permissive
+
+  def modelLens: Lens[SandboxGameModel, Unit] =
+    Lens.unit
+
+  def viewModelLens: Lens[SandboxViewModel, Unit] =
+    Lens.unit
+
+  def name: SceneName =
+    SceneName("timeline")
+
+  def subSystems: Set[SubSystem] =
+    Set()
+
+  def updateModel(
+      context: FrameContext[SandboxStartupData],
+      model: Unit
+  ): GlobalEvent => Outcome[Unit] =
+    _ => Outcome(model)
+
+  def updateViewModel(
+      context: FrameContext[SandboxStartupData],
+      model: Unit,
+      viewModel: Unit
+  ): GlobalEvent => Outcome[Unit] =
+    _ => Outcome(viewModel)
+
+  val crate = Graphic(64, 64, SandboxAssets.cratesMaterial)
+    .modifyMaterial(_.withLighting(LightingModel.Unlit))
+    .withCrop(0, 0, 32, 32)
+
+  def present(
+      context: FrameContext[SandboxStartupData],
+      model: Unit,
+      viewModel: Unit
+  ): Outcome[SceneUpdateFragment] =
+    Outcome(
+      SceneUpdateFragment(
+        Layer(
+          crate
+        )
+      )
+    )
