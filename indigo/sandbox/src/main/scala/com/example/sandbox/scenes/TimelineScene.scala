0b852c34ecbad9ff38c4fa263f6dc7ec2cd19ff5
Dave Smith <davesmith.00000@gmail.com>
2022-09-22 21:39:16 +0100

    Timeline: Animate alpha

--- indigo/sandbox/src/main/scala/com/example/sandbox/scenes/TimelineScene.scala
+++ indigo/sandbox/src/main/scala/com/example/sandbox/scenes/TimelineScene.scala
@@ -43,26 +43,31 @@ object TimelineScene extends Scene[SandboxStartupData, SandboxGameModel, Sandbox
   ): GlobalEvent => Outcome[Unit] =
     _ => Outcome(viewModel)
 
-  val crate = Graphic(64, 64, SandboxAssets.cratesMaterial)
-    .modifyMaterial(_.withLighting(LightingModel.Unlit))
-    .withCrop(0, 0, 32, 32)
+  val crate: Graphic[Material.ImageEffects] = 
+    Graphic(64, 64, SandboxAssets.cratesMaterial)
+      .modifyMaterial(_.toImageEffects.withLighting(LightingModel.Unlit))
+      .withCrop(0, 0, 32, 32)
 
   // No frills timeline animation
   import indigo.shared.temporal.SignalFunction as SF
 
-  def move(g: Graphic[Material.Bitmap]): SignalFunction[Point, Graphic[Material.Bitmap]] =
+  def move(g: Graphic[Material.ImageEffects]): SignalFunction[Point, Graphic[Material.ImageEffects]] =
     SignalFunction(pt => g.moveTo(pt))
 
-  val modifier1: Graphic[Material.Bitmap] => SignalFunction[Seconds, Graphic[Material.Bitmap]] =
+  val modifier1: Graphic[Material.ImageEffects] => SignalFunction[Seconds, Graphic[Material.ImageEffects]] =
     SF.easeInOut(5.seconds) >>> SF.lerp(Point(0), Point(100)) >>> move(_)
 
-  val modifier2: Graphic[Material.Bitmap] => SignalFunction[Seconds, Graphic[Material.Bitmap]] =
+  val modifier2: Graphic[Material.ImageEffects] => SignalFunction[Seconds, Graphic[Material.ImageEffects]] =
     SF.easeInOut(3.seconds) >>> SF.lerp(Point(100), Point(100, 0)) >>> move(_)
+
+  val modifier3: Graphic[Material.ImageEffects] => SignalFunction[Seconds, Graphic[Material.ImageEffects]] =
+    g => SF.sin >>> SignalFunction(d => (d + 1) / 2) >>> SignalFunction(d => g.modifyMaterial(_.withAlpha(d)))
 
   val timeline =
     Timeline(
       TimeSlot(2.seconds, 7.seconds, modifier1),
-      TimeSlot(7.seconds, 10.seconds, modifier2)
+      TimeSlot(7.seconds, 10.seconds, modifier2),
+      TimeSlot(2.seconds, 10.seconds, modifier3)
     )
 
   def present(
