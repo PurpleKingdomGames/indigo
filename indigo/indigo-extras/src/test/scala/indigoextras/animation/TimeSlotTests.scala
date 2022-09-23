4818ca79c7bf649087d9af83ade950846f95aebc
Dave Smith <davesmith.00000@gmail.com>
2022-09-23 08:48:55 +0100

    TimeSlot tests

--- indigo/indigo-extras/src/test/scala/indigoextras/animation/TimeSlotTests.scala
+++ indigo/indigo-extras/src/test/scala/indigoextras/animation/TimeSlotTests.scala
@@ -8,8 +8,25 @@ import indigo.syntax.*
 
 class TimeSlotTests extends munit.FunSuite {
 
-  test("test") {
-    assert(1 == 1)
+  import TimeSlot.*
+
+  val f = (i: Int) => SignalFunction((_: Seconds) => i)
+
+  test("toWindows") {
+    val slot: TimeSlot[Int] = pause(2.seconds) andThen
+      animate(5.seconds)(f) andThen
+      animate(3.seconds, f)
+
+    val actual =
+      slot.toWindows
+
+    val expected =
+      Batch(
+        TimeWindow(Seconds(2), Seconds(7), f),
+        TimeWindow(Seconds(7), Seconds(10), f)
+      )
+
+    assert(actual == expected)
   }
 
 }
