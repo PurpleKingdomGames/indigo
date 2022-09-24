64370e134ef646fdc766e7d1b847123689068ba7
Dave Smith <davesmith.00000@gmail.com>
2022-09-24 21:51:35 +0100

    Scene/Frame Contexts export instead of delegate

--- indigo/indigo/src/main/scala/indigo/scenes/SceneContext.scala
+++ indigo/indigo/src/main/scala/indigo/scenes/SceneContext.scala
@@ -27,20 +27,15 @@ final class SceneContext[StartUpData](
     val sceneTime: Seconds,
     val frameContext: FrameContext[StartUpData]
 ):
-
-  lazy val gameTime: GameTime               = frameContext.gameTime
-  lazy val dice: Dice                       = frameContext.dice
-  lazy val inputState: InputState           = frameContext.inputState
-  lazy val boundaryLocator: BoundaryLocator = frameContext.boundaryLocator
-  lazy val startUpData: StartUpData         = frameContext.startUpData
-  lazy val running: Seconds                 = frameContext.gameTime.running
-  lazy val delta: Seconds                   = frameContext.gameTime.delta
-  lazy val mouse: Mouse                     = frameContext.inputState.mouse
-  lazy val keyboard: Keyboard               = frameContext.inputState.keyboard
-  lazy val gamepad: Gamepad                 = frameContext.inputState.gamepad
-
-  def findBounds(sceneNode: SceneNode): Option[Rectangle] =
-    frameContext.findBounds(sceneNode)
-
-  def bounds(sceneGraphNode: SceneNode): Rectangle =
-    frameContext.bounds(sceneGraphNode)
+  export frameContext.gameTime
+  export frameContext.dice
+  export frameContext.inputState
+  export frameContext.boundaryLocator
+  export frameContext.startUpData
+  export frameContext.gameTime.running
+  export frameContext.gameTime.delta
+  export frameContext.inputState.mouse
+  export frameContext.inputState.keyboard
+  export frameContext.inputState.gamepad
+  export frameContext.findBounds
+  export frameContext.bounds
