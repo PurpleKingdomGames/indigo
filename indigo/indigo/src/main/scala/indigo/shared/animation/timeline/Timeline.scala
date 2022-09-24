f9919cd3d860fa18fdb8956e29d5fe0141228ee4
Dave Smith <davesmith.00000@gmail.com>
2022-09-24 21:20:42 +0100

    Timeline is an opaque type

--- indigo/indigo/src/main/scala/indigo/shared/animation/timeline/Timeline.scala
+++ indigo/indigo/src/main/scala/indigo/shared/animation/timeline/Timeline.scala
@@ -8,44 +8,48 @@ import indigo.shared.time.Seconds
 import scala.annotation.tailrec
 import scala.annotation.targetName
 
-final case class Timeline[A](windows: Batch[TimeWindow[A]]):
-
-  def add(animation: TimeSlot[A]): Timeline[A] =
-    addWindows(animation.toWindows)
-
-  def addWindow(nextWindow: TimeWindow[A]): Timeline[A] =
-    this.copy(windows = windows :+ nextWindow)
-
-  def addWindows(nextWindows: Batch[TimeWindow[A]]): Timeline[A] =
-    this.copy(windows = windows ++ nextWindows)
-
-  def at(time: Seconds): A => Option[A] = subject =>
-    @tailrec
-    def rec(remaining: Batch[TimeWindow[A]], acc: Option[A]): Option[A] =
-      if remaining.isEmpty then acc
-      else
-        val x  = remaining.head
-        val xs = remaining.tail
-
-        if x.within(time) then
-          val value = acc.getOrElse(subject)
-          val next  = (Signal.Time |> x.modifier(value)).at(time - x.start)
-          rec(xs, Some(next))
-        else rec(xs, acc)
-
-    rec(windows, None)
+opaque type Timeline[A] = Batch[TimeWindow[A]]
 
 object Timeline:
-  def apply[A](timeWindows: TimeWindow[A]*): Timeline[A] =
+  inline def apply[A](timeWindows: Batch[TimeWindow[A]]): Timeline[A] =
+    timeWindows
+
+  inline def apply[A](timeWindows: TimeWindow[A]*): Timeline[A] =
     Timeline(Batch.fromSeq(timeWindows))
 
   @targetName("timeline_apply_slots")
-  def apply[A](timeSlots: TimeSlot[A]*): Timeline[A] =
+  inline def apply[A](timeSlots: TimeSlot[A]*): Timeline[A] =
     Timeline(Batch.fromSeq(timeSlots).flatMap(_.toWindows))
 
   @targetName("timeline_apply_animations")
-  def apply[A](animations: TimelineAnimation[A]*): Timeline[A] =
+  inline def apply[A](animations: TimelineAnimation[A]*): Timeline[A] =
     Timeline(Batch.fromSeq(animations).flatMap(_.compile.toWindows))
 
   def empty[A]: Timeline[A] =
     Timeline(Batch.empty[TimeWindow[A]])
+
+  extension [A](tl: Timeline[A])
+    def add(animation: TimeSlot[A]): Timeline[A] =
+      addWindows(animation.toWindows)
+
+    def addWindow(nextWindow: TimeWindow[A]): Timeline[A] =
+      tl :+ nextWindow
+
+    def addWindows(nextWindows: Batch[TimeWindow[A]]): Timeline[A] =
+      tl ++ nextWindows
+
+    def at(time: Seconds): A => Option[A] = subject =>
+      @tailrec
+      def rec(remaining: Batch[TimeWindow[A]], acc: Option[A]): Option[A] =
+        if remaining.isEmpty then acc
+        else
+          val x  = remaining.head
+          val xs = remaining.tail
+
+          if x.within(time) then
+            val value = acc.getOrElse(subject)
+            val next  = (Signal.Time |> x.modifier(value)).at(time - x.start)
+            rec(xs, Some(next))
+          else rec(xs, acc)
+
+      rec(tl, None)
