874131d67e9d7b7278a81ac26c7db219e2584e76
Dave Smith <davesmith.00000@gmail.com>
2022-09-24 21:14:38 +0100

    TimelineAnimation is an opaque type

--- indigo/indigo/src/main/scala/indigo/shared/animation/timeline/TimelineAnimation.scala
+++ indigo/indigo/src/main/scala/indigo/shared/animation/timeline/TimelineAnimation.scala
@@ -2,11 +2,17 @@ package indigo.shared.animation.timeline
 
 import indigo.shared.collections.Batch
 
-final case class TimelineAnimation[A](slots: Batch[TimeSlot[A]]):
-  def compile: TimeSlot[A] =
-    if slots.isEmpty then TimeSlot.start
-    else slots.tail.foldLeft(slots.head) { case (acc, next) => acc andThen next }
+opaque type TimelineAnimation[A] = Batch[TimeSlot[A]]
 
 object TimelineAnimation:
-  def apply[A](slots: TimeSlot[A]*): TimelineAnimation[A] =
-    TimelineAnimation(Batch.fromSeq(slots))
+
+  inline def apply[A](slots: TimeSlot[A]*): TimelineAnimation[A] =
+    Batch.fromSeq(slots)
+
+  inline def apply[A](slots: Batch[TimeSlot[A]]): TimelineAnimation[A] =
+    slots
+
+  extension [A](tl: TimelineAnimation[A])
+    def compile: TimeSlot[A] =
+      if tl.isEmpty then TimeSlot.start
+      else tl.tail.foldLeft(tl.head) { case (acc, next) => acc andThen next }
