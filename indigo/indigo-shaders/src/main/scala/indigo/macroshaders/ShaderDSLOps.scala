c705a6e742c77ff1a2958c410d34d8b465a89b61
Dave Smith <davesmith.00000@gmail.com>
2022-10-16 09:34:52 +0100

    Length uses Union type for argument

--- indigo/indigo-shaders/src/main/scala/indigo/macroshaders/ShaderDSLOps.scala
+++ indigo/indigo-shaders/src/main/scala/indigo/macroshaders/ShaderDSLOps.scala
@@ -2,21 +2,28 @@ package indigo.macroshaders
 
 trait ShaderDSLOps extends ShaderDSLTypeExtensions:
 
-  inline def length(genType: Float): Float =
-    Math.sqrt(Math.pow(genType, 2.0f)).toFloat
-  inline def length(genType: vec2): Float =
-    Math.sqrt(Math.pow(genType.x, 2.0f) + Math.pow(genType.y, 2.0f)).toFloat
-  inline def length(genType: vec3): Float =
-    Math.sqrt(Math.pow(genType.x, 2.0f) + Math.pow(genType.y, 2.0f) + Math.pow(genType.z, 2.0f)).toFloat
-  inline def length(genType: vec4): Float =
-    Math
-      .sqrt(
-        Math.pow(genType.x, 2.0f) +
-          Math.pow(genType.y, 2.0f) +
-          Math.pow(genType.z, 2.0f) +
-          Math.pow(genType.w, 2.0f)
-      )
-      .toFloat
+  def length(genType: Float | vec2 | vec3 | vec4 | rgba): Float =
+    genType match
+      case f: Float =>
+        Math.sqrt(Math.pow(f, 2.0f)).toFloat
 
-  inline def step(edge: Float, x: Float): Float =
+      case vec2(x, y) =>
+        Math.sqrt(Math.pow(x, 2.0f) + Math.pow(y, 2.0f)).toFloat
+
+      case vec3(x, y, z) =>
+        Math.sqrt(Math.pow(x, 2.0f) + Math.pow(y, 2.0f) + Math.pow(z, 2.0f)).toFloat
+
+      case vec4(x, y, z, w) =>
+        Math
+          .sqrt(
+            Math.pow(x, 2.0f) +
+              Math.pow(y, 2.0f) +
+              Math.pow(z, 2.0f) +
+              Math.pow(w, 2.0f)
+          )
+          .toFloat
+      case c: rgba =>
+        length(c.toVec4)
+
+  def step(edge: Float, x: Float): Float =
     if x < edge then 0.0f else 1.0f
