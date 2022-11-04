cf1c42c44e4f2bcfc175ef3f5d72b28d438dd961
Dave Smith <davesmith.00000@gmail.com>
2022-11-04 16:35:09 +0000

    Added a test to show scoping is respected

--- indigo/indigo-shaders/src/test/scala/indigo/macroshaders/ShaderASTTests.scala
+++ indigo/indigo-shaders/src/test/scala/indigo/macroshaders/ShaderASTTests.scala
@@ -453,4 +453,35 @@ class ShaderASTTests extends munit.FunSuite {
   //   //
   // }
 
+  test("scoping is respected") {
+    inline def fragment: Shader[FragEnv, vec3] =
+      Shader { _ =>
+        def foo: vec3 =
+          val f: Float => vec3 = r => vec3(r, 0.0f, 0.0f)
+          f(1.0)
+
+        def bar: vec3 =
+          val f: vec2 => vec3 = rg => vec3(rg, 0.0f)
+          f(vec2(0.5))
+        foo + bar
+      }
+
+    val actual =
+      fragment.toGLSL
+
+    // DebugAST.toAST(fragment)
+    // println(actual)
+
+    assertEquals(
+      actual,
+      s"""
+      |vec3 fn0(float r){return vec3(r,0.0,0.0);}
+      |vec3 foo(){return fn0(1.0);}
+      |vec3 fn1(vec2 rg){return vec3(rg,0.0);}
+      |vec3 bar(){return fn1(vec2(0.5));}
+      |(foo)+(bar);
+      |""".stripMargin.trim
+    )
+  }
+
 }
