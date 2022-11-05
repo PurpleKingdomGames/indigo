fa4b0e67c020cdae1e89aad8a44f37f620dcddee
Dave Smith <davesmith.00000@gmail.com>
2022-11-05 16:00:17 +0000

    Added a few stub GLSL ops

--- indigo/indigo-shaders/src/main/scala/indigo/macroshaders/ShaderDSLOps.scala
+++ indigo/indigo-shaders/src/main/scala/indigo/macroshaders/ShaderDSLOps.scala
@@ -1,5 +1,7 @@
 package indigo.macroshaders
 
+import scala.annotation.targetName
+
 trait ShaderDSLOps extends ShaderDSLTypeExtensions:
 
   def length(genType: Float | vec2 | vec3 | vec4): Float =
@@ -25,3 +27,10 @@ trait ShaderDSLOps extends ShaderDSLTypeExtensions:
 
   def step(edge: Float, x: Float): Float =
     if x < edge then 0.0f else 1.0f
+
+  @targetName("fract_float")
+  def fract(genType: Float): Float = genType
+  @targetName("fract_vec2")
+  def fract(genType: vec2): vec2   = genType
+  def floor(genType: vec2): vec2   = genType
+  def dot(a: vec2, b: vec2): Float = 0.0f
