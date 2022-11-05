70c47cb41cda4703f099ca7667e28daf069e4c81
Dave Smith <davesmith.00000@gmail.com>
2022-11-05 15:59:59 +0000

    Basic operators extensions for Float

--- indigo/indigo-shaders/src/main/scala/indigo/macroshaders/ShaderDSL.scala
+++ indigo/indigo-shaders/src/main/scala/indigo/macroshaders/ShaderDSL.scala
@@ -2,6 +2,20 @@ package indigo.macroshaders
 
 object ShaderDSL extends ShaderDSLOps:
 
-  // 
+  extension (f: Float)
+    def +(v: vec2): vec2 = vec2(f + v.x, f + v.y)
+    def -(v: vec2): vec2 = vec2(f - v.x, f - v.y)
+    def *(v: vec2): vec2 = vec2(f * v.x, f * v.y)
+    def /(v: vec2): vec2 = vec2(f / v.x, f / v.y)
+
+    def +(v: vec3): vec3 = vec3(f + v.x, f + v.y, f + v.z)
+    def -(v: vec3): vec3 = vec3(f - v.x, f - v.y, f - v.z)
+    def *(v: vec3): vec3 = vec3(f * v.x, f * v.y, f * v.z)
+    def /(v: vec3): vec3 = vec3(f / v.x, f / v.y, f / v.z)
+
+    def +(v: vec4): vec4 = vec4(f + v.x, f + v.y, f + v.z, f + v.w)
+    def -(v: vec4): vec4 = vec4(f - v.x, f - v.y, f - v.z, f - v.w)
+    def *(v: vec4): vec4 = vec4(f * v.x, f * v.y, f * v.z, f * v.w)
+    def /(v: vec4): vec4 = vec4(f / v.x, f / v.y, f / v.z, f / v.w)
 
 end ShaderDSL
