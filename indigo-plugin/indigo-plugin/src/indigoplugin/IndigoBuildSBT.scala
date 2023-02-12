49ad55603654efe1e0732ef5bb2c93fce305bc50
Dave Smith <davesmith.00000@gmail.com>
2023-02-12 14:22:17 +0000

    Scalafmt Indigo-Plugin project

--- indigo-plugin/indigo-plugin/src/indigoplugin/IndigoBuildSBT.scala
+++ indigo-plugin/indigo-plugin/src/indigoplugin/IndigoBuildSBT.scala
@@ -7,7 +7,7 @@ object IndigoBuildSBT {
   def build(baseDir: String, templateOptions: TemplateOptions, outputFolderName: String, scriptName: String): Unit = {
 
     println(AsciiLogo.logo)
-    
+
     IndigoBuild.build(
       templateOptions,
       IndigoBuild.createDirectoryStructure(Path(baseDir) / "target" / outputFolderName),
