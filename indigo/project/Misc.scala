ee58300af07109b1cd4fbb72695720b0650885a0
Dave Smith <davesmith.00000@gmail.com>
2023-02-18 08:46:04 +0000

    Add a reminder of how to check the formatting to welcome message

--- indigo/project/Misc.scala
+++ indigo/project/Misc.scala
@@ -27,6 +27,7 @@ object Misc {
     UsefulTask("", "sandboxRun", "Run the sandbox game (fastOptJS + Electron)"),
     UsefulTask("", "perfRun", "Run the perf game (fastOptJS + Electron)"),
     UsefulTask("", "shaderRun", "Run the shader game (fastOptJS + Electron)"),
+    UsefulTask("", "scalafmtCheckAll", "Launch VSCode"),
     UsefulTask("", "code", "Launch VSCode")
   )
 
