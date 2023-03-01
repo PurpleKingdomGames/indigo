e75843dcd16001f6557259b5f7cdc265a853dd8d
Dave Smith <davesmith.00000@gmail.com>
2023-03-01 21:35:45 +0000

    Fixed #481: Added Scaladocs to Dice

--- indigo/indigo/src/main/scala/indigo/shared/dice/Dice.scala
+++ indigo/indigo/src/main/scala/indigo/shared/dice/Dice.scala
@@ -7,19 +7,59 @@ import indigo.shared.time.Seconds
 import scala.annotation.tailrec
 import scala.util.Random
 
-/** All dice rolls are from value 1 to N inclusive. Like a dice.
+/** The Dice primitive supplies a consistent way to get psuedo-random values into your game.
+  *
+  * A dice instance can be found in the FrameContext object with 'max int' sides, and every frame the dice's seed value
+  * is set to the current running time of the game in milliseconds.
+  *
+  * Dice also serve as a handy proxy to a number of functions found on a normal `Random` instance, like alphanumeric,
+  * but with a predicatable seed.
   */
 trait Dice:
-  val seed: Long
+
+  /** The seed value of the dice. The dice supplied in the `FrameContext` has the seed set to the current running time
+    * of the game in milliseconds.
+    */
+  def seed: Long
+
+  /** Roll an Int from 1 to the number of sides on the dice (inclusive)
+    */
   def roll: Int
+
+  /** Roll an Int from 1 to the specified number of sides (inclusive), using this dice instance as the seed.
+    */
   def roll(sides: Int): Int
+
+  /** Roll an Int from 0 to the number of sides on the dice (inclusive)
+    */
   def rollFromZero: Int
+
+  /** Roll an Int from 0 to the specified number of sides (inclusive), using this dice instance as the seed.
+    */
   def rollFromZero(sides: Int): Int
+
+  /** Produces a random Float from 0.0f to 1.0f
+    */
   def rollFloat: Float
+
+  /** Produces a random Double from 0.0 to 1.0
+    */
   def rollDouble: Double
+
+  /** Produces an alphanumeric string of the specified length
+    */
   def rollAlphaNumeric(length: Int): String
+
+  /** Produces an alphanumeric string 16 characters long
+    */
   def rollAlphaNumeric: String
-  def rollBoolean: Boolean = if roll(2) == 1 then true else false
+
+  /** Produces a random Boolean
+    */
+  def rollBoolean: Boolean = roll(2) == 1
+
+  /** Shuffles a list of values into a random order
+    */
   def shuffle[A](items: List[A]): List[A]
 
   override def toString: String =
@@ -27,12 +67,19 @@ trait Dice:
 
 object Dice:
 
+  /** Construct a 'max int' sided dice using a time in seconds (converted to millis) as the seed.
+    */
   def fromSeconds(time: Seconds): Dice =
     Sides.MaxInt(time.toMillis.toLong)
 
+  /** Construct a 'max int' sided dice using a time in milliseconds as the seed.
+    */
   def fromMillis(time: Millis): Dice =
     Sides.MaxInt(time.toLong)
 
+  /** Construct a 'max int' sided dice from a given seed value. This is the default dice presented by the
+    * `FrameContext`.
+    */
   def fromSeed(seed: Long): Dice =
     Sides.MaxInt(seed)
 
@@ -42,6 +89,17 @@ object Dice:
   private val sanitise: Int => Int =
     i => Math.max(1, Math.abs(i))
 
+  /** Rolls a number of dice.
+    *
+    * @param numberOfDice
+    *   How many dice to roll
+    * @param sides
+    *   How many sides the dice all have
+    * @param seed
+    *   The seed value to based the dice on
+    * @return
+    *   Returns and Optional NonEmtpyList of Int's, where None is produced when the input values are invalid.
+    */
   def rollMany(numberOfDice: Int, sides: Int, seed: Long): Option[NonEmptyList[Int]] =
     @tailrec
     def rec(remaining: Int, acc: NonEmptyList[Int]): Option[NonEmptyList[Int]] =
@@ -57,6 +115,8 @@ object Dice:
       rec(numberOfDice - 1, NonEmptyList(diceSidesN(sides, seed).roll))
     else None
 
+  /** Constructs a 'loaded' dice where everything returns a fixed value. Intended for use during testing.
+    */
   def loaded(fixedTo: Int): Dice =
     new Dice {
       val seed: Long = 0
@@ -89,6 +149,8 @@ object Dice:
         items
     }
 
+  /** Constructs a dice with a given number of sides and a seed value.
+    */
   def diceSidesN(sides: Int, seedValue: Long): Dice =
     new Dice {
       val seed: Long = seedValue
@@ -123,21 +185,75 @@ object Dice:
         r.shuffle(items)
     }
 
+  /** Pre-constructed dice with a fixed number of sides, rolls are includive and start at 1, not 0. You need to provide
+    * a seed value.
+    */
   object Sides:
-    val MaxInt: Long => Dice   = (seed: Long) => diceSidesN(Int.MaxValue, seed)
-    val One: Long => Dice      = (seed: Long) => diceSidesN(1, seed)
-    val Two: Long => Dice      = (seed: Long) => diceSidesN(2, seed)
-    val Three: Long => Dice    = (seed: Long) => diceSidesN(3, seed)
-    val Four: Long => Dice     = (seed: Long) => diceSidesN(4, seed)
-    val Five: Long => Dice     = (seed: Long) => diceSidesN(5, seed)
-    val Six: Long => Dice      = (seed: Long) => diceSidesN(6, seed)
-    val Seven: Long => Dice    = (seed: Long) => diceSidesN(7, seed)
-    val Eight: Long => Dice    = (seed: Long) => diceSidesN(8, seed)
-    val Nine: Long => Dice     = (seed: Long) => diceSidesN(9, seed)
-    val Ten: Long => Dice      = (seed: Long) => diceSidesN(10, seed)
-    val Eleven: Long => Dice   = (seed: Long) => diceSidesN(11, seed)
-    val Twelve: Long => Dice   = (seed: Long) => diceSidesN(12, seed)
-    val Thirteen: Long => Dice = (seed: Long) => diceSidesN(13, seed)
-    val Fourteen: Long => Dice = (seed: Long) => diceSidesN(14, seed)
-    val Fifteen: Long => Dice  = (seed: Long) => diceSidesN(15, seed)
-    val Sixteen: Long => Dice  = (seed: Long) => diceSidesN(16, seed)
+
+    /** A dice with 2147483647 - 1 (no zero) sides
+      */
+    def MaxInt(seed: Long): Dice = diceSidesN(Int.MaxValue, seed)
+
+    /** A one-sided dice.
+      */
+    def One(seed: Long): Dice = diceSidesN(1, seed)
+
+    /** A two-sided dice.
+      */
+    def Two(seed: Long): Dice = diceSidesN(2, seed)
+
+    /** A three-sided dice.
+      */
+    def Three(seed: Long): Dice = diceSidesN(3, seed)
+
+    /** A four-sided dice.
+      */
+    def Four(seed: Long): Dice = diceSidesN(4, seed)
+
+    /** A five-sided dice.
+      */
+    def Five(seed: Long): Dice = diceSidesN(5, seed)
+
+    /** A six-sided dice.
+      */
+    def Six(seed: Long): Dice = diceSidesN(6, seed)
+
+    /** A seven-sided dice.
+      */
+    def Seven(seed: Long): Dice = diceSidesN(7, seed)
+
+    /** An eight-sided dice.
+      */
+    def Eight(seed: Long): Dice = diceSidesN(8, seed)
+
+    /** A nine-sided dice.
+      */
+    def Nine(seed: Long): Dice = diceSidesN(9, seed)
+
+    /** A ten-sided dice.
+      */
+    def Ten(seed: Long): Dice = diceSidesN(10, seed)
+
+    /** An eleven-sided dice.
+      */
+    def Eleven(seed: Long): Dice = diceSidesN(11, seed)
+
+    /** A twelve-sided dice.
+      */
+    def Twelve(seed: Long): Dice = diceSidesN(12, seed)
+
+    /** A thirteen-sided dice.
+      */
+    def Thirteen(seed: Long): Dice = diceSidesN(13, seed)
+
+    /** A forteen-sided dice.
+      */
+    def Fourteen(seed: Long): Dice = diceSidesN(14, seed)
+
+    /** A fifteen-sided dice.
+      */
+    def Fifteen(seed: Long): Dice = diceSidesN(15, seed)
+
+    /** A sixteen-sided dice.
+      */
+    def Sixteen(seed: Long): Dice = diceSidesN(16, seed)
