6c7710bbc817baf0c3dc6ec23df5440dd20e1af6
Dave Smith <davesmith.00000@gmail.com>
2023-03-16 08:22:11 +0000

    Added some scaladocs

--- indigo/indigo/src/main/scala/indigo/shared/animation/timeline/Timeline.scala
+++ indigo/indigo/src/main/scala/indigo/shared/animation/timeline/Timeline.scala
@@ -29,15 +29,46 @@ object Timeline:
     Timeline(Batch.empty[TimeWindow[A]])
 
   extension [A](tl: Timeline[A])
+    /** Add a `TimeSlot[A]` to the animation as a `TimeWindow`. Time slots are the building block of animation and can
+      * be combined together to build up an animation, for example: `startAfter(2.seconds) andThen
+      * show(5.seconds)(_.withAlpha(0.5))`
+      *
+      * @param animation
+      *   the `TimeSlot[A]` to add
+      * @return
+      *   `Timeline[A]`
+      */
     def add(animation: TimeSlot[A]): Timeline[A] =
       addWindows(animation.toWindows)
 
+    /** Add a time window to the animation.
+      *
+      * @param nextWindow
+      *   `TimeWindow[A]`
+      * @return
+      *   `Timeline[A]`
+      */
     def addWindow(nextWindow: TimeWindow[A]): Timeline[A] =
       tl :+ nextWindow
 
+    /** Add a batch of time windows to the animation.
+      *
+      * @param nextWindows
+      *   `Batch[TimeWindow[A]]`
+      * @return
+      *   `Timeline[A]`
+      */
     def addWindows(nextWindows: Batch[TimeWindow[A]]): Timeline[A] =
       tl ++ nextWindows
 
+    /** Samples the animation at the given time to produce a value. The return type is ultimately optional because you
+      * could sample the animation outside of the animations time range.
+      *
+      * @param time
+      *   the current running time in `Second`s to sample the animation at.
+      * @return
+      *   `A => Option[A]` where `A` is the 'subject', the thing to be animated.
+      */
     def at(time: Seconds): A => Option[A] = subject =>
       @tailrec
       def rec(remaining: Batch[TimeWindow[A]], acc: Option[A]): Option[A] =
@@ -54,17 +85,50 @@ object Timeline:
 
       rec(tl, None)
 
+    /** Calls `at` but always returns a value, using the original subject `A` if no other value would be produced by the
+      * animation.
+      *
+      * @param time
+      *   the current running time in `Second`s to sample the animation at.
+      * @return
+      *   `A`
+      */
     def atOrElse(time: Seconds): A => A =
       subject => at(time)(subject).getOrElse(subject)
 
+    /** Calls `at` but always returns a value, using the given default subject `A` if no other value would be produced
+      * by the animation.
+      *
+      * @param time
+      *   the current running time in `Second`s to sample the animation at.
+      * @param default
+      *   the subject to use if no other value is produced
+      * @return
+      *   `A`
+      */
     def atOrElse(time: Seconds, default: A): A => A =
       subject => at(time)(subject).getOrElse(default)
 
+    /** Give the time windows as a Batch.
+      *
+      * @return
+      *   `Batch[TimeWindow[A]]`
+      */
     def toWindows: Batch[TimeWindow[A]] =
       tl
 
+    /** Return the total time duration of the animation in seconds, including any initial delays.
+      *
+      * @return
+      *   `Seconds`
+      */
     def duration: Seconds =
       tl.map(_.end).sortWith(_ > _).headOption.getOrElse(Seconds.zero)
 
+    /** Alias for duration, returns the total time duration of the animation in seconds, including any initial delays.
+      *
+      * @return
+      *   `Seconds`
+      */
     def length: Seconds =
       duration
