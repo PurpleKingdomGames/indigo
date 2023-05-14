db4f500d6236279a8453cbb261ff7b90e65ed0b0
Dave Smith <davesmith.00000@gmail.com>
2023-05-14 21:27:30 +0100

    Added a basic Circle datatype

---
+++ indigo/indigo/src/main/scala/indigo/shared/datatypes/Circle.scala
@@ -0,0 +1,99 @@
+package indigo.shared.datatypes
+
+import indigo.shared.collections.Batch
+import indigo.shared.datatypes.Vector2
+
+final case class Circle(position: Point, radius: Int) derives CanEqual:
+  lazy val x: Int        = position.x
+  lazy val y: Int        = position.y
+  lazy val diameter: Int = radius * 2
+
+  lazy val left: Int   = x - radius
+  lazy val right: Int  = x + radius
+  lazy val top: Int    = y - radius
+  lazy val bottom: Int = y + radius
+
+  def toRectangle: Rectangle =
+    Rectangle(Point(left, top), Size(diameter, diameter))
+
+  def contains(vertex: Point): Boolean =
+    vertex.distanceTo(position) <= radius
+  def contains(x: Int, y: Int): Boolean =
+    contains(Point(x, y))
+  def contains(vector: Vector2): Boolean =
+    contains(vector.toPoint)
+
+  def +(d: Int): Circle = resize(radius + d)
+  def -(d: Int): Circle = resize(radius - d)
+  def *(d: Int): Circle = resize(radius * d)
+  def /(d: Int): Circle = resize(radius / d)
+
+  def sdf(vertex: Point): Int =
+    Circle.signedDistanceFunction(vertex - position, radius)
+  def sdf(vector: Vector2): Int =
+    sdf(vector.toPoint)
+
+  def distanceToBoundary(vertex: Point): Int =
+    sdf(vertex)
+  def distanceToBoundary(vector: Vector2): Int =
+    sdf(vector.toPoint)
+
+  def expandToInclude(other: Circle): Circle =
+    Circle.expandToInclude(this, other)
+
+  def encompasses(other: Circle): Boolean =
+    Circle.encompassing(this, other)
+
+  def overlaps(other: Circle): Boolean =
+    Circle.overlapping(this, other)
+
+  def moveBy(amount: Point): Circle =
+    this.copy(position = position + amount)
+  def moveBy(x: Int, y: Int): Circle =
+    moveBy(Point(x, y))
+  def moveBy(amount: Vector2): Circle =
+    moveBy(amount.toPoint)
+
+  def moveTo(newPosition: Point): Circle =
+    this.copy(position = newPosition)
+  def moveTo(x: Int, y: Int): Circle =
+    moveTo(Point(x, y))
+  def moveTo(newPosition: Vector2): Circle =
+    moveTo(newPosition.toPoint)
+
+  def resize(newRadius: Int): Circle =
+    this.copy(radius = newRadius)
+
+object Circle:
+
+  val zero: Circle =
+    Circle(Point.zero, 0)
+
+  def apply(x: Int, y: Int, radius: Int): Circle =
+    Circle(Point(x, y), radius)
+
+  def fromTwoPoints(center: Point, boundary: Point): Circle =
+    Circle(center, center.distanceTo(boundary).toInt)
+
+  def fromPoint(points: Batch[Point]): Circle =
+    val bb = Rectangle.fromPointCloud(points)
+    Circle(bb.center, bb.center.distanceTo(bb.topLeft).toInt)
+
+  def fromPointCloud(points: Batch[Point]): Circle =
+    fromPoint(points)
+
+  def fromRectangle(rectangle: Rectangle): Circle =
+    Circle(rectangle.center, Math.max(rectangle.halfSize.width, rectangle.halfSize.height))
+
+  def expandToInclude(a: Circle, b: Circle): Circle =
+    a.resize((a.position.distanceTo(b.position) + Math.abs(b.radius)).toInt)
+
+  def encompassing(a: Circle, b: Circle): Boolean =
+    a.position.distanceTo(b.position) <= Math.abs(a.radius) - Math.abs(b.radius)
+
+  def overlapping(a: Circle, b: Circle): Boolean =
+    a.position.distanceTo(b.position) < Math.abs(a.radius) + Math.abs(b.radius)
+
+  // Centered at the origin
+  def signedDistanceFunction(point: Point, radius: Int): Int =
+    (point.toVector.length - radius).toInt
---
+++ indigo/indigo/src/test/scala/indigo/shared/datatypes/CircleTests.scala
@@ -0,0 +1,108 @@
+package indigo.shared.datatypes
+
+import indigo.shared.collections.Batch
+
+class CircleTests extends munit.FunSuite {
+
+  test("contains") {
+    val c = Circle(Point(20, 20), 10)
+
+    assert(c.contains(Point(15, 15)))
+    assert(!c.contains(Point.zero))
+  }
+
+  test("expandToInclude") {
+    val c1 = Circle(Point(20, 20), 10)
+    val c2 = Circle(Point(40, 20), 5)
+    val c3 = Circle(Point(20, 20), 25)
+
+    assertEquals(c1.expandToInclude(c2), c3)
+  }
+
+  test("encompasses") {
+    val c1 = Circle(Point(20, 20), 10)
+    val c2 = Circle(Point(40, 20), 5)
+    val c3 = Circle(Point(20, 20), 25)
+
+    assert(c3.encompasses(c1))
+    assert(c3.encompasses(c2))
+    assert(!c1.encompasses(c2))
+  }
+
+  test("overlaps") {
+    val c1 = Circle(Point(20, 20), 10)
+    val c2 = Circle(Point(25, 25), 10)
+    val c3 = Circle(Point(10, 40), 10)
+
+    assert(c1 overlaps c2)
+    assert(!(c1 overlaps c3))
+  }
+
+  test("moveBy | moveTo") {
+    val c = Circle(Point(20, 20), 10)
+
+    assertEquals(c.moveBy(1, 2).position, Point(21, 22))
+    assertEquals(c.moveTo(1, 2).position, Point(1, 2))
+  }
+
+  test("resize") {
+    val c = Circle(Point(20, 20), 10)
+
+    assertEquals(c.resize(5).radius, 5)
+  }
+
+  test("Constructor - fromTwoVertices") {
+    val c = Circle.fromTwoPoints(Point(5, 10), Point(5, -10))
+
+    assertEquals(c.position, Point(5, 10))
+    assertEquals(c.radius, 20)
+  }
+
+  test("Constructor - fromVertices") {
+    val actual =
+      Circle.fromPointCloud(
+        Batch(
+          Point(10, 10), // tl
+          Point(20, 10), // tr
+          Point(20, 20), // br
+          Point(10, 20)  // bl
+        )
+      )
+
+    val expected =
+      Circle(Point(15, 15), Point(15, 15).distanceTo(Point(10, 10)).toInt)
+
+    assertEquals(actual.position, expected.position)
+    assert(doubleCloseEnough(actual.radius, expected.radius))
+  }
+
+  test("signed distance function") {
+    val c = Circle(Point(20, 20), 10)
+
+    // top
+    assertEquals(c.sdf(Point(20, 1)), 9)
+
+    // bottom
+    assertEquals(c.sdf(Point(20, 55)), 25)
+
+    // left
+    assertEquals(c.sdf(Point(-10, 20)), 20)
+
+    // right
+    assertEquals(c.sdf(Point(33, 20)), 3)
+
+    // edge
+    assertEquals(c.sdf(Point(10, 20)), 0)
+
+    // inside
+    assertEquals(c.sdf(Point(12, 20)), -2)
+
+    // Diagonal
+    assertEquals(c.sdf(Point.zero), 18)
+
+  }
+
+  def doubleCloseEnough(r1: Double, r2: Double): Boolean =
+    r1 - 0.001 < r2 && r1 + 0.001 > r2
+
+}
