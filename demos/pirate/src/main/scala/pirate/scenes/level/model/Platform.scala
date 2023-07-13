3527184e8e48c6f865a146942b7b7f934a204921
Dave Smith <davesmith.00000@gmail.com>
2023-07-13 19:21:48 +0100

    Remove platform welding

--- demos/pirate/src/main/scala/pirate/scenes/level/model/Platform.scala
+++ demos/pirate/src/main/scala/pirate/scenes/level/model/Platform.scala
@@ -4,8 +4,6 @@ import indigo.*
 import indigo.syntax.*
 import indigo.physics.*
 import pirate.core.TileType
-import scala.annotation.tailrec
-import scala.collection.immutable.Nil
 
 /*
 Most games where you wander around operate on the idea of a
@@ -20,7 +18,7 @@ Indigo has two sorts of primitives, illustrated by `Rectangle` &
 `BoundingBox`. Primitives used for rendering all operate on whole
 pixels, hence Rectangle is really:
 
-Rectangle(position: Point(x: Int, y: Int), size: Point(width: Int, height: Int))
+Rectangle(position: Point(x: Int, y: Int), size: Size(width: Int, height: Int))
 
 That makes rendering nice and easy, but whole pixels are terrible
 for abstract representations and for smoothly graphing movement.
@@ -30,78 +28,44 @@ BoundingBox(position: Vertex(x: Double, y: Double), size: Vertex(width: Double, 
 
 Almost the same, but a different level of explicit precision.
 
-So in this case, the nav mesh is a bunch of bounding boxes that
-we can perform collision checks against.
+So in this case, the nav mesh is a bunch of bounding boxes that Indigo's physics
+model can perform collision checks against.
  */
 final case class Platform(navMesh: Batch[Collider[String]], rowCount: Int) //:
-
-// def hitTest(bounds: BoundingBox): Option[BoundingBox] =
-//   navMesh.find(_.overlaps(bounds))
 
 object Platform:
 
   given CanEqual[Option[BoundingBox], Option[BoundingBox]] = CanEqual.derived
 
   def fromTerrainMap(terrainMap: TiledGridMap[TileType]): Platform =
-    val layer = terrainMap.toListPerLayer.head
+    val layer = terrainMap.toListPerLayer.head.toBatch
 
-    def toNavMesh: List[TiledGridCell[TileType]] => List[BoundingBox] =
-      filterPlatformTiles andThen
-        convertCellsToBoundingBoxes andThen
-        weldBoundingBoxes
+    def toNavMesh: Batch[TiledGridCell[TileType]] => Batch[BoundingBox] =
+      filterPlatformTiles andThen convertCellsToBoundingBoxes
 
     Platform(
-      toNavMesh(layer).toBatch.map(b => Collider.Box("platform", b).makeStatic.withFriction(Friction(0.5))),
+      toNavMesh(layer).map(b => Collider.Box("platform", b).makeStatic.withFriction(Friction(0.5))),
       terrainMap.layers.head.rowCount
     )
 
-  val filterPlatformTiles: List[TiledGridCell[TileType]] => List[TiledGridCell[TileType]] =
+  val filterPlatformTiles: Batch[TiledGridCell[TileType]] => Batch[TiledGridCell[TileType]] =
     tiles =>
       tiles
-        .filter { cell =>
+        .filter: cell =>
           cell.tile match
             case TileType.Solid if cell.row != 0 =>
-              tiles.find(t => t.column == cell.column && t.row == cell.row - 1) match {
-                case Some(TiledGridCell(_, _, TileType.Empty)) =>
-                  true
-
-                case _ =>
-                  false
-              }
+              tiles.find(t => t.column == cell.column && t.row == cell.row - 1) match 
+                case Some(TiledGridCell(_, _, TileType.Empty)) => true
+                case _ => false
 
             case _ =>
               false
 
-        }
-
-  val convertCellsToBoundingBoxes: List[TiledGridCell[TileType]] => List[BoundingBox] =
-    _.flatMap { t =>
+  val convertCellsToBoundingBoxes: Batch[TiledGridCell[TileType]] => Batch[BoundingBox] =
+    _.flatMap: t =>
       t.tile match
         case TileType.Empty =>
-          Nil
+          Batch.empty
+
         case TileType.Solid =>
-          List(BoundingBox(t.column.toDouble, t.row.toDouble, 1, 1))
-
-    }
-
-  val weldBoundingBoxes: List[BoundingBox] => List[BoundingBox] =
-    rectangles =>
-      @tailrec
-      def rec(remaining: List[BoundingBox], acc: List[BoundingBox]): List[BoundingBox] =
-        remaining match
-          case Nil =>
-            acc
-
-          case head :: next =>
-            next.find(r => head.y == r.y && (r.x + r.width == head.x || r.x == head.x + head.width)) match
-              case Some(r) =>
-                if r.x + r.width == head.x then
-                  rec(BoundingBox(r.x, r.y, r.width + head.width, 1) :: next.filterNot(_ == r), acc)
-                else if r.x == head.x + head.width then
-                  rec(BoundingBox(head.x, head.y, head.width + r.width, 1) :: next.filterNot(_ == r), acc)
-                else rec(next, head :: acc) // Shouldn't get here.
-
-              case None =>
-                rec(next, head :: acc)
-
-      rec(rectangles, Nil)
+          Batch(BoundingBox(t.column.toDouble, t.row.toDouble, 1, 1))
