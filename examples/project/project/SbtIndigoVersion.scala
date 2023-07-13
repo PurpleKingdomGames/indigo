5d982b05dd4f600b72ab5f0ae203b23a45ad93fc
David North <hobnob@users.noreply.github.com>
2023-07-13 21:43:19 +0100

    Fixes a Scala 3 warning
    The warning was that this:
    `scala.io.Source.fromFile(path).getLines.toList`
    
    Should be this:
    `scala.io.Source.fromFile(path).getLines().toList`

--- examples/project/project/SbtIndigoVersion.scala
+++ examples/project/project/SbtIndigoVersion.scala
@@ -3,13 +3,13 @@ object SbtIndigoVersion {
     def rec(path: String, levels: Int, version: Option[String]): String = {
       val msg = "ERROR: Couldn't find indigo version."
       version match {
-        case Some(v) => 
+        case Some(v) =>
           println(s"""Indigo version set to '$v'""")
           v
 
         case None if levels < 3 =>
           try {
-            val v = scala.io.Source.fromFile(path).getLines.toList.head
+            val v = scala.io.Source.fromFile(path).getLines().toList.head
             rec(path, levels, Some(v))
           } catch {
             case _: Throwable =>
@@ -21,7 +21,7 @@ object SbtIndigoVersion {
           throw new Exception(msg)
       }
     }
-    
+
     rec(".indigo-version", 0, None)
   }
 }
