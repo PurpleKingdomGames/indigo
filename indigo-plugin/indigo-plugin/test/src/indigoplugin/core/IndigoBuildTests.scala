7c02dd146700a51f36c39a46d244337bdc490afa
Dave Smith <davesmith.00000@gmail.com>
2023-09-02 10:05:50 +0100

    Tests for isCopyAllowed pass

--- indigo-plugin/indigo-plugin/test/src/indigoplugin/core/IndigoBuildTests.scala
+++ indigo-plugin/indigo-plugin/test/src/indigoplugin/core/IndigoBuildTests.scala
@@ -3,20 +3,51 @@ package indigoplugin.core
 class IndigoBuildTests extends munit.FunSuite {
 
   test("Asset filters (default)") {
-    //isCopyAllowed
-    assert(1 == 2)
+
+    val base: os.Path                  = os.Path("/some")
+    val toCopy: os.Path                = os.Path("/some/path/foo.txt")
+    val include: os.RelPath => Boolean = _ => false
+    val exclude: os.RelPath => Boolean = _ => false
+
+    val actual = IndigoBuild.isCopyAllowed(base, toCopy, include, exclude)
+
+    assert(actual)
   }
 
-  test("Asset filters (exclude)") {
-    assert(1 == 2)
+  test("Asset filters (exclude specific file)") {
+
+    val base: os.Path                  = os.Path("/some")
+    val toCopy: os.Path                = os.Path("/some/path/foo.txt")
+    val include: os.RelPath => Boolean = _ => false
+    val exclude: os.RelPath => Boolean = _ == os.RelPath("path/foo.txt")
+
+    val actual = !IndigoBuild.isCopyAllowed(base, toCopy, include, exclude)
+
+    assert(actual)
   }
 
-  test("Asset filters (include)") {
-    assert(1 == 2)
+  test("Asset filters (include - would work anyway)") {
+
+    val base: os.Path                  = os.Path("/some")
+    val toCopy: os.Path                = os.Path("/some/path/foo.txt")
+    val include: os.RelPath => Boolean = _ == os.RelPath("path/foo.txt")
+    val exclude: os.RelPath => Boolean = _ => false
+
+    val actual = IndigoBuild.isCopyAllowed(base, toCopy, include, exclude)
+
+    assert(actual)
   }
 
-  test("Asset filters (include inside excluded folder)") {
-    assert(1 == 2)
+  test("Asset filters (include a file inside excluded folder)") {
+
+    val base: os.Path                  = os.Path("/some")
+    val toCopy: os.Path                = os.Path("/some/path/foo.txt")
+    val include: os.RelPath => Boolean = _ == os.RelPath("path/foo.txt")
+    val exclude: os.RelPath => Boolean = _ == os.RelPath("path")
+
+    val actual = IndigoBuild.isCopyAllowed(base, toCopy, include, exclude)
+
+    assert(actual)
   }
 
 }
