d82ade6e08cc4f4f06e776678d50db1efa8ce0f0
Dave Smith <davesmith.00000@gmail.com>
2023-09-15 08:47:54 +0100

    WIP: Config generator

---
+++ indigo-plugin/indigo-plugin/test/src/indigoplugin/generators/ConfigGenTests.scala
@@ -0,0 +1,25 @@
+package indigoplugin.generators
+
+class ConfigGenTests extends munit.FunSuite {
+
+  test("Can detect/extract rgba") {
+    assert(1 == 2)
+  }
+
+  test("Can detect/extract rgb") {
+    assert(1 == 2)
+  }
+
+  test("Can detect/extract hex") {
+    assert(1 == 2)
+  }
+
+  test("Can detect/extract named colour") {
+    assert(1 == 2)
+  }
+
+  test("Will fall back to black") {
+    assert(1 == 2)
+  }
+
+}
