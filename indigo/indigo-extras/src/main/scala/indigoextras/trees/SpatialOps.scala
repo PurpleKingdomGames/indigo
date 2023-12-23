40bca1b4afc649f222e5b619c5043a2cdc42c3f5
Dave Smith <davesmith.00000@gmail.com>
2023-12-23 23:23:10 +0000

    Added more spatial ops types, tests failing

--- indigo/indigo-extras/src/main/scala/indigoextras/trees/SpatialOps.scala
+++ indigo/indigo-extras/src/main/scala/indigoextras/trees/SpatialOps.scala
@@ -2,33 +2,38 @@ package indigoextras.trees
 
 import indigo.shared.collections.Batch
 import indigo.shared.datatypes.Point
+import indigo.shared.datatypes.Rectangle
 import indigo.shared.geometry.BoundingBox
 import indigo.shared.geometry.Vertex
 
 trait SpatialOps[S]:
-  def giveBounds(ref: S): BoundingBox
-  def withinBounds(ref: S, bounds: BoundingBox): Boolean
+  def bounds(ref: S): BoundingBox
+  def within(ref: S, bounds: BoundingBox): Boolean
   def equals(ref: S, other: S): Boolean
-  def giveDistance(ref: S, vertex: Vertex): Double
-
-  extension (ref: S)
-    def bounds: BoundingBox                       = giveBounds(ref)
-    def containedBy(bounds: BoundingBox): Boolean = withinBounds(ref, bounds)
-    def ===(other: S): Boolean                    = equals(ref, other)
-    def distanceTo(vertex: Vertex): Double        = giveDistance(ref, vertex)
-
-  extension (b: BoundingBox) def contains(ref: S): Boolean = withinBounds(ref, b)
+  def distance(ref: S, vertex: Vertex): Double
 
 object SpatialOps:
 
   given SpatialOps[Vertex] with
-    def giveBounds(ref: Vertex): BoundingBox                    = BoundingBox.fromVertexCloud(Batch(ref))
-    def withinBounds(ref: Vertex, bounds: BoundingBox): Boolean = bounds.contains(ref)
-    def equals(ref: Vertex, other: Vertex): Boolean             = ref ~== other
-    def giveDistance(ref: Vertex, vertex: Vertex): Double       = ref.distanceTo(vertex)
+    def bounds(ref: Vertex): BoundingBox                  = BoundingBox.fromVertexCloud(Batch(ref))
+    def within(ref: Vertex, bounds: BoundingBox): Boolean = bounds.contains(ref)
+    def equals(ref: Vertex, other: Vertex): Boolean       = ref ~== other
+    def distance(ref: Vertex, vertex: Vertex): Double     = ref.distanceTo(vertex)
 
   given SpatialOps[Point] with
-    def giveBounds(ref: Point): BoundingBox                    = BoundingBox.fromVertexCloud(Batch(ref.toVertex))
-    def withinBounds(ref: Point, bounds: BoundingBox): Boolean = bounds.contains(ref.toVertex)
-    def equals(ref: Point, other: Point): Boolean              = ref == other
-    def giveDistance(ref: Point, vertex: Vertex): Double       = ref.toVertex.distanceTo(vertex)
+    def bounds(ref: Point): BoundingBox                  = BoundingBox.fromVertexCloud(Batch(ref.toVertex))
+    def within(ref: Point, bounds: BoundingBox): Boolean = bounds.contains(ref.toVertex)
+    def equals(ref: Point, other: Point): Boolean        = ref == other
+    def distance(ref: Point, vertex: Vertex): Double     = ref.toVertex.distanceTo(vertex)
+
+  given SpatialOps[BoundingBox] with
+    def bounds(ref: BoundingBox): BoundingBox                  = ref
+    def within(ref: BoundingBox, bounds: BoundingBox): Boolean = bounds.overlaps(ref)
+    def equals(ref: BoundingBox, other: BoundingBox): Boolean  = ref ~== other
+    def distance(ref: BoundingBox, vertex: Vertex): Double     = ref.sdf(vertex)
+
+  given SpatialOps[Rectangle] with
+    def bounds(ref: Rectangle): BoundingBox                  = ref.toBoundingBox
+    def within(ref: Rectangle, bounds: BoundingBox): Boolean = bounds.overlaps(ref.toBoundingBox)
+    def equals(ref: Rectangle, other: Rectangle): Boolean    = ref == other
+    def distance(ref: Rectangle, vertex: Vertex): Double     = ref.toBoundingBox.sdf(vertex)
