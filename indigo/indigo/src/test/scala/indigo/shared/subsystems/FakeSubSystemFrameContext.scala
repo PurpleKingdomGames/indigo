9f584853aa7b99cfbfbcd8e8d3e5a84b96bf5735
Dave Smith <davesmith.00000@gmail.com>
2024-03-23 10:39:56 +0000

    Fixed tests

--- indigo/indigo/src/test/scala/indigo/shared/subsystems/FakeSubSystemFrameContext.scala
+++ indigo/indigo/src/test/scala/indigo/shared/subsystems/FakeSubSystemFrameContext.scala
@@ -10,30 +10,31 @@ import indigo.shared.subsystems.SubSystemFrameContext
 import indigo.shared.time.GameTime
 import indigo.shared.time.Seconds
 
-object FakeSubSystemFrameContext {
+object FakeSubSystemFrameContext:
 
-  def context(sides: Int): SubSystemFrameContext =
-    new SubSystemFrameContext(
+  def context(sides: Int): SubSystemFrameContext[Unit] =
+    SubSystemFrameContext(
       GameTime.zero,
       Dice.loaded(sides),
       InputState.default,
-      new BoundaryLocator(new AnimationsRegister, new FontRegister, new DynamicText)
+      new BoundaryLocator(new AnimationsRegister, new FontRegister, new DynamicText),
+      ()
     )
 
-  def context(sides: Int, time: Seconds): SubSystemFrameContext =
-    new SubSystemFrameContext(
+  def context(sides: Int, time: Seconds): SubSystemFrameContext[Unit] =
+    SubSystemFrameContext(
       GameTime.is(time),
       Dice.loaded(sides),
       InputState.default,
-      new BoundaryLocator(new AnimationsRegister, new FontRegister, new DynamicText)
+      new BoundaryLocator(new AnimationsRegister, new FontRegister, new DynamicText),
+      ()
     )
 
-  def context(sides: Int, time: Seconds, delta: Seconds): SubSystemFrameContext =
-    new SubSystemFrameContext(
+  def context(sides: Int, time: Seconds, delta: Seconds): SubSystemFrameContext[Unit] =
+    SubSystemFrameContext(
       GameTime.withDelta(time, delta),
       Dice.loaded(sides),
       InputState.default,
-      new BoundaryLocator(new AnimationsRegister, new FontRegister, new DynamicText)
+      new BoundaryLocator(new AnimationsRegister, new FontRegister, new DynamicText),
+      ()
     )
-
-}
--- indigo/indigo/src/test/scala/indigo/shared/subsystems/SubSystemsRegisterTests.scala
+++ indigo/indigo/src/test/scala/indigo/shared/subsystems/SubSystemsRegisterTests.scala
@@ -9,33 +9,33 @@ class SubSystemsRegisterTests extends munit.FunSuite {
   import FakeSubSystemFrameContext._
 
   test("The sub system register.should allow you to add sub systems") {
-    val r = new SubSystemsRegister()
+    val r = new SubSystemsRegister[Int]()
     r.register(Batch(PointsTrackerExample(1, 0), PointsTrackerExample(2, 0)))
 
     assertEquals(r.size, 2)
   }
 
   test("The sub system register.should allow you to update sub systems") {
-    val r = new SubSystemsRegister()
+    val r = new SubSystemsRegister[Int]()
     r.register(Batch(PointsTrackerExample(1, 10), PointsTrackerExample(2, 50)))
 
     val data = r
-      .update(context(6), Batch(PointsTrackerEvent.Add(10)).toJSArray)
+      .update(context(6), 10, Batch(PointsTrackerEvent.Add(10)).toJSArray)
       .unsafeGet
       .stateMap
 
     val actual = data.toList.map(_._2.asInstanceOf[Int])
 
     assertEquals(actual.length, 2)
-    assertEquals(actual.contains(20), true)
-    assertEquals(actual.contains(60), true)
+    assertEquals(actual.contains(30), true) // 20 + ref data of 10
+    assertEquals(actual.contains(70), true) // 60 + ref data of 10
   }
 
   test("The sub system register.should allow you to update sub systems and emit events") {
-    val r = new SubSystemsRegister()
+    val r = new SubSystemsRegister[Int]()
     r.register(Batch(PointsTrackerExample(1, 10), PointsTrackerExample(2, 50)))
 
-    val updated = r.update(context(6), Batch(PointsTrackerEvent.LoseAll).toJSArray)
+    val updated = r.update(context(6), 10, Batch(PointsTrackerEvent.LoseAll).toJSArray)
 
     val actual = updated.unsafeGet.stateMap.toList.map(_._2.asInstanceOf[Int])
 
@@ -46,13 +46,13 @@ class SubSystemsRegisterTests extends munit.FunSuite {
   }
 
   test("The sub system register.should allow you to render sub systems") {
-    val r = new SubSystemsRegister()
+    val r = new SubSystemsRegister[Int]()
     r.register(Batch(PointsTrackerExample(1, 10), PointsTrackerExample(2, 50)))
 
     val rendered =
-      r.update(context(6), Batch(PointsTrackerEvent.Add(10)).toJSArray)
+      r.update(context(6), 0, Batch(PointsTrackerEvent.Add(10)).toJSArray)
         .unsafeGet
-        .present(context(6))
+        .present(context(6), 0)
         .unsafeGet
         .layers
         .flatMap(_.nodes)
