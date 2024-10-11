e6f734c00c757f3fd89a2d7936a0273ccbde2556
David North <hobnob@users.noreply.github.com>
2024-10-11 08:03:12 +0100

    Allows the data URL to be retrieved from an ImageData instance

--- indigo/indigo/src/main/scala/indigo/shared/ImageData.scala
+++ indigo/indigo/src/main/scala/indigo/shared/ImageData.scala
@@ -1,5 +1,7 @@
 package indigo.shared
 
+import java.util.Base64
+
 enum ImageType derives CanEqual:
   case JPEG, PNG, WEBP
 
@@ -9,4 +11,7 @@ enum ImageType derives CanEqual:
     case ImageType.WEBP => "image/webp"
   }
 
-final case class ImageData(size: Int, `type`: ImageType, data: Array[Byte])
+final case class ImageData(size: Int, `type`: ImageType, data: Array[Byte]) {
+  def getDataUrl: String =
+    s"data:${`type`.toString};base64,${new String(Base64.getEncoder.encode(data))}"
+}
