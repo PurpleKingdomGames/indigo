8ddefc74cc4250f449cb7fc76671502044ff8416
davesmith00000 <davesmith.00000@gmail.com>
2024-11-10 14:51:34 +0000

    Use Random in Confetti Scene

--- indigo/sandbox/src/main/scala/com/example/sandbox/SandboxGame.scala
+++ indigo/sandbox/src/main/scala/com/example/sandbox/SandboxGame.scala
@@ -52,7 +52,7 @@ object SandboxGame extends IndigoGame[SandboxBootData, SandboxStartupData, Sandb
   val viewportHeight: Int     = gameHeight * magnificationLevel // 256
 
   def initialScene(bootData: SandboxBootData): Option[SceneName] =
-    Some(CaptureScreenScene.name)
+    Some(ConfettiScene.name)
 
   def scenes(bootData: SandboxBootData): NonEmptyList[Scene[SandboxStartupData, SandboxGameModel, SandboxViewModel]] =
     NonEmptyList(
