8031533cf954255854cc7d8c9017eb99efd0d14c
davesmith00000 <davesmith.00000@gmail.com>
2024-11-23 22:11:20 +0000

    Adding tests for the nineslice functions

--- indigo/indigo/src/test/scala/indigo/shared/shader/library/TileAndStretchTests.scala
+++ indigo/indigo/src/test/scala/indigo/shared/shader/library/TileAndStretchTests.scala
@@ -78,117 +78,71 @@ class TileAndStretchTests extends munit.FunSuite {
 
   }
 
-  test("should correctly render tile and stretch code") {
-
-    inline def fragment =
-      Shader {
-        import TileAndStretch.*
-
-        // Delegates
-        val _tileAndStretchChannel: (Int, vec4, sampler2D.type, vec2, vec2, vec2, vec2, vec2, vec4) => vec4 =
-          tileAndStretchChannel
-
-        val fillType: Int              = 0
-        val fallback: vec4             = vec4(1.0)
-        val srcChannel: sampler2D.type = sampler2D
-        val channelPos: vec2           = vec2(2.0)
-        val channelSize: vec2          = vec2(3.0)
-        val uv: vec2                   = vec2(4.0)
-        val entitySize: vec2           = vec2(5.0)
-        val textureSize: vec2          = vec2(6.0)
-        val nineSliceCenter: vec4      = vec4(7.0)
-
-        _tileAndStretchChannel(
-          fillType,       // env.FILLTYPE.toInt,
-          fallback,       // env.CHANNEL_0,
-          srcChannel,     // env.SRC_CHANNEL,
-          channelPos,     // env.CHANNEL_0_POSITION,
-          channelSize,    // env.CHANNEL_0_SIZE,
-          uv,             // env.UV,
-          entitySize,     // env.SIZE,
-          textureSize,    // env.TEXTURE_SIZE
-          nineSliceCenter // env.NINE_SLICE_CENTER
-        )
-      }
-
+  test("regionToUV") {
     val actual =
-      fragment.toGLSL[WebGL2].toOutput.code
+      TileAndStretch.regionToUV(vec4(vec2(16.0f), vec2(32.0f)), vec2(64.0f))
 
-    // println(actual)
+    val expected =
+      vec4(vec2(0.25f), vec2(0.5f))
 
-    assertEquals(
-      actual,
-      s"""
-      |vec4 def0(in int fillType,in vec4 fallback,in sampler2D srcChannel,in vec2 channelPos,in vec2 channelSize,in vec2 uv,in vec2 entitySize,in vec2 textureSize){
-      |  vec4 val0;
-      |  switch(fillType){
-      |    case 1:
-      |      val0=texture(srcChannel,channelPos+(uv*channelSize));
-      |      break;
-      |    case 2:
-      |      val0=texture(srcChannel,channelPos+((fract(uv*(entitySize/textureSize)))*channelSize));
-      |      break;
-      |    case 3:
-      |      // maddness ensues
-      |      break;
-      |    default:
-      |      val0=fallback;
-      |      break;
-      |  }
-      |  return val0;
-      |}
-      |int fillType=0;
-      |vec4 fallback=vec4(1.0);
-      |sampler2D srcChannel=sampler2D;
-      |vec2 channelPos=vec2(2.0);
-      |vec2 channelSize=vec2(3.0);
-      |vec2 uv=vec2(4.0);
-      |vec2 entitySize=vec2(5.0);
-      |vec2 textureSize=vec2(6.0);
-      |def0(fillType,fallback,srcChannel,channelPos,channelSize,uv,entitySize,textureSize);
-      |""".stripMargin.trim
-    )
-
+    assertEquals(actual, expected)
   }
 
-  test("should correctly render nineslice code") {
+  test("regionContainsUV") {
+    val uvRegion = TileAndStretch.regionToUV(vec4(vec2(16.0f), vec2(32.0f)), vec2(64.0f))
 
-    inline def fragment =
-      Shader {
-        import TileAndStretch.*
+    assert(clue(!TileAndStretch.regionContainsUV(uvRegion, vec2(0.0f, 0.0f))))
+    assert(clue(!TileAndStretch.regionContainsUV(uvRegion, vec2(0.24f, 0.24f))))
+    assert(clue(TileAndStretch.regionContainsUV(uvRegion, vec2(0.25f, 0.25f))))
+    assert(clue(TileAndStretch.regionContainsUV(uvRegion, vec2(0.5f, 0.5f))))
+    assert(clue(TileAndStretch.regionContainsUV(uvRegion, vec2(0.74f, 0.74f))))
+    assert(clue(!TileAndStretch.regionContainsUV(uvRegion, vec2(0.75f, 0.75f))))
+    assert(clue(!TileAndStretch.regionContainsUV(uvRegion, vec2(1.0f, 1.0f))))
+  }
 
-        val uv: vec2              = vec2(1.0)
-        val channelPos: vec2      = vec2(2.0)
-        val channelSize: vec2     = vec2(3.0)
-        val entitySize: vec2      = vec2(128.0)
-        val textureSize: vec2     = vec2(64.0)
-        val nineSliceCenter: vec4 = vec4(16.0f, 16.0f, 32.0f, 32.0f)
+  test("mapUVToRegionUV") {
+    val entityRegionUV = TileAndStretch.regionToUV(vec4(vec2(16.0f), vec2(32.0f)), vec2(64.0f))
 
-        def doNineSlice(): vec2 =
-          nineSliceUVs(
-            uv,             // env.UV,
-            channelPos,     // env.CHANNEL_0_POSITION,
-            channelSize,    // env.CHANNEL_0_SIZE,
-            entitySize,     // env.SIZE,
-            textureSize,    // env.TEXTURE_SIZE
-            nineSliceCenter // env.NINE_SLICE_CENTER
-          )
+    def forUV(uv: vec2): vec2 =
+      TileAndStretch.mapUVToRegionUV(uv, entityRegionUV)
 
-        doNineSlice()
-      }
+    assertEquals(clue(forUV(vec2(0.0f))), clue(vec2(-0.5f)))
+    assertEquals(clue(forUV(vec2(0.25f))), clue(vec2(0.0f)))
+    assertEquals(clue(forUV(vec2(0.5f))), clue(vec2(0.5f)))
+    assertEquals(clue(forUV(vec2(0.75f))), clue(vec2(1.0f)))
+    assertEquals(clue(forUV(vec2(1.0f))), clue(vec2(1.5f)))
+  }
 
-    val actual =
-      fragment.toGLSL[WebGL2].toOutput.code
+  test("tileUV") {
+    val unscaledEntityRegionSize  = vec2(64.0f)
+    val unscaledTextureRegionSize = vec2(32.0f)
 
-    // println(actual)
+    // Maybe split this function in two?
 
-    assertEquals(
-      actual,
-      s"""
-      |fish
-      |""".stripMargin.trim
-    )
+    def forUV(uv: vec2): vec2 =
+      TileAndStretch.tileUV(uv, unscaledEntityRegionSize, unscaledTextureRegionSize)
 
+    assertEquals(clue(forUV(vec2(0.0f))), clue(vec2(0.0f)))
+    assertEquals(clue(forUV(vec2(0.25f))), clue(vec2(0.5f)))
+    assertEquals(clue(forUV(vec2(0.5f))), clue(vec2(0.0f)))
+    assertEquals(clue(forUV(vec2(0.75f))), clue(vec2(0.5f)))
+    // We never see 1.0 because that's the wrap point. In practice this doesnt matter.
+    assertEquals(clue(forUV(vec2(1.0f))), clue(vec2(0.0f)))
+  }
+
+  test("tiledUVsToTextureCoords") {
+    val channelSize   = vec2(64.0f)
+    val textureRegion = TileAndStretch.regionToUV(vec4(vec2(16.0f), vec2(32.0f)), channelSize)
+    val channelPos    = vec2(10.0f)
+
+    def forUV(tileUV: vec2): vec2 =
+      TileAndStretch.tiledUVsToTextureCoords(tileUV, textureRegion, channelPos, channelSize)
+
+    assertEquals(clue(forUV(vec2(0.0f))), clue(vec2(16.0f) + vec2(10.0f)))
+    assertEquals(clue(forUV(vec2(0.25f))), clue(vec2(24.0f) + vec2(10.0f)))
+    assertEquals(clue(forUV(vec2(0.5f))), clue(vec2(32.0f) + vec2(10.0f)))
+    assertEquals(clue(forUV(vec2(0.75f))), clue(vec2(40.0f) + vec2(10.0f)))
+    assertEquals(clue(forUV(vec2(1.0f))), clue(vec2(48.0f) + vec2(10.0f)))
   }
 
 }
