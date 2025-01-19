48417db623652f752fbf570ba11b1d3fd0872253
davesmith00000 <davesmith.00000@gmail.com>
2025-01-19 23:36:30 +0000

    Size: min/max tests

--- indigo/indigo/src/test/scala/indigo/shared/datatypes/SizeTests.scala
+++ indigo/indigo/src/test/scala/indigo/shared/datatypes/SizeTests.scala
@@ -13,4 +13,18 @@ class SizeTests extends munit.FunSuite {
     assertEquals(clue(Size.mod(Size(-11), Size(-10))), clue(Size(-1)))
   }
 
+  test("min") {
+    assertEquals(Size(11, 12).min(Size(10, 10)), Size(10, 10))
+    assertEquals(Size(11, 12).min(10), Size(10, 10))
+    assertEquals(Size(11, 12).min(Size(12, 10)), Size(11, 10))
+    assertEquals(Size(11, 12).min(5), Size(5, 5))
+  }
+
+  test("max") {
+    assertEquals(Size(11, 12).max(Size(10, 10)), Size(11, 12))
+    assertEquals(Size(11, 12).max(10), Size(11, 12))
+    assertEquals(Size(11, 12).max(Size(12, 10)), Size(12, 12))
+    assertEquals(Size(11, 12).max(12), Size(12, 12))
+  }
+
 }
