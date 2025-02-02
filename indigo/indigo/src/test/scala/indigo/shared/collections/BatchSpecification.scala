5cdd074117d0681075baa52608ef6d5fd4a78366
exoego <ytatsuno.jp@gmail.com>
2025-02-02 10:16:24 +0900

    add failing properties

---
+++ indigo/indigo/src/test/scala/indigo/shared/collections/BatchSpecification.scala
@@ -0,0 +1,22 @@
+package indigo.shared.collections
+
+import org.scalacheck.*
+
+import scala.annotation.nowarn
+
+@SuppressWarnings(Array("scalafix:DisableSyntax.var"))
+@nowarn("msg=unused")
+class BatchSpecification extends Properties("Batch") {
+  property("hashCode - both Wrapped") = Prop.forAll { (a: List[Int]) =>
+    Batch(a*).hashCode == Batch(a*).hashCode
+  }
+
+  property("hashCode - both Combined") = Prop.forAll { (a: List[Int], b: List[Int], c: List[Int]) =>
+    Batch.Combine(Batch.Combine(Batch(a*), Batch(b*)), Batch(c*)).hashCode ==
+      Batch.Combine(Batch(a*), Batch.Combine(Batch(b*), Batch(c*))).hashCode
+  }
+
+  property("hashCode - Wrapped and Combined, same elements") = Prop.forAll { (a: List[Int], b: List[Int]) =>
+    Batch((a ++ b)*).hashCode == Batch.Combine(Batch(a*), Batch(b*)).hashCode
+  }
+}
