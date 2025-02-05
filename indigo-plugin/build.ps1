fb5dae10bfba6ab86ac713e4b293b22aa65c1652
Miguel <miguel.trigueira@gmail.com>
2025-02-05 11:02:56 +0000

    Fail build fast in windows

--- indigo-plugin/build.ps1
+++ indigo-plugin/build.ps1
@@ -1,14 +1,22 @@
+function mill {
+    param (
+        [string]$Command
+    )
+
+    .\millw.bat -i $Command
+    if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+}
 
 Remove-Item -Recurse -Force -Path .\out
 
-.\millw.bat -i clean
-.\millw.bat -i clean indigo-plugin[2.12]
-.\millw.bat -i clean indigo-plugin[2.13]
-.\millw.bat -i indigo-plugin[2.12].compile
-.\millw.bat -i indigo-plugin[2.13].compile
-.\millw.bat -i indigo-plugin[2.12].test
-.\millw.bat -i indigo-plugin[2.13].test
-.\millw.bat -i indigo-plugin[2.12].checkFormat
-.\millw.bat -i indigo-plugin[2.13].checkFormat
-.\millw.bat -i indigo-plugin[2.12].publishLocal
-.\millw.bat -i indigo-plugin[2.13].publishLocal
+mill clean
+mill clean indigo-plugin[2.12]
+mill clean indigo-plugin[2.13]
+mill indigo-plugin[2.12].compile
+mill indigo-plugin[2.13].compile
+mill indigo-plugin[2.12].test
+mill indigo-plugin[2.13].test
+mill indigo-plugin[2.12].checkFormat
+mill indigo-plugin[2.13].checkFormat
+mill indigo-plugin[2.12].publishLocal
+mill indigo-plugin[2.13].publishLocal
