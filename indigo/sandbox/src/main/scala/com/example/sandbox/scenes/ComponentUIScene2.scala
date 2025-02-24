b0e5af145d219cea0b4f94b92ac80c06afe75384
davesmith00000 <davesmith.00000@gmail.com>
2025-02-24 22:37:01 +0000

    Improved scroll component demo to include buttons

--- indigo/sandbox/src/main/scala/com/example/sandbox/scenes/ComponentUIScene2.scala
+++ indigo/sandbox/src/main/scala/com/example/sandbox/scenes/ComponentUIScene2.scala
@@ -1,6 +1,7 @@
 package com.example.sandbox.scenes
 
 import com.example.sandbox.Fonts
+import com.example.sandbox.Log
 import com.example.sandbox.SandboxAssets
 import com.example.sandbox.SandboxGameModel
 import com.example.sandbox.SandboxStartupData
@@ -38,6 +39,10 @@ object ComponentUIScene2 extends Scene[SandboxStartupData, SandboxGameModel, San
     case ChangeValue(value) =>
       Outcome(model.copy(num = value))
 
+    case Log(msg) =>
+      println(s"Log: $msg")
+      Outcome(model)
+
     case e =>
       val ctx =
         UIContext(context.toContext)
@@ -54,7 +59,8 @@ object ComponentUIScene2 extends Scene[SandboxStartupData, SandboxGameModel, San
       model: SandboxGameModel,
       viewModel: SandboxViewModel
   ): GlobalEvent => Outcome[SandboxViewModel] =
-    _ => Outcome(viewModel)
+    case _ =>
+      Outcome(viewModel)
 
   def present(
       context: SceneContext[SandboxStartupData],
@@ -95,18 +101,71 @@ object ComponentUIScene2 extends Scene[SandboxStartupData, SandboxGameModel, San
     val listOfLabels: ComponentList[Int] =
       ComponentList(Dimensions(200, 200)) { (ctx: UIContext[Int]) =>
         (1 to ctx.reference).toBatch.map { i =>
-          ComponentId("lbl" + i) -> Label[Int](
-            i.toString,
-            (_, label) => Bounds(0, 0, 250, 20)
-          ) { case (ctx, label) =>
-            Outcome(
-              Layer(
-                text
-                  .withText(label.text(ctx))
-                  .moveTo(ctx.parent.coords.unsafeToPoint)
+          ComponentId("lbl" + i) ->
+            ComponentGroup[Int]()
+              .withLayout(ComponentLayout.Horizontal(Padding.right(10)))
+              .add(
+                Button[Int](Bounds(16, 16)) { (ctx, btn) =>
+                  Outcome(
+                    Layer(
+                      Shape
+                        .Box(
+                          Rectangle(
+                            ctx.parent.bounds.unsafeToRectangle.position,
+                            btn.bounds.dimensions.unsafeToSize
+                          ),
+                          Fill.Color(RGBA.Magenta.mix(RGBA.Black)),
+                          Stroke(1, RGBA.Magenta)
+                        )
+                    )
+                  )
+                }
+                  .presentDown { (ctx, btn) =>
+                    Outcome(
+                      Layer(
+                        Shape
+                          .Box(
+                            Rectangle(
+                              ctx.parent.bounds.unsafeToRectangle.position,
+                              btn.bounds.dimensions.unsafeToSize
+                            ),
+                            Fill.Color(RGBA.Cyan.mix(RGBA.Black)),
+                            Stroke(1, RGBA.Cyan)
+                          )
+                      )
+                    )
+                  }
+                  .presentOver { (ctx, btn) =>
+                    Outcome(
+                      Layer(
+                        Shape
+                          .Box(
+                            Rectangle(
+                              ctx.parent.bounds.unsafeToRectangle.position,
+                              btn.bounds.dimensions.unsafeToSize
+                            ),
+                            Fill.Color(RGBA.Yellow.mix(RGBA.Black)),
+                            Stroke(1, RGBA.Yellow)
+                          )
+                      )
+                    )
+                  }
+                  .onClick(_ => Batch(Log("Clicked button: " + i)))
               )
-            )
-          }
+              .add(
+                Label[Int](
+                  i.toString,
+                  (_, label) => Bounds(0, 0, 250, 20)
+                ) { case (ctx, label) =>
+                  Outcome(
+                    Layer(
+                      text
+                        .withText(label.text(ctx))
+                        .moveTo(ctx.parent.coords.unsafeToPoint)
+                    )
+                  )
+                }
+              )
         }
       }
         .withLayout(ComponentLayout.Vertical(Padding(10)))
@@ -142,7 +201,7 @@ object ComponentUIScene2 extends Scene[SandboxStartupData, SandboxGameModel, San
             )
           )
         }
-        .presentOver((ctx, btn) =>
+        .presentOver { (ctx, btn) =>
           Outcome(
             Layer(
               Shape
@@ -156,7 +215,7 @@ object ComponentUIScene2 extends Scene[SandboxStartupData, SandboxGameModel, San
                 )
             )
           )
-        )
+        }
 
     val pane: ScrollPane[ComponentList[Int], Int] =
       ScrollPane(
