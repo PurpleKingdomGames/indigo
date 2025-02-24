f08d2bf2612ccb87dbc59f358d3bfee24c7c880e
davesmith00000 <davesmith.00000@gmail.com>
2025-02-24 21:58:40 +0000

    Added a multi window scene to the sandbox

--- indigo/sandbox/src/main/scala/com/example/sandbox/SandboxGame.scala
+++ indigo/sandbox/src/main/scala/com/example/sandbox/SandboxGame.scala
@@ -22,7 +22,7 @@ object SandboxGame extends IndigoGame[SandboxBootData, SandboxStartupData, Sandb
   val viewportHeight: Int     = gameHeight * magnificationLevel // 256
 
   def initialScene(bootData: SandboxBootData): Option[SceneName] =
-    Some(ComponentUIScene2.name)
+    Some(WindowsScene.name)
 
   def scenes(bootData: SandboxBootData): NonEmptyList[Scene[SandboxStartupData, SandboxGameModel, SandboxViewModel]] =
     NonEmptyList(
@@ -52,6 +52,7 @@ object SandboxGame extends IndigoGame[SandboxBootData, SandboxStartupData, Sandb
       NineSliceScene,
       ComponentUIScene,
       ComponentUIScene2,
+      WindowsScene,
       MeshScene
     )
 
