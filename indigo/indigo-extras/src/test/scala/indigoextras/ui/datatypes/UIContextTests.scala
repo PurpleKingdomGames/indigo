8c43af2f3565286c5eadba71542d8ea965dae5b9
davesmith00000 <davesmith.00000@gmail.com>
2025-04-09 23:12:17 +0100

    Fixed #857: UIContext ignores snapgrid after construction

---
+++ indigo/indigo-extras/src/test/scala/indigoextras/ui/datatypes/UIContextTests.scala
@@ -0,0 +1,27 @@
+package indigoextras.ui.datatypes
+
+import indigo.*
+
+class UIContextTests extends munit.FunSuite {
+
+  test("UIContext should correctly calculate the pointer coords") {
+    val uiContext =
+      UIContext(Context.initial)
+        .withPointerCoords(Coords(10, 20))
+
+    assertEquals(uiContext.pointerCoords, Coords(10, 20))
+    assertEquals(uiContext.withSnapGrid(Size(2)).pointerCoords, Coords(5, 10))
+    assertEquals(uiContext.withSnapGrid(Size(10)).pointerCoords, Coords(1, 2))
+
+    val ctxA =
+      UIContext(SubSystemContext.fromContext(Context.initial), Size(10), 2)
+        .withPointerCoords(Coords(10, 20))
+    val ctxB =
+      UIContext(Context.initial)
+        .withSnapGrid(Size(10))
+        .withPointerCoords(Coords(10, 20))
+
+    assertEquals(ctxA.pointerCoords, ctxB.pointerCoords)
+  }
+
+}
