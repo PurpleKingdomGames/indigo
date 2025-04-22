43773d3577f85343f2cfc9afc3fa0fb6d5c88859
davesmith00000 <davesmith.00000@gmail.com>
2025-04-22 08:00:17 +0100

    Scalafmt

--- indigo/indigo-extras/src/main/scala/indigoextras/performers/PerformerId.scala
+++ indigo/indigo-extras/src/main/scala/indigoextras/performers/PerformerId.scala
@@ -6,7 +6,7 @@ object PerformerId:
   def apply(value: String): PerformerId = value
 
   extension (a: PerformerId)
-    def value: String = a
+    def value: String    = a
     def toString: String = a
 
   given CanEqual[PerformerId, PerformerId] = CanEqual.derived
