1997db1e5a26b16bbefba39922f53c116c229ed0
davesmith00000 <davesmith.00000@gmail.com>
2025-04-24 09:14:15 +0100

    Performer types: Narrator, Extra, and Lead

--- indigo/indigo-extras/src/main/scala/indigoextras/performers/Performer.scala
+++ indigo/indigo-extras/src/main/scala/indigoextras/performers/Performer.scala
@@ -5,24 +5,58 @@ import indigo.shared.collections.Batch
 import indigo.shared.events.GlobalEvent
 import indigo.shared.scenegraph.SceneNode
 
-// TODO: Might need to remove the ReferenceData type, generally unprovable. Use an 'extract your own' approach instead?
-trait Performer[ReferenceData]:
+/** Performers represent entities in your scene. Indigo uses performance metaphors (like Lead and Extra) to represent
+  * different levels of performer complexity. It’s meant to be fun, but also to suggest how much responsibility each
+  * type takes on in the simulation. From Leads who take the starring roles, to silent Extras filling the background, to
+  * Narrators who influence events without being seen — each has a role to play.
+  *
+  * @tparam ReferenceData
+  *   The type of reference data used by the performer.
+  */
+sealed trait Performer[ReferenceData]:
 
   /** A unique identifier for the performer.
     */
   def id: PerformerId
 
-  // TODO: Depth? Location? Position? Either the position is managed in the Stage manager and altered by events, or the performer says where it ought to be.
   /** Provide the depth / order of the performer
     */
   def depth: PerformerDepth
 
-  // TODO: Should the performer say which layer it's targeting? Or something?
+object Performer:
 
-  /** Update the performer
+  /** Narrators are the storytellers of the performance. In practical terms, they are strictly state holders and message
+    * passers, and have no rendered presence in the scene.
     */
-  def update(context: PerformerContext[ReferenceData]): GlobalEvent => Outcome[Performer[ReferenceData]]
+  trait Narrator[ReferenceData] extends Performer[ReferenceData]:
 
-  /** Draw the performer
+    /** Update the performer
+      */
+    def update(context: PerformerContext[ReferenceData]): GlobalEvent => Outcome[Performer.Narrator[ReferenceData]]
+
+  /** Extra performers are the background characters of the performance. They are responsible for rendering themselves
+    * and updating their state, but they cannot interact with the game directly since they have no way to listen to or
+    * emit events.
     */
-  def present(context: PerformerContext[ReferenceData]): Outcome[Batch[SceneNode]]
+  trait Extra[ReferenceData] extends Performer[ReferenceData]:
+
+    /** Update the performer
+      */
+    def update(context: PerformerContext[ReferenceData]): Performer.Extra[ReferenceData]
+
+    /** Draw the performer
+      */
+    def present(context: PerformerContext[ReferenceData]): SceneNode
+
+  /** Lead performers are the stars of the show. They are responsible for rendering themselves, updating their state,
+    * and can also listen to and emit events. They are the most complex type of performer, but also the most powerful.
+    */
+  trait Lead[ReferenceData] extends Performer[ReferenceData]:
+
+    /** Update the performer
+      */
+    def update(context: PerformerContext[ReferenceData]): GlobalEvent => Outcome[Performer.Lead[ReferenceData]]
+
+    /** Draw the performer
+      */
+    def present(context: PerformerContext[ReferenceData]): Outcome[Batch[SceneNode]]
