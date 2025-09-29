b0c0b3d1405378d95326b80c481e1e0a35990dde
davesmith00000 <davesmith.00000@gmail.com>
2025-09-29 08:51:11 +0100

    Remove generator overloads that take a path as a String

--- indigo-plugin/indigo-plugin/src/indigoplugin/IndigoGenerators.scala
+++ indigo-plugin/indigo-plugin/src/indigoplugin/IndigoGenerators.scala
@@ -73,23 +73,6 @@ final case class IndigoGenerators private (
         EmbedText.generate(moduleName, fullyQualifiedPackageName, os.RelPath(file).resolveFrom(workspaceDir), present)
     )
 
-  /** Takes the contents of a text file, and leaves it to you to decide how to turn it into Scala code. The template
-    * provides nothing except the package declaration.
-    *
-    * @param moduleName
-    *   The name for the Scala module, in this case, acts as the file name only.
-    * @param file
-    *   The relative path to the text file to embed.
-    * @param present
-    *   A function that takes and String and expects you to create a String of Scala code. You could parse JSON or read
-    *   a list of files or... anything!
-    */
-  def embed(moduleName: String, file: String)(present: String => String): IndigoGenerators =
-    this.copy(
-      sources = sources :+
-        EmbedText.generate(moduleName, fullyQualifiedPackageName, os.RelPath(file).resolveFrom(workspaceDir), present)
-    )
-
   /** Embed raw text into a static variable.
     *
     * @param moduleName
@@ -114,19 +97,6 @@ final case class IndigoGenerators private (
     this.copy(
       sources = sources :+
         EmbedText.generate(moduleName, fullyQualifiedPackageName, os.Path(file))
-    )
-
-  /** Embed raw text into a static variable.
-    *
-    * @param moduleName
-    *   The name for the Scala module, e.g. 'MyModule' would be `object MyModule {}`
-    * @param file
-    *   The relative path to the text file to embed.
-    */
-  def embedText(moduleName: String, file: String): IndigoGenerators =
-    this.copy(
-      sources = sources :+
-        EmbedText.generate(moduleName, fullyQualifiedPackageName, os.RelPath(file).resolveFrom(workspaceDir))
     )
 
   /** Embed a GLSL shader pair into a Scala module.
@@ -201,44 +171,6 @@ final case class IndigoGenerators private (
           fullyQualifiedPackageName,
           os.Path(vertexShaderPath),
           os.Path(fragmentShaderPath),
-          validate
-        )
-    )
-
-  /** Embed a GLSL shader pair into a Scala module.
-    *
-    * The shader pair can be raw GLSL, or they can make use of the indigo tags to defined the regions you want to embed.
-    * E.g.,
-    *
-    * ```...Code above and below the tag will be ignored...```
-    * ```//<indigo-vertex>```
-    * ```...shader code goes here...```
-    * ```//</indigo-vertex>```
-    *
-    * Available tags are: vertex, fragment, prepare, composite, light
-    *
-    * @param moduleName
-    *   The name for the Scala module, e.g. 'MyModule' would be `object MyModule {}`
-    * @param vertexShaderPath
-    *   The relative path to the vertex shader file
-    * @param fragmentShaderPath
-    *   The relative path to the fragment shader file
-    * @param validate
-    *   Attempt to validate the GLSL, requires the glslang validator to be install locally on the machine.
-    */
-  def embedGLSLShaders(
-      moduleName: String,
-      vertexShaderPath: String,
-      fragmentShaderPath: String,
-      validate: Boolean
-  ): IndigoGenerators =
-    this.copy(
-      sources = sources :+
-        EmbedGLSLShaderPair.generate(
-          moduleName,
-          fullyQualifiedPackageName,
-          os.RelPath(vertexShaderPath).resolveFrom(workspaceDir),
-          os.RelPath(fragmentShaderPath).resolveFrom(workspaceDir),
           validate
         )
     )
@@ -324,31 +256,6 @@ final case class IndigoGenerators private (
       os.Path(font),
       fontOptions,
       os.Path(imageOut)
-    )
-
-  /** Used to generate a rendered font sheet and `FontInfo` instance based on a supplied font source file.
-    *
-    * @param moduleName
-    *   The name for the Scala module, e.g. 'MyModule' would be `object MyModule {}`
-    * @param font
-    *   The path to the font file, e.g. a TrueType *.ttf file
-    * @param fontOptions
-    *   Parameters for the font, such as its identifier (font key), size, and anti-aliasing.
-    * @param imageOut
-    *   The destination directory for the font-sheet image to be written into, typically somewhere in your assets
-    *   directory so that your game can load it.
-    */
-  def embedFont(
-      moduleName: String,
-      font: String,
-      fontOptions: FontOptions,
-      imageOut: String
-  ): IndigoGenerators =
-    embedFont(
-      moduleName,
-      os.RelPath(font).resolveFrom(workspaceDir),
-      fontOptions,
-      os.RelPath(imageOut).resolveFrom(workspaceDir)
     )
 
   /** Used to embed CSV (Comma Separated Value) data, usage: embedCSV.asEnum(moduleName, filePath), or
@@ -465,34 +372,6 @@ final case class IndigoGenerators private (
           )
       )
 
-    /** Embed the data as a Scala 3 Enum. */
-    def asEnum(moduleName: String, file: String): IndigoGenerators =
-      gens.copy(
-        sources = sources :+
-          EmbedData.generate(
-            moduleName,
-            fullyQualifiedPackageName,
-            os.RelPath(file).resolveFrom(workspaceDir),
-            delimiter,
-            rowFilter,
-            embedMode = EmbedData.Mode.AsEnum(None)
-          )
-      )
-
-    /** Embed the data as a Scala 3 Enum that extends some fully qualified module name. E.g. `com.example.MyData`. */
-    def asEnum(moduleName: String, file: String, extendsFrom: String): IndigoGenerators =
-      gens.copy(
-        sources = sources :+
-          EmbedData.generate(
-            moduleName,
-            fullyQualifiedPackageName,
-            os.RelPath(file).resolveFrom(workspaceDir),
-            delimiter,
-            rowFilter,
-            embedMode = EmbedData.Mode.AsEnum(Option(extendsFrom))
-          )
-      )
-
     /** Embed the data as a Map. */
     def asMap(moduleName: String, file: os.Path): IndigoGenerators =
       gens.copy(
@@ -515,20 +394,6 @@ final case class IndigoGenerators private (
             moduleName,
             fullyQualifiedPackageName,
             os.Path(file),
-            delimiter,
-            rowFilter,
-            embedMode = EmbedData.Mode.AsMap
-          )
-      )
-
-    /** Embed the data as a Map. */
-    def asMap(moduleName: String, file: String): IndigoGenerators =
-      gens.copy(
-        sources = sources :+
-          EmbedData.generate(
-            moduleName,
-            fullyQualifiedPackageName,
-            os.RelPath(file).resolveFrom(workspaceDir),
             delimiter,
             rowFilter,
             embedMode = EmbedData.Mode.AsMap
@@ -597,36 +462,6 @@ final case class IndigoGenerators private (
           )
       )
 
-    /** Embed the data using a custom present function. In this mode we make no assumptions about what code you plan to
-      * generate. The result of the present function will be placed inside a scala file named after the module name,
-      * that includes the package, but nothing else.
-      *
-      * We provide the same mechanism and guarantees as using the other data embed methods, things like consistent row
-      * lengths, and all columns having the same type of data (`StringData`, `IntData`, `DouleData`, or `BooleanData`).
-      * The `DataType` provides information about the type in the Cell, and some simple/useful methods, and can be
-      * exhustively pattern matched.
-      *
-      * @param moduleName
-      *   In this mode, this is the name of the file only. If you wish to use it in your present function, you can
-      *   always apply it as an argument to a curried function.
-      * @param file
-      *   The data file to read.
-      * @param present
-      *   A function from a list of all the rows, including the headers, to some generated output code, written as a
-      *   simple `String` value.
-      */
-    def asCustom(moduleName: String, file: String)(present: List[List[DataType]] => String): IndigoGenerators =
-      gens.copy(
-        sources = sources :+
-          EmbedData.generate(
-            moduleName,
-            fullyQualifiedPackageName,
-            os.RelPath(file).resolveFrom(workspaceDir),
-            delimiter,
-            rowFilter,
-            embedMode = EmbedData.Mode.AsCustom(present)
-          )
-      )
   }
 
   /** Embed Aseprite data in a module.
@@ -657,25 +492,6 @@ final case class IndigoGenerators private (
     this.copy(
       sources = sources :+
         EmbedAseprite.generate(moduleName, fullyQualifiedPackageName, os.Path(file))
-    )
-
-  /** Embed Aseprite data in a module.
-    *
-    * PLEASE NOTE: Aseprite data must be exported using the 'array' option, the 'hash' format is not supported.
-    *
-    * @param moduleName
-    *   The name for the Scala module, e.g. 'MyModule' would be `object MyModule {}`
-    * @param file
-    *   The path to the Asprite JSON data file.
-    */
-  def embedAseprite(moduleName: String, file: String): IndigoGenerators =
-    this.copy(
-      sources = sources :+
-        EmbedAseprite.generate(
-          moduleName,
-          fullyQualifiedPackageName,
-          os.RelPath(file).resolveFrom(workspaceDir)
-        )
     )
 
 }
